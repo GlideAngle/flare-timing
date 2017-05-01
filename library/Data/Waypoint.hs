18efe845949deaee6dc94b4081e770bf4b724e27
Phil de Joux <phil.dejoux@blockscope.com>
2017-05-01 14:19:01 +1200

    Ignore lines that are not B or fix records.

--- library/Data/Waypoint.hs
+++ library/Data/Waypoint.hs
@@ -11,46 +11,39 @@ import Text.ParserCombinators.Parsec
     )
 import qualified Text.ParserCombinators.Parsec as P
 
-{- A CSV file contains 0 or more lines, each of which is terminated
-   by the end-of-line character (eol). -}
-csvFile :: GenParser Char st [[String]]
-csvFile = 
-    do result <- many line
-       _ <- eof
-       return result
+data IgcRecord
+    = B String
+    | Ignore
+    deriving Show
 
--- Each line contains 1 or more cells, separated by a comma
-line :: GenParser Char st [String]
-line = 
-    do result <- cells
-       _ <- eol
-       return result
+isB :: IgcRecord -> Bool
+isB (B _) = True
+isB _ = False
+
+igcFile :: GenParser Char st [IgcRecord]
+igcFile = do
+    result <- many line
+    _ <- eof
+    return $ filter isB result
+
+line :: GenParser Char st IgcRecord
+line = do
+    result <- fix <|> ignore
+    _ <- eol
+    return result
        
--- Build up a list of cells.  Try to parse the first cell, then figure out 
--- what ends the cell.
-cells :: GenParser Char st [String]
-cells = 
-    do first <- cellContent
-       next <- remainingCells
-       return (first : next)
+fix :: GenParser Char st IgcRecord
+fix = do
+    _ <- char 'B'
+    rest <- many (noneOf "\n")
+    return $ B rest
 
--- The cell either ends with a comma, indicating that 1 or more cells follow,
--- or it doesn't, indicating that we're at the end of the cells for this line
-remainingCells :: GenParser Char st [String]
-remainingCells =
-    (char ',' >> cells)            -- Found comma?  More cells coming
-    <|> (return [])                -- No comma?  Return [], no more cells
+ignore :: GenParser Char st IgcRecord
+ignore = do
+    return Ignore
 
--- Each cell contains 0 or more characters, which must not be a comma or
--- EOL
-cellContent :: GenParser Char st String
-cellContent = 
-    many (noneOf ",\n")
-       
-
--- The end of line character is \n
 eol :: GenParser Char st Char
 eol = char '\n'
 
-parse :: String -> Either ParseError [[String]]
-parse input = P.parse csvFile "(stdin)" input
+parse :: String -> Either ParseError [IgcRecord]
+parse input = P.parse igcFile "(stdin)" input
