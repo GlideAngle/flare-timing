362cdb104518a1922399fe201646b1026d50642a
Phil de Joux <phil.dejoux@blockscope.com>
2017-05-18 10:04:52 +1200

    Add failing test results.

--- test-suite-score/Score.hs
+++ test-suite-score/Score.hs
@@ -21,17 +21,30 @@ properties = testGroup "Properties" [scProps, qcProps]
 
 scProps :: TestTree
 scProps = testGroup "(checked by SmallCheck)"
+    -- WARNING: Failing test.
+    --    there exist 0 1 0 1 such that
+    --      condition is false
     [ SC.testProperty "Launch validity is in the range of [0, 1]" scLaunchValidity
     ]
 
 
 qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)"
+    -- WARNING: Failing test.
+    --    *** Failed! Falsifiable (after 1 test and 1 shrink):
+    --    NonNegative {getNonNegative = 0}
+    --    Positive {getPositive = 1}
+    --    NonNegative {getNonNegative = 0}
+    --    Positive {getPositive = 1}
+    --    Use --quickcheck-replay '0 TFGenR 0000002DEC6F478500000000002625A0000000000000E2220000000000000000 0 1 1 0' to reproduce.
     [ QC.testProperty "Launch validity is in the range of [0, 1]" qcLaunchValidity
     ]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
+    -- WARNING: Failing test.
+    --  expected: 0 % 1
+    --   but got: (-269653970229347386159395778618353710042696546841345985910145121736599013708251444699062715983611304031680170819807090036488184653221624933739271145959211186566651 840137298227914453329401869141179179624428127508653257226023513694322210869665811240855745025766026879447359920868907719574457253034494436336205824) % 1
     [ HU.testCase "Launch validity 0 0 == 0, (nominal actual)" $
         let zero = 0 % 1
         in launchValidity zero zero @?= zero
@@ -41,11 +54,17 @@ unitTests = testGroup "Unit tests"
             one = 1 % 1
         in launchValidity one zero @?= zero
 
+    -- WARNING: Failing test.
+    --  expected: 0 % 1
+    --   but got: 2751699372323373 % 562949953421312
     , HU.testCase "Launch validity 0 1 == 0, (nominal actual)" $
         let zero = 0 % 1
             one = 1 % 1
         in launchValidity zero one @?= zero
 
+    -- WARNING: Failing test.
+    --  expected: 1 % 1
+    --   but got: 2751699372323373 % 562949953421312
     , HU.testCase "Launch validity 1 1 == 1, (nominal actual)" $
         let one = 1 % 1
         in launchValidity one one @?= one
