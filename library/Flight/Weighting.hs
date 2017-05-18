7ca42e5f54571ebc34129f32b4104b2856e324cc
Phil de Joux <phil.dejoux@blockscope.com>
2017-05-19 07:39:21 +1200

    Stub weight functions.

--- library/Flight/Weighting.hs
+++ library/Flight/Weighting.hs
@@ -1,1 +1,17 @@
 module Flight.Weighting where
+
+type GoalRatio = Rational
+type DistanceWeight = Rational
+type LeadingWeight = Rational
+type ArrivalWeight = Rational
+
+data HgPg = Hg | Pg deriving (Eq, Show)
+
+distanceWeight :: GoalRatio -> DistanceWeight
+distanceWeight = undefined
+
+leadingWeight :: HgPg -> DistanceWeight -> LeadingWeight
+leadingWeight = undefined
+
+arrivalWeight :: HgPg -> DistanceWeight -> ArrivalWeight
+arrivalWeight = undefined
