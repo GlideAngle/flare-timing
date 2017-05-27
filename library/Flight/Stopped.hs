3d59b06c00227a9a7aec8bab8bd8e78b39561759
Phil de Joux <phil.dejoux@blockscope.com>
2017-05-28 02:07:51 +1200

    Add stopped validity.

--- library/Flight/Stopped.hs
+++ library/Flight/Stopped.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Flight.Stopped
@@ -12,9 +11,17 @@ module Flight.Stopped
     , CanScoreStopped(..)
     , stopTaskTime
     , canScoreStopped
+    , PilotsLaunched
+    , PilotsLandedBeforeStop
+    , DistanceLaunchToEss
+    , DistanceFlown
+    , StoppedValidity
+    , stoppedValidity
     ) where
 
 import Data.Ratio ((%))
+import Statistics.Sample (mean, stdDev)
+import qualified Data.Vector as V
 
 import Flight.Points (Hg, Pg)
 import Flight.Leading (TaskTime(..))
@@ -45,6 +52,8 @@ data CanScoreStopped a where
     FromGetGo :: TaskStopTime -> CanScoreStopped Pg
     FromLastStart :: [TaskTime] -> TaskStopTime -> CanScoreStopped Pg
 
+deriving instance Show (CanScoreStopped a)
+
 canScoreStopped :: forall a. CanScoreStopped a -> Bool
 canScoreStopped (Womens (NumberInGoalAtStop n) (TaskStopTime t)) =
     n > 0 || t > 60 * 60
@@ -58,3 +67,59 @@ canScoreStopped (FromLastStart xs (TaskStopTime t)) =
     (t - lastStart) > 60 * 60
     where
         lastStart = minimum $ (\(TaskTime x) -> x) <$> xs
+
+newtype PilotsMakingEss = PilotsMakingEss Int deriving (Eq, Ord, Show)
+newtype StoppedValidity = StoppedValidity Rational deriving (Eq, Show)
+newtype DistanceFlown = DistanceFlown Rational deriving (Eq, Show)
+newtype DistanceLaunchToEss = DistanceLaunchToEss Rational deriving (Eq, Show)
+newtype PilotsLandedBeforeStop = PilotsLandedBeforeStop Int deriving (Eq, Show)
+newtype PilotsLaunched = PilotsLaunched Int deriving (Eq, Show)
+
+stoppedValidity :: PilotsLaunched
+                   -> PilotsLandedBeforeStop
+                   -> DistanceLaunchToEss
+                   -> [DistanceFlown]
+                   -> StoppedValidity
+stoppedValidity _ _ _ [] =
+    StoppedValidity 0
+stoppedValidity
+    (PilotsLaunched launched)
+    (PilotsLandedBeforeStop landed)
+    (DistanceLaunchToEss dist)
+    xs =
+    StoppedValidity $ min 1 validity
+    where
+        validity :: Rational
+        validity = toRational $ (a * b) ** (1 / 2) + c ** 3
+
+        numerator :: Double
+        numerator = maxFlown - zsMean
+
+        denominator :: Double 
+        denominator = (fromRational dist) - maxFlown + 1
+
+        a :: Double 
+        a = numerator / denominator
+
+        b :: Double 
+        b = (zsStdDev / 5) ** (1 / 2)
+
+        c :: Double 
+        c = (fromIntegral landed / fromIntegral launched) ** (1 / 3)
+
+        ys :: [Rational]
+        ys = (\(DistanceFlown x) -> x) <$> xs
+
+        zs :: [Double]
+        zs = fromRational <$> ys
+
+        vs = V.fromList zs
+
+        zsMean :: Double
+        zsMean = mean vs
+
+        zsStdDev :: Double
+        zsStdDev = stdDev vs
+
+        maxFlown :: Double
+        maxFlown = fromRational $ maximum ys
