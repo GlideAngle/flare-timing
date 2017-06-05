12b750764a329e9f6d672cf960b17bf05c13e2bc
Phil de Joux <phil.dejoux@blockscope.com>
2017-06-05 12:39:44 +1200

    QC found a failing test for distance edge-to-edge.

--- test-suite-task/Task.hs
+++ test-suite-task/Task.hs
@@ -37,5 +37,10 @@ scProps = testGroup "(checked by SmallCheck)"
 qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)"
     [ QC.testProperty "Zone distances, point-to-point, are not negative" distancePointToPoint
+
+    -- WARNING: Failing test.
+    --        *** Failed! Exception: 'Prelude.head: empty list' (after 4 tests):
+    --        ZonesTest [Vector (Bearing ((-8128971062566) % 1890401950547)) (LatLng (17587866865261 % 1776959198642,5128428415813 % 6215300640898)),SemiCircle (Radius (5940919133137 % 3734234476148)) (LatLng (7344651197476 % 9602508952187,19192052829749 % 4604097196246)),Point (LatLng (15366472926857 % 5752281391201,(-11618437853167) % 6393280256761))]
+    --        Use --quickcheck-replay '3 TFGenR 00000004CC168D850000000000989680000000000000E2350001B90FF2BF9340 0 30 5 0' to reproduce.
     , QC.testProperty "Zone distances, edge-to-edge, are not negative" distanceEdgeToEdge
     ]
