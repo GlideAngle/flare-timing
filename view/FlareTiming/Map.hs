48922508a6bfe56ea3deb504a592e1b89a68237d
Phil de Joux <philderbeast@gmail.com>
2017-06-27 15:38:50 -0400

    Drop leaflet prefix. Remove unused imports.

--- view/FlareTiming/Map.hs
+++ view/FlareTiming/Map.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE KindSignatures #-}
 
 module FlareTiming.Map (map) where
 
 import Prelude hiding (map)
-import Control.Monad
-import Control.Applicative
-import Data.Aeson
-import GHC.Generics
 import Reflex.Dom
     ( MonadWidget
     , (=:)
@@ -20,29 +13,20 @@ import Reflex.Dom
     , _element_raw
     )
 import Reflex.Dom.Time (delay)
-import qualified Data.Text as T
-import qualified Data.Map as Map
-import Data.Map (Map)
-import Data.Scientific (Scientific, toRealFloat, fromRationalRepetend)
-import Data.Monoid
-import GHCJS.Types
-import GHCJS.Foreign
-import GHCJS.DOM.Element
-import GHCJS.DOM.Types
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (liftIO)
 
-import FlareTiming.Map.Leaflet
-    ( LeafletMap(..)
-    , LeafletTileLayer(..)
-    , LeafletMarker(..)
-    , leafletMap
-    , leafletMapSetView
-    , leafletTileLayer
-    , leafletTileLayerAddToMap
-    , leafletMarker
-    , leafletMarkerAddToMap
-    , leafletMarkerPopup
-    , leafletMapInvalidateSize
+import qualified FlareTiming.Map.Leaflet as L
+    ( Map(..)
+    , TileLayer(..)
+    , Marker(..)
+    , map
+    , mapSetView
+    , tileLayer
+    , tileLayerAddToMap
+    , marker
+    , markerAddToMap
+    , markerPopup
+    , mapInvalidateSize
     )
 
 attribution :: String
@@ -60,25 +44,21 @@ map :: MonadWidget t m => m ()
 map = do
     postBuild <- delay 0 =<< getPostBuild
     (e, _) <- elAttr' "div" ("style" =: "height: 600px;") $ return ()
-    rec performEvent_ $ fmap (\_ -> liftIO $ leafletMapInvalidateSize lmap) postBuild
+    rec performEvent_ $ fmap (\_ -> liftIO $ L.mapInvalidateSize lmap) postBuild
         lmap <- liftIO $ do
-            lmap <- leafletMap (_element_raw e)
-            leafletMapSetView lmap (-33.36137, 147.93207) 12
+            lmap <- L.map (_element_raw e)
+            L.mapSetView lmap (-33.36137, 147.93207) 12
             layer <-
                 -- SEE: http://leaflet-extras.github.io/leaflet-providers/preview/
-                leafletTileLayer
+                L.tileLayer
                     "http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
                     17
                     attribution
 
-            leafletTileLayerAddToMap layer lmap
-
-            mark <- leafletMarker (-33.36137, 147.9320)
-
-            leafletMarkerAddToMap mark lmap
-
-            leafletMarkerPopup mark "FORBES"
-
+            L.tileLayerAddToMap layer lmap
+            mark <- L.marker (-33.36137, 147.9320)
+            L.markerAddToMap mark lmap
+            L.markerPopup mark "FORBES"
             return lmap
 
     return ()
--- view/FlareTiming/Map/Leaflet.hs
+++ view/FlareTiming/Map/Leaflet.hs
@@ -1,140 +1,88 @@
-{-# LANGUAGE RecursiveDo #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE JavaScriptFFI #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module FlareTiming.Map.Leaflet
-    ( LeafletMap(..)
-    , LeafletTileLayer(..)
-    , LeafletMarker(..)
-    , leafletMap
-    , leafletMapSetView
-    , leafletTileLayer
-    , leafletTileLayerAddToMap
-    , leafletMarker
-    , leafletMarkerAddToMap
-    , leafletMarkerPopup
-    , leafletMapInvalidateSize
+    ( Map(..)
+    , TileLayer(..)
+    , Marker(..)
+    , map
+    , mapSetView
+    , tileLayer
+    , tileLayerAddToMap
+    , marker
+    , markerAddToMap
+    , markerPopup
+    , mapInvalidateSize
     ) where
 
 import Prelude hiding (map)
-import Control.Monad
-import Control.Applicative
-import Data.Aeson
-import GHC.Generics
-import Reflex.Dom
-    ( MonadWidget
-    , (=:)
-    , elAttr'
-    , getPostBuild
-    , performEvent_
-    , _element_raw
-    )
-import Reflex.Dom.Time (delay)
-import qualified Data.Text as T
-import qualified Data.Map as Map
-import Data.Map (Map)
-import Data.Scientific (Scientific, toRealFloat, fromRationalRepetend)
-import Data.Monoid
-import GHCJS.Types
-import GHCJS.Foreign
-import GHCJS.DOM.Element
-import GHCJS.DOM.Types
-import Control.Monad.IO.Class
+import GHCJS.Types (JSVal, JSString)
+import GHCJS.DOM.Element (IsElement(..))
+import GHCJS.DOM.Types (Element(..), toElement, toJSString)
 
 -- SEE: https://gist.github.com/ali-abrar/fa2adbbb7ee64a0295cb
-newtype LeafletMap =
-    LeafletMap { unLeafletMap :: JSVal }
-
-newtype LeafletTileLayer =
-    LeafletTileLayer { unLeafletTileLayer :: JSVal }
-
-newtype LeafletMarker =
-    LeafletMarker { unLeafletMarker :: JSVal }
+newtype Map = Map { unMap :: JSVal }
+newtype TileLayer = TileLayer { unTileLayer :: JSVal }
+newtype Marker = Marker { unMarker :: JSVal }
 
 foreign import javascript unsafe
     "L['map']($1)"
-    leafletMap_ :: JSVal
-                -> IO JSVal
+    map_ :: JSVal -> IO JSVal
 
 foreign import javascript unsafe
     "$1['setView']([$2, $3], $4)"
-    leafletMapSetView_ :: JSVal
-                       -> Double
-                       -> Double
-                       -> Int
-                       -> IO ()
+    mapSetView_ :: JSVal -> Double -> Double -> Int -> IO ()
 
 foreign import javascript unsafe
     "L['marker']([$1, $2])"
-    leafletMarker_ :: Double
-                   -> Double
-                   -> IO JSVal
+    marker_ :: Double -> Double -> IO JSVal
 
 foreign import javascript unsafe
     "L['tileLayer']($1, { maxZoom: $2, attribution: $3})"
-    leafletTileLayer_ :: JSString
-                      -> Int
-                      -> JSString
-                      -> IO JSVal
+    tileLayer_ :: JSString -> Int -> JSString -> IO JSVal 
 
 foreign import javascript unsafe
     "$1['addTo']($2)"
-    leafletTileLayerAddToMap_ :: JSVal
-                              -> JSVal
-                              -> IO ()
+    tileLayerAddToMap_ :: JSVal -> JSVal -> IO ()
 
 foreign import javascript unsafe
     "$1['addTo']($2)"
-    leafletMarkerAddToMap_ :: JSVal
-                           -> JSVal
-                           -> IO ()
+    markerAddToMap_ :: JSVal -> JSVal -> IO ()
 
 foreign import javascript unsafe
     "$1.invalidateSize()"
-    leafletMapInvalidateSize_ :: JSVal
-                              -> IO ()
+    mapInvalidateSize_ :: JSVal -> IO () 
 
 foreign import javascript unsafe
     "$1.bindPopup($2).openPopup()"
-    leafletMarkerPopup_ :: JSVal
-                        -> JSString
-                        -> IO ()
+    markerPopup_ :: JSVal -> JSString -> IO ()
 
-leafletMap :: IsElement e => e -> IO LeafletMap
-leafletMap e = do
-    lmap <- leafletMap_ $ unElement $ toElement e
-    return $ LeafletMap lmap
+map :: IsElement e => e -> IO Map
+map e = do
+    lmap <- map_ $ unElement $ toElement e
+    return $ Map lmap
 
-leafletMapSetView :: LeafletMap -> (Double, Double) -> Int -> IO ()
-leafletMapSetView lm (lat, lng) zoom =
-    leafletMapSetView_ (unLeafletMap lm) lat lng zoom
+mapSetView :: Map -> (Double, Double) -> Int -> IO ()
+mapSetView lm (lat, lng) zoom = mapSetView_ (unMap lm) lat lng zoom
 
-leafletMarker :: (Double, Double) -> IO LeafletMarker
-leafletMarker (lat, lng) = do
-    marker <- leafletMarker_ lat lng
-    return $ LeafletMarker marker
+marker :: (Double, Double) -> IO Marker
+marker (lat, lng) = do
+    marker <- marker_ lat lng
+    return $ Marker marker
 
-leafletTileLayer :: String -> Int -> String -> IO LeafletTileLayer
-leafletTileLayer src maxZoom attribution = do
-    layer <- leafletTileLayer_ (toJSString src) maxZoom (toJSString attribution)
-    return $ LeafletTileLayer layer
+tileLayer :: String -> Int -> String -> IO TileLayer
+tileLayer src maxZoom attribution = do
+    layer <- tileLayer_ (toJSString src) maxZoom (toJSString attribution)
+    return $ TileLayer layer
 
-leafletMarkerAddToMap :: LeafletMarker -> LeafletMap -> IO ()
-leafletMarkerAddToMap marker lmap =
-    leafletMarkerAddToMap_ (unLeafletMarker marker) (unLeafletMap lmap)
+markerAddToMap :: Marker -> Map -> IO ()
+markerAddToMap marker lmap = markerAddToMap_ (unMarker marker) (unMap lmap)
 
-leafletTileLayerAddToMap :: LeafletTileLayer -> LeafletMap -> IO ()
-leafletTileLayerAddToMap layer lmap = 
-    leafletTileLayerAddToMap_ (unLeafletTileLayer layer) (unLeafletMap lmap)
+tileLayerAddToMap :: TileLayer -> Map -> IO ()
+tileLayerAddToMap layer lmap = tileLayerAddToMap_ (unTileLayer layer) (unMap lmap)
 
-leafletMarkerPopup :: LeafletMarker -> String -> IO ()
-leafletMarkerPopup marker msg =
-    leafletMarkerPopup_ (unLeafletMarker marker) (toJSString msg)
+markerPopup :: Marker -> String -> IO ()
+markerPopup marker msg = markerPopup_ (unMarker marker) (toJSString msg)
 
-leafletMapInvalidateSize :: LeafletMap -> IO ()
-leafletMapInvalidateSize lmap =
-    leafletMapInvalidateSize_ (unLeafletMap lmap)
+mapInvalidateSize :: Map -> IO ()
+mapInvalidateSize lmap = mapInvalidateSize_ (unMap lmap)
