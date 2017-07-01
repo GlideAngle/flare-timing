9f7e10f8e08087812bfba65350c8a7a598981c69
Phil de Joux <philderbeast@gmail.com>
2017-07-01 16:43:47 -0400

    Turnpoint getName & getNameRadius.

--- view/FlareTiming/Turnpoint.hs
+++ view/FlareTiming/Turnpoint.hs
@@ -3,20 +3,23 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE KindSignatures #-}
 
-module FlareTiming.Turnpoint (liTurnpointRadius) where
+module FlareTiming.Turnpoint (turnpointRadius, turnpoint) where
 
 import Reflex.Dom (MonadWidget , Dynamic , el , dynText)
 import qualified Data.Text as T (Text, pack)
 
 import FlareTiming.WireTypes (Turnpoint(..), showRadius)
 
-liTurnpointRadius :: forall t (m :: * -> *).
-             MonadWidget t m =>
-             Dynamic t Turnpoint -> m ()
-liTurnpointRadius x = do
-    let dyTp :: Dynamic t T.Text =
-            fmap (\(Turnpoint name _ _ radius) ->
-                T.pack $ name ++ " " ++ showRadius radius) x
+getNameRadius :: Turnpoint -> T.Text
+getNameRadius (Turnpoint name _ _ radius) = T.pack $ name ++ " " ++ showRadius radius
 
-    el "li" $ do
-        dynText dyTp
+getName :: Turnpoint -> T.Text
+getName (Turnpoint name _ _ _) = T.pack name
+
+turnpointRadius :: forall t (m :: * -> *). MonadWidget t m => Dynamic t Turnpoint -> m ()
+turnpointRadius x = do
+    dynText $ fmap getNameRadius x
+
+turnpoint :: forall t (m :: * -> *). MonadWidget t m => Dynamic t Turnpoint -> m ()
+turnpoint x = do
+    dynText $ fmap getName x
