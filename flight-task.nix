5f2cf573df6a897699d1eeb69a13ce6bdb71ea1b
Phil de Joux <philderbeast@gmail.com>
2017-07-05 15:10:46 -0400

    Prep *.nix for nix builds, dontCheck.

---
+++ flight-task.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, base, fgl, hlint, numbers, smallcheck, stdenv
+, tasty, tasty-hunit, tasty-quickcheck, tasty-smallcheck
+}:
+
+mkDerivation {
+  pname = "flight-task";
+  version = "0.1.0";
+  src = ./.;
+  libraryHaskellDepends = [ base fgl numbers ];
+  testHaskellDepends = [
+    base fgl hlint numbers smallcheck tasty tasty-hunit
+    tasty-quickcheck tasty-smallcheck
+  ];
+  homepage = "https://github.com/BlockScope/haskell-flight-task#readme";
+  description = "Tasks to fly";
+  license = stdenv.lib.licenses.bsd3;
+}
