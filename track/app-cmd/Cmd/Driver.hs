0f1a527ef56047106303b7bb72c03f5d2cf66120
Phil de Joux <philderbeast@gmail.com>
2017-07-07 17:21:28 -0400

    Show if tracklog exists.

--- track/app-cmd/Cmd/Driver.hs
+++ track/app-cmd/Cmd/Driver.hs
@@ -6,13 +6,13 @@ module Cmd.Driver (driverMain) where
 import Data.Maybe (catMaybes)
 import Control.Monad (mapM_)
 import System.Directory (doesFileExist, doesDirectoryExist)
-import System.FilePath (takeFileName)
+import System.FilePath (FilePath, (</>), takeFileName, takeDirectory)
 import System.FilePath.Find (FileType(..), (==?), (&&?), find, always, fileType, extension)
 
 import Cmd.Args (withCmdArgs)
 import Cmd.Options (CmdOptions(..), Reckon(..))
 import Data.Flight.Pilot
-    ( Pilot(..), PilotTrackLogFile(..), parseTracks)
+    ( Pilot(..), PilotTrackLogFile(..), TrackLogFile(..), parseTracks)
 
 type Task = Int
 
@@ -38,22 +38,43 @@ drive CmdOptions{..} = do
             let contents' = dropWhile (/= '<') contents
 
             case reckon of
-                Goal -> printMadeGoal task (Pilot <$> pilot) contents'
-                x -> putStrLn $ "TODO: Handle other reckon of " ++ show x
+                Goal ->
+                    printMadeGoal
+                        (takeDirectory path)
+                        task
+                        (Pilot <$> pilot)
+                        contents'
 
-showTaskPilotTracks :: [ (Int, [ PilotTrackLogFile ]) ] -> [ String ]
-showTaskPilotTracks [] = [ "No tasks." ]
-showTaskPilotTracks xs =
-    (\(i, pilotTracks) -> "Task #" ++ show i ++ " pilot tracks: " ++ show pilotTracks) <$> xs
+                x ->
+                    putStrLn $ "TODO: Handle other reckon of " ++ show x
 
-showPilotTracks :: [[ PilotTrackLogFile ]] -> String
-showPilotTracks [] = "No pilots."
-showPilotTracks tasks =
-    unlines $ showTaskPilotTracks (zip [ 1 .. ] tasks) 
+goalPilotTrack :: PilotTrackLogFile -> IO (Pilot, Bool)
+goalPilotTrack (PilotTrackLogFile p Nothing) = return (p, False)
+goalPilotTrack (PilotTrackLogFile p (Just (TrackLogFile file))) = do
+    dfe <- doesFileExist file
+    return (p, dfe)
+
+goalTaskPilotTracks :: [ (Int, [ PilotTrackLogFile ]) ] -> IO [ String ]
+goalTaskPilotTracks [] = return [ "No tasks." ]
+goalTaskPilotTracks xs = do
+    zs <- sequence $ (\(i, pilotTracks) -> do
+                ys <- sequence $ goalPilotTrack <$> pilotTracks
+                return $ "Task #"
+                         ++ show i
+                         ++ " pilot tracks: "
+                         ++ (unlines $ show <$> ys))
+                <$> xs
+    return $ zs
+
+goalPilotTracks :: [[ PilotTrackLogFile ]] -> IO String
+goalPilotTracks [] = return "No pilots."
+goalPilotTracks tasks = do
+    xs <- goalTaskPilotTracks (zip [ 1 .. ] tasks) 
+    return $ unlines xs
 
 filterPilots :: [ Pilot ]
              -> [[ PilotTrackLogFile ]]
-             ->[[ PilotTrackLogFile ]]
+             -> [[ PilotTrackLogFile ]]
 
 filterPilots [] xs = xs
 filterPilots pilots xs =
@@ -68,20 +89,29 @@ filterPilots pilots xs =
 
 filterTasks :: [ Task ]
             -> [[ PilotTrackLogFile ]]
-            ->[[ PilotTrackLogFile ]]
+            -> [[ PilotTrackLogFile ]]
 
 filterTasks [] xs = xs
 filterTasks tasks xs =
     zipWith (\i ys ->
         if i `elem` tasks then ys else []) [ 1 .. ] xs
 
-printMadeGoal :: [ Task ] -> [ Pilot ] -> String -> IO ()
-printMadeGoal tasks pilots contents = do
+makeAbsolute :: FilePath -> Task -> PilotTrackLogFile -> PilotTrackLogFile
+makeAbsolute _ _ x@(PilotTrackLogFile _ Nothing) = x
+makeAbsolute dir task (PilotTrackLogFile p (Just (TrackLogFile file))) =
+    PilotTrackLogFile p (Just (TrackLogFile file'))
+    where
+        -- TODO: Get the track log folder for each task.
+        file' = dir </> "Tracklogs" </> "day " ++ show task </> file
+
+printMadeGoal :: FilePath -> [ Task ] -> [ Pilot ] -> String -> IO ()
+printMadeGoal dir tasks pilots contents = do
     xs <- parseTracks contents
     case xs of
          Left msg -> print msg
-         Right xs' ->
-             putStr
-             $ showPilotTracks 
-             $ filterPilots pilots
-             $ filterTasks tasks xs'
+         Right xs' -> do
+             let ys = filterPilots pilots $ filterTasks tasks xs'
+             let fs = (makeAbsolute dir) <$> [ 1 .. length ys ]
+             let zs = zipWith (\f y -> f <$> y) fs ys
+             s <- goalPilotTracks zs
+             putStr s
