76535b10bfb72f63eab7eaae819bd065d34d0b98
Phil de Joux <philderbeast@gmail.com>
2017-07-30 18:11:41 -0400

    Update nix dependencies.

--- flight-task.nix
+++ flight-task.nix
@@ -1,15 +1,17 @@
-{ mkDerivation, base, fgl, hlint, numbers, siggy-chardust
-, smallcheck, stdenv, tasty, tasty-hunit, tasty-quickcheck
-, tasty-smallcheck
+{ mkDerivation, base, bifunctors, fgl, hlint, numbers
+, siggy-chardust, smallcheck, stdenv, tasty, tasty-hunit
+, tasty-quickcheck, tasty-smallcheck, uom-plugin
 }:
 mkDerivation {
   pname = "flight-task";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [ base fgl numbers ];
+  libraryHaskellDepends = [
+    base bifunctors fgl numbers siggy-chardust uom-plugin
+  ];
   testHaskellDepends = [
-    base fgl hlint numbers siggy-chardust smallcheck tasty tasty-hunit
-    tasty-quickcheck tasty-smallcheck
+    base bifunctors fgl hlint numbers siggy-chardust smallcheck tasty
+    tasty-hunit tasty-quickcheck tasty-smallcheck uom-plugin
   ];
   homepage = "https://github.com/BlockScope/haskell-flight-task#readme";
   description = "Tasks to fly";
