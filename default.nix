43212243f60c0f488c252316b0117e5b9e5e858a
Phil de Joux <philderbeast@gmail.com>
2017-07-31 10:57:47 -0400

    Reference the haddock issue with the problem.

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ with nixpkgs.pkgs.haskell.packages.${compiler};
 
 let hlib = nixpkgs.pkgs.haskell.lib;
 in
+# NOTE: Add dontHaddock to workaround https://github.com/haskell/haddock/issues/233
 hlib.dontCheck(hlib.dontHaddock(callPackage ./flight-task.nix {
   fgl = fgl;
   siggy-chardust = siggy-chardust;
