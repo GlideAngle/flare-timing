113bcf4ef36b690b817b49acf8a7ad27c78d402d
Phil de Joux <philderbeast@gmail.com>
2017-08-17 16:22:55 -0400

    Add recent dependency additions to nix build.

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802", flight-comp, flight-fsdb, flight-kml, flight-igc }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802", siggy-chardust, flight-comp, flight-fsdb, flight-kml, flight-igc, flight-gap, flight-task, flight-track }:
 
 with nixpkgs.pkgs.haskell.packages.${compiler};
 
 let hlib = nixpkgs.pkgs.haskell.lib;
 in
 hlib.dontCheck(callPackage ./flare-timing.nix {
+  siggy-chardust = siggy-chardust;
   flight-comp = flight-comp;
   flight-fsdb = flight-fsdb;
   flight-kml = flight-kml;
   flight-igc = flight-igc;
+  flight-gap = flight-gap;
+  flight-task= flight-task;
+  flight-track = flight-track;
 })
