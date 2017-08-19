d237dbfd617a13f193520a5118321b41dde8ce33
Phil de Joux <philderbeast@gmail.com>
2017-08-19 08:17:27 -0400

    Move units to their own package.

---
+++ flight-units.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, base, bifunctors, hlint, numbers, siggy-chardust
+, stdenv, uom-plugin
+}:
+mkDerivation {
+  pname = "flight-units";
+  version = "0.1.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base bifunctors numbers siggy-chardust uom-plugin
+  ];
+  testHaskellDepends = [
+    base bifunctors hlint numbers siggy-chardust uom-plugin
+  ];
+  homepage = "https://github.com/BlockScope/haskell-flight-task#readme";
+  description = "Units used in hang gliding and paragliding competitions";
+  license = stdenv.lib.licenses.bsd3;
+}
---
+++ library/Flight/Units.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Flight.Units
+    ( Length
+    , abs
+    , toRational'
+    , showRadian
+    ) where
+
+import Data.UnitsOfMeasure (u, convert, fromRational')
+import Data.UnitsOfMeasure.Internal (Quantity(..))
+import Data.Bifunctor.Flip (Flip(..))
+import Data.Number.RoundingFunctions (dpRound)
+
+type Length u = Quantity Rational u
+
+[u| s, m |]
+
+[u| km = 1000 m, min = 60 s, h = 3600 s, d = 86400 s |]
+
+[u| ft = 100 % 328 m, mi = 1609.344 m, mph = mi/h |]
+
+[u| rad |]
+[u| deg = (5030569068109113 % 288230376151711744) rad |]
+
+-- | Convert any 'Real' quantity into a 'Rational' type ('toRational').
+toRational' :: Real a => Quantity a u -> Quantity Rational u
+toRational' (MkQuantity x) = MkQuantity (toRational x)
+
+instance Functor (Flip Quantity u) where
+    fmap = map'
+
+map' :: (a -> b) -> Flip Quantity u a -> Flip Quantity u b
+map' f (Flip (MkQuantity x)) = Flip $ MkQuantity $ f x
+
+showRadian :: Quantity Rational [u| rad |] -> String
+showRadian b = show dbl
+    where
+        deg = convert b :: Quantity Rational [u| deg |]
+        Flip rounded = dpRound 3 <$> Flip deg
+        MkQuantity dbl = fromRational' rounded :: Quantity Double [u| deg |]
