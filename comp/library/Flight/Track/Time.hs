460f586a47d5f537eb9e82b0630d56ba42a38779
Phil de Joux <philderbeast@gmail.com>
2017-10-22 13:23:37 -0400

    Move cassava ToNamedRecord instance to Comp module.

--- comp/library/Flight/Track/Time.hs
+++ comp/library/Flight/Track/Time.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 {-|
 Module      : Flight.Track.Time
@@ -11,9 +13,13 @@ Tracks aligned in time based on when the first pilot starts the speed section.
 -}
 module Flight.Track.Time (TimeRow(..)) where
 
+import Data.Csv (ToNamedRecord(..), namedRecord, namedField)
+import Data.List.Split (wordsBy)
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.HashMap.Strict (unions)
 import Data.Time.Clock (UTCTime)
 import GHC.Generics (Generic)
-import Data.Aeson (ToJSON(..), FromJSON(..))
+import Data.Aeson (ToJSON(..), FromJSON(..), encode)
 import Flight.Pilot (Pilot(..))
 import Flight.LatLng.Raw (RawLat, RawLng)
 
@@ -30,3 +36,25 @@ data TimeRow =
 
 instance ToJSON TimeRow
 instance FromJSON TimeRow
+
+unquote :: String -> String
+unquote s =
+    case wordsBy (== '"') s of
+        [x] -> x
+        _ -> s
+
+instance ToNamedRecord TimeRow where
+    toNamedRecord TimeRow{..} =
+        unions [local, toNamedRecord lat, toNamedRecord lng]
+        where
+            local =
+                namedRecord
+                    [ namedField "time" t
+                    , namedField "pilot" p
+                    , namedField "distance" d
+                    ]
+
+
+            t = unquote . unpack . encode $ time
+            d = unquote . unpack . encode $ distance
+            Pilot p = pilot
--- flare-timing/prod-apps/align-time/Cmd/Outputs.hs
+++ flare-timing/prod-apps/align-time/Cmd/Outputs.hs
@@ -4,46 +4,11 @@
 
 module Cmd.Outputs (writeTimeRowsToCsv) where
 
-import Data.Csv
-    ( EncodeOptions(..)
-    , ToNamedRecord(..)
-    , namedRecord
-    , namedField
-    , encodeByNameWith
-    , defaultEncodeOptions
-    )
-import Data.List.Split (wordsBy)
+import Data.Csv (EncodeOptions(..), encodeByNameWith, defaultEncodeOptions)
 import qualified Data.ByteString.Char8 as S (pack)
-import qualified Data.ByteString.Lazy.Char8 as L (writeFile, unpack)
+import qualified Data.ByteString.Lazy.Char8 as L (writeFile)
 import qualified Data.Vector as V (fromList)
-import Data.HashMap.Strict (unions)
-import Flight.Comp (Pilot(..))
 import Flight.Track.Time (TimeRow(..))
-import Data.Aeson (encode)
-
-newtype Row = Row TimeRow
-
-unquote :: String -> String
-unquote s =
-    case wordsBy (== '"') s of
-        [x] -> x
-        _ -> s
-
-instance ToNamedRecord Row where
-    toNamedRecord (Row TimeRow{..}) =
-        unions [local, toNamedRecord lat, toNamedRecord lng]
-        where
-            local =
-                namedRecord
-                    [ namedField "time" t
-                    , namedField "pilot" p
-                    , namedField "distance" d
-                    ]
-
-
-            t = unquote . L.unpack . encode $ time
-            d = unquote . L.unpack . encode $ distance
-            Pilot p = pilot
 
 writeTimeRowsToCsv :: FilePath -> [String] -> [TimeRow] -> IO ()
 writeTimeRowsToCsv filename headers xs =
@@ -51,4 +16,4 @@ writeTimeRowsToCsv filename headers xs =
     where
         opts = defaultEncodeOptions {encUseCrLf = False}
         hs = V.fromList $ S.pack <$> headers
-        rows = encodeByNameWith opts hs $ Row <$> xs
+        rows = encodeByNameWith opts hs xs
