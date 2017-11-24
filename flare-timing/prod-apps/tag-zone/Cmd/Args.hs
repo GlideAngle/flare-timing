7dcd259e106dfb01a5c985ed4f8f3c7050f15c07
Phil de Joux <philderbeast@gmail.com>
2017-11-24 13:24:36 -0500

    Update the help & readme for tag-zone.

--- flare-timing/prod-apps/tag-zone/Cmd/Args.hs
+++ flare-timing/prod-apps/tag-zone/Cmd/Args.hs
@@ -30,11 +30,16 @@ import Flight.TaskTrack (TaskDistanceMeasure(..))
 
 description :: String
 description = [r|
-Interpolate between crossing fixes '.cross-zone.yaml' for the time and place where a track tags a zone.
+For each crossing, given as a pair of fixes, interpolates the time and place
+where it touches the control zone.
 
-If a list of tasks are supplied then those tasks alone are processed, otherwise all tasks are processed.
+Where 'c' is the comp name and '.' is the folder with competition inputs;
+    Reads  ./c.cross-zone.yaml
+    Writes ./c.tag-zone.yaml
 
-The same thing goes if a list of pilots is supplied or not.
+If a list of tasks are supplied then those tasks alone are processed, otherwise
+all tasks are processed. The same thing goes if a list of pilots is supplied or
+not.
 |]
 
 data Drive
--- flare-timing/prod-apps/tag-zone/README.md
+++ flare-timing/prod-apps/tag-zone/README.md
@@ -2,13 +2,16 @@
 
     $ __shake-build/tag-zone --help
 
-    Interpolate between crossing fixes '.cross-zone.yaml' for the time and place
-    where a track tags a zone.
+    For each crossing, given as a pair of fixes, interpolates the time and place
+    where it touches the control zone.
+
+    Where 'c' is the comp name and '.' is the folder with competition inputs;
+        Reads  ./c.cross-zone.yaml
+        Writes ./c.tag-zone.yaml
 
     If a list of tasks are supplied then those tasks alone are processed, otherwise
-    all tasks are processed.
-
-    The same thing goes if a list of pilots is supplied or not.
+    all tasks are processed. The same thing goes if a list of pilots is supplied or
+    not.
 
     tag-zone [OPTIONS]
 
