fb2df88182eb8a96b66aa1c9de584d9bc580266d
Phil de Joux <philderbeast@gmail.com>
2017-11-24 11:52:22 -0500

    Update the help and readme for align-time.

--- flare-timing/prod-apps/align-time/Cmd/Args.hs
+++ flare-timing/prod-apps/align-time/Cmd/Args.hs
@@ -31,10 +31,25 @@ description :: String
 description = intro
     where
         intro = [r|
-From competition inputs '.comp-input.yaml' and relative track logs '.kml',
-writes the distance to goal for every track log fix. These are collected in
-a single '.csv' per-pilot per-task. These are grouped into a single folder
-per-task as ./flare-timing/task-n/_.csv.
+From competition inputs '.comp-input.yaml', relative track logs '.kml' and
+tagged zones '.tag-zone.yaml', writes each fix to single '.csv' per-pilot
+per-task. These are grouped into a single folder per-task as
+./flare-timing/task-n/_.csv.
+
+Using the tagged zones to find the first crossing of a pilot into the speed
+section, three columns are added to the CSV;
+    leg        The leg of the speed section of the task
+    tick       The time in seconds from the first crossing
+    distance   The distance to goal
+
+Where 'c' is the comp name, 'p' is the pilot name, '.' is the folder with
+competition inputs and k is a folder path specified in the inputs for
+tracklogs, one per task;
+    Reads  ./c.comp-input.yaml
+    Reads  ./c.tag-zone.yaml
+    Reads  ./k/p.kml
+    Writes ./flare-timing/align-time/task-n/p.csv
+
 
 If a list of tasks are supplied then those tasks alone are processed, otherwise
 all tasks are processed. The same thing goes if a list of pilots is supplied or
