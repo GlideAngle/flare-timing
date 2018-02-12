7eae65371186d4fe6d2972324d5abaddca8757e9
Phil de Joux <philderbeast@gmail.com>
2018-02-12 15:11:01 -0500

    Remove small check tests, they take too long, #7.

--- earth-ellipsoid/test-suite-ellipsoid/Ellipsoid.hs
+++ earth-ellipsoid/test-suite-ellipsoid/Ellipsoid.hs
@@ -1,7 +1,6 @@
 module Ellipsoid (main) where
 
 import Test.Tasty (TestTree, testGroup, defaultMain)
-import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC
 
 import General
@@ -18,8 +17,7 @@ tests = testGroup "Tests"
 
 properties :: TestTree
 properties = testGroup "Properties"
-        [ scProps
-        , qcProps
+        [ qcProps
         ]
 
 units :: TestTree
@@ -27,14 +25,6 @@ units = testGroup "Units"
         [ zoneUnits
         , specificUnits
         ]
-
-scProps :: TestTree
-scProps = testGroup "(checked by SmallCheck)"
-    [ SC.testProperty "VincentyF distances, are not negative" distanceVincentyF
-    , SC.testProperty "Vincenty distances, are not negative" distanceVincenty
-    , SC.testProperty "Zone distances, point-to-point, are not negative" distancePoint
-    , SC.testProperty "Zone distances, point-to-point, are not negative" distancePoint
-    ]
 
 qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)"
