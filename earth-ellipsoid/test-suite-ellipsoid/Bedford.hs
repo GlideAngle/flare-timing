f77b100324682429c5e224472fc82b8f5ff9f2cc
Phil de Joux <philderbeast@gmail.com>
2018-02-14 11:02:35 -0500

    Be careful of sign, converting DMS to decimal degrees, #7.

--- earth-ellipsoid/test-suite-ellipsoid/Bedford.hs
+++ earth-ellipsoid/test-suite-ellipsoid/Bedford.hs
@@ -41,14 +41,19 @@ instance Show DMS where
     show = showDMS
 
 showDMS :: DMS -> String
+showDMS (DMS (deg, 0, 0)) =
+    show deg ++ "°"
 showDMS (DMS (deg, min, 0)) =
     show deg ++ "°" ++ show min ++ "'"
 showDMS (DMS (deg, min, sec)) =
     show deg ++ "°" ++ show min ++ "'" ++ show sec ++ "''"
 
 toDeg :: DMS -> Double
-toDeg (DMS (deg, min, sec)) =
-    fromIntegral deg + (fromIntegral min / 60) + (sec / 3600)
+toDeg (DMS (deg, min, s)) =
+    signum d * (abs d + m / 60 + s / 3600)
+        where
+            d = fromIntegral deg
+            m = fromIntegral min 
 
 points :: [((DMS, DMS), (DMS, DMS))]
 points =
