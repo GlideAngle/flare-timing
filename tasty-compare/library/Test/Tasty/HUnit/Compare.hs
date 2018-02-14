8fe0ca5cba4fba75fb9f8e6ff0121d9b630b6409
Phil de Joux <philderbeast@gmail.com>
2018-02-14 12:33:46 -0500

    Remove Enum constraint from assertCompare, passing symbol #7.

--- tasty-compare/library/Test/Tasty/HUnit/Compare.hs
+++ tasty-compare/library/Test/Tasty/HUnit/Compare.hs
@@ -12,81 +12,74 @@ import Data.CallStack
 import Test.Tasty.HUnit
 
 assertCompare
-  :: (Eq a, Ord a, Enum a, Show a, HasCallStack)
+  :: (Eq a, Ord a, Show a, HasCallStack)
   => String -- ^ The message prefix
   -> (a -> a -> Bool) -- ^ The comparison
-  -> a      -- ^ The expected value
-  -> a      -- ^ The actual value
+  -> String -- ^ A symbol for the comparison
+  -> a -- ^ The expected value
+  -> a -- ^ The actual value
   -> Assertion
-assertCompare preface compare key actual =
+assertCompare preface compare cmpSymbol key actual =
     unless (actual `compare` key) (assertFailure msg)
     where
-        cmp
-            | key `compare` key =
-              if succ key `compare` key then " >= " else
-                if pred key `compare` key then " <= " else " == "
-            | succ key `compare` key = " > "
-            | pred key `compare` key = " < "
-            | otherwise = " == "
-
         msg =
             (if null preface then "" else preface ++ "\n") ++
-            "expected: " ++ show actual ++ cmp ++ show key
+            "expected: " ++ show actual ++ cmpSymbol ++ show key
 
 infix 1 @<?, @?<, @<=?, @?<=, @>?, @?>, @>=?, @?>=
 
 (@<?)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The key value
     -> a -- ^ The actual value
     -> Assertion
-key @<? actual = assertCompare "" (>) key actual
+key @<? actual = assertCompare "" (>) ">" key actual
 
 (@?<)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The actual value
     -> a -- ^ The key value
     -> Assertion
-actual @?< key = assertCompare "" (<) key actual
+actual @?< key = assertCompare "" (<) "<" key actual
 
 (@<=?)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The key value
     -> a -- ^ The actual value
     -> Assertion
-key @<=? actual = assertCompare "" (>=) key actual
+key @<=? actual = assertCompare "" (>=) ">=" key actual
 
 (@?<=)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The actual value
     -> a -- ^ The key value
     -> Assertion
-actual @?<= key = assertCompare "" (<=) key actual
+actual @?<= key = assertCompare "" (<=) "<=" key actual
 
 (@>?)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The key value
     -> a -- ^ The actual value
     -> Assertion
-key @>? actual = assertCompare "" (<=) key actual
+key @>? actual = assertCompare "" (<=) "<=" key actual
 
 (@?>)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The actual value
     -> a -- ^ The key value
     -> Assertion
-actual @?> key = assertCompare "" (>) key actual
+actual @?> key = assertCompare "" (>) ">" key actual
 
 (@>=?)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The key value
     -> a -- ^ The actual value
     -> Assertion
-key @>=? actual = assertCompare "" (<) key actual
+key @>=? actual = assertCompare "" (<) "<" key actual
 
 (@?>=)
-    :: (HasCallStack, Eq a, Ord a, Enum a, Show a)
+    :: (HasCallStack, Eq a, Ord a, Show a)
     => a -- ^ The actual value
     -> a -- ^ The key value
     -> Assertion
-actual @?>= key = assertCompare "" (>=) key actual
+actual @?>= key = assertCompare "" (>=) ">=" key actual
