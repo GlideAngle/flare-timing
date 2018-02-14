22ab28f6000dbf86a0227f611598e9fc271b2cde
Phil de Joux <philderbeast@gmail.com>
2018-02-14 08:39:37 -0500

    Refactor only, for brevity & for comparing double vs rational, #7.

--- earth-ellipsoid/library/Flight/Ellipsoid/PointToPoint/Double.hs
+++ earth-ellipsoid/library/Flight/Ellipsoid/PointToPoint/Double.hs
@@ -68,66 +68,47 @@ vincentyInverse
     -> a
     -> VincentyInverse a
 vincentyInverse
-    ellipsoid@Ellipsoid{semiMajor, semiMinor}
+    ellipsoid@Ellipsoid{semiMajor = MkQuantity a, semiMinor = MkQuantity b}
     accuracy@(VincentyAccuracy tolerance)
     λ _L _U1 _U2 =
     if abs λ > pi
         then VincentyAntipodal
         else
             if abs (λ - λ') < tolerance
-                then VincentyInverse s
+                then VincentyInverse $ b * _A * (σ - _Δσ)
                 else vincentyInverse ellipsoid accuracy λ' _L _U1 _U2
     where
         f = flattening ellipsoid
+        (sinU1, sinU2, sinλ) = (sin _U1, sin _U2, sin λ)
+        (cosU1, cosU2, cosλ) = (cos _U1, cos _U2, cos λ)
 
-        i = cos _U2 * sin λ
-        j = cos _U1 * sin _U2 - sin _U1 * cos _U2 * cos λ
-        i² = i * i 
-        j² = j * j
-        sinσ = sqrt $ i² + j²
-        cosσ = sin _U1 * sin _U2 + cos _U1 * cos _U2 * cos λ
+        i = cosU2 * sinλ
+        j = cosU1 * sinU2 - sinU1 * cosU2 * cosλ
+        sin²σ = i * i + j * j
+        sinσ = sqrt sin²σ
+        cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+
         σ = atan2 sinσ cosσ
-        sinα = cos _U1 * cos _U2 * sin λ / sin σ
-        sin²α = sinα * sinα 
-        cos²α = 1 - sin²α 
 
-        cos2σm =
-            if cos²α == 0
-                then
-                    -- NOTE: Start and end points on the equator, _C = 0.
-                    0
-                else
-                    cosσ - 2 * sin _U1 * sin _U2 / cos²α
+        sinα = cosU1 * cosU2 * sinλ / sinσ
+        cos²α = 1 - sinα * sinα
+
+        -- NOTE: Start and end points on the equator, _C = 0.
+        cos2σm = if cos²α == 0 then 0 else cosσ - 2 * sinU1 * sinU2 / cos²α
 
         cos²2σm = cos2σm * cos2σm
         _C = f / 16 * cos²α * (4 + f * (4 - 3 * cos²α))
-        x = σ + _C * sinσ * y
-        y = cos2σm + _C * cosσ * (negate 1 + 2 * cos²2σm)
-        λ' = _L + (1 - _C) * f * sinα * x
+        λ' = _L + (1 - _C) * f * sinα * (σ + _C * sinσ * x)
+        x = cos2σm + _C * cosσ * (-1 + 2 * cos²2σm)
 
-        MkQuantity a = semiMajor
-        MkQuantity b = semiMinor
-        a² = a * a
-        b² = b * b
-        u² = cos²α * (a² - b²) / b² 
-        _A = 1 + u² / 16384 * (4096 + u² * (negate 768 + u² * (320 - 175 * u²)))
-        _B = u² / 1024 * (256 + u² * (negate 128 + u² * (74 - 47 * u²)))
-        sin²σ = sinσ * sinσ
+        u² = let b² = b * b in cos²α * (a * a - b²) / b² 
+        _A = 1 + u² / 16384 * (4096 + u² * (-768 + u² * (320 - 175 * u²)))
+        _B = u² / 1024 * (256 + u² * (-128 + u² * (74 - 47 * u²)))
 
-        _Δσ =
-            _B * sinσ *
-                (cos2σm
-                    + _B / 4
-                    *
-                        (cosσ * (negate 1 + 2 * cos²2σm)
-                        - _B / 6
-                        * cos2σm
-                        * (negate 3 + 4 * sin²σ)
-                        * (negate 3 + 4 * cos²2σm)
-                        )
-                )
-
-        s = b * _A * (σ - _Δσ)
+        _Δσ = _B * sinσ * (cos2σm + _B / 4 * y)
+        y =
+            cosσ * (-1 + 2 * cos²2σm)
+            - _B / 6 * cos2σm * (-3 + 4 * sin²σ) * (-3 + 4 * cos²2σm)
 
 tooFar :: Num a => TaskDistance a
 tooFar = TaskDistance [u| 20000000 m |]
@@ -164,16 +145,19 @@ distanceVincenty'
     | x == y = VincentyInverse $ TaskDistance [u| 0 m |]
     | otherwise =
         case d of
-            VincentyInverse d' -> VincentyInverse . TaskDistance . MkQuantity $ d'
+            VincentyInverse d' ->
+                VincentyInverse . TaskDistance . MkQuantity $ d'
+                
             VincentyAntipodal -> VincentyAntipodal
             VincentyAbnormal ab -> VincentyAbnormal ab
         where
+            f = flattening ellipsoid
             _U1 = atan $ (1 - f) * tan _Φ1
             _U2 = atan $ (1 - f) * tan _Φ2
             _L = _L2 - _L1
-            λ = _L 
-            d = vincentyInverse ellipsoid accuracy' λ _L _U1 _U2
-            f = flattening ellipsoid
+            λ = _L
 
             VincentyAccuracy accuracy = defaultVincentyAccuracy
             accuracy' = VincentyAccuracy $ fromRational accuracy
+            
+            d = vincentyInverse ellipsoid accuracy' λ _L _U1 _U2
--- earth-ellipsoid/library/Flight/Ellipsoid/PointToPoint/Rational.hs
+++ earth-ellipsoid/library/Flight/Ellipsoid/PointToPoint/Rational.hs
@@ -48,71 +48,49 @@ vincentyInverse
     -> VincentyInverse Rational
 vincentyInverse
     e@(Epsilon eps)
-    ellipsoid@Ellipsoid{semiMajor, semiMinor}
+    ellipsoid@Ellipsoid{semiMajor = MkQuantity a, semiMinor = MkQuantity b}
     accuracy@(VincentyAccuracy tolerance)
     λ _L _U1 _U2 =
     if abs λ > F.pi eps
         then VincentyAntipodal
         else
             if abs (λ - λ') < tolerance
-                then VincentyInverse s
+                then VincentyInverse $ b * _A * (σ - _Δσ)
                 else vincentyInverse e ellipsoid accuracy λ' _L _U1 _U2
     where
-        f :: Rational
+        sin' = F.sin eps
+        cos' = F.cos eps
         f = toRational . flattening $ ellipsoid
+        (sinU1, sinU2, sinλ) = (sin' _U1, sin' _U2, sin' λ)
+        (cosU1, cosU2, cosλ) = (cos' _U1, cos' _U2, cos' λ)
 
-        i = cos' _U2 * sin' λ
-        j = cos' _U1 * sin' _U2 - sin' _U1 * cos' _U2 * cos' λ
-        i² = i * i
-        j² = j * j
-        sinσ = F.sqrt eps $ i² + j²
-        cosσ = sin' _U1 * sin' _U2 + cos' _U1 * cos' _U2 * cos' λ
+        i = cosU2 * sinλ
+        j = cosU1 * sinU2 - sinU1 * cosU2 * cosλ
+        sin²σ = i * i + j * j
+        sinσ = F.sqrt eps sin²σ
+        cosσ = sinU1 * sinU2 + cosU1 * cosU2 * cosλ
+
         σ = atan2' e sinσ cosσ
-        sinα = cos' _U1 * cos' _U2 * sin' λ / sin' σ
-        sin²α = sinα * sinα 
-        cos²α = 1 - sin²α 
 
-        cos2σm =
-            if cos²α == 0
-                then
-                    -- NOTE: Start and end points on the equator, _C = 0.
-                    0
-                else
-                    cosσ - 2 * sin' _U1 * sin' _U2 / cos²α
+        sinα = cosU1 * cosU2 * sinλ / sinσ
+        cos²α = 1 - sinα * sinα
+
+        -- NOTE: Start and end points on the equator, _C = 0.
+        cos2σm = if cos²α == 0 then 0 else cosσ - 2 * sinU1 * sinU2 / cos²α
 
         cos²2σm = cos2σm * cos2σm
         _C = f / 16 * cos²α * (4 + f * (4 - 3 * cos²α))
-        x = σ + _C * sinσ * y
-        y = cos2σm + _C * cosσ * (negate 1 + 2 * cos²2σm)
-        λ' = _L + (1 - _C) * f * sinα * x
+        λ' = _L + (1 - _C) * f * sinα * (σ + _C * sinσ * x)
+        x = cos2σm + _C * cosσ * (-1 + 2 * cos²2σm)
 
-        sin' = F.sin eps
-        cos' = F.cos eps
+        u² = let b² = b * b in cos²α * (a * a - b²) / b² 
+        _A = 1 + u² / 16384 * (4096 + u² * (-768 + u² * (320 - 175 * u²)))
+        _B = u² / 1024 * (256 + u² * (-128 + u² * (74 - 47 * u²)))
 
-        MkQuantity a = semiMajor
-        MkQuantity b = semiMinor
-        a² = a * a
-        b² = b * b
-        u² = cos²α * (a² - b²) / b² 
-        _A = 1 + u² / 16384 * (4096 + u² * (negate 768 + u² * (320 - 175 * u²)))
-        _B = u² / 1024 * (256 + u² * (negate 128 + u² * (74 - 47 * u²)))
-        sin²σ = sinσ * sinσ
-
-        _Δσ =
-            _B * sinσ *
-                (cos2σm
-                    + _B / 4
-                    *
-                        (cosσ * (negate 1 + 2 * cos²2σm)
-                        - _B / 6
-                        * cos2σm
-                        * (negate 3 + 4 * sin²σ)
-                        * (negate 3 + 4 * cos²2σm)
-                        )
-                )
-
-        s = b * _A * (σ - _Δσ)
-
+        _Δσ = _B * sinσ * (cos2σm + _B / 4 * y)
+        y =
+            cosσ * (-1 + 2 * cos²2σm)
+            - _B / 6 * cos2σm * (-3 + 4 * sin²σ) * (-3 + 4 * cos²2σm)
 tooFar :: Num a => TaskDistance a
 tooFar = TaskDistance [u| 20000000 m |]
 
@@ -154,9 +132,15 @@ distanceVincenty' e@(Epsilon eps) ellipsoid
         case d of
             VincentyInverse d' ->
                 VincentyInverse . TaskDistance . fromRational' . MkQuantity $ d'
+                
             VincentyAntipodal -> VincentyAntipodal
             VincentyAbnormal ab -> VincentyAbnormal ab
         where
+            tan' = F.tan eps
+            atan' = F.atan eps
+            ellipsoidR = toRationalEllipsoid ellipsoid
+            f = flattening ellipsoidR
+
             (LatLng (Lat (MkQuantity _Φ1), Lng (MkQuantity _L1))) =
                     toRationalLatLng x
 
@@ -168,11 +152,6 @@ distanceVincenty' e@(Epsilon eps) ellipsoid
             _L = _L2 - _L1
             λ = _L
 
-            ellipsoidR = toRationalEllipsoid ellipsoid
-            f = flattening ellipsoidR
             accuracy = defaultVincentyAccuracy
 
             d = vincentyInverse e ellipsoidR accuracy λ _L _U1 _U2
-
-            tan' = F.tan eps
-            atan' = F.atan eps
