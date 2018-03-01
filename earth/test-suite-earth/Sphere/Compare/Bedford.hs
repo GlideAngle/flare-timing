e07098ef24c68bb50ce499d59b2815710426076d
Phil de Joux <philderbeast@gmail.com>
2018-03-01 08:01:08 -0500

    Add a SphereVersusEllipsoid module, #8.

--- earth/test-suite-earth/Sphere/Compare/Bedford.hs
+++ earth/test-suite-earth/Sphere/Compare/Bedford.hs
@@ -1,59 +1,9 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE QuasiQuotes #-}
-
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -fplugin Data.UnitsOfMeasure.Plugin #-}
-{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
-
 module Sphere.Compare.Bedford (bedfordUnits) where
 
-import Prelude hiding (span, min)
-import Data.Ratio ((%))
 import Test.Tasty (TestTree, testGroup)
-import Data.UnitsOfMeasure (u, convert)
 
-import Flight.Units ()
-import Flight.Units.DegMinSec (DMS(..))
-import Flight.LatLng.Rational (Epsilon(..))
-import Flight.Distance (TaskDistance(..))
-import qualified Flight.Earth.Sphere.PointToPoint.Double as Dbl (distanceHaversine)
-import qualified Flight.Earth.Sphere.PointToPoint.Rational as Rat (distanceHaversine)
-import Bedford (GetTolerance, points, solutions)
-import qualified Bedford as B (dblChecks, ratChecks)
-
-getTolerance :: (Ord a, Fractional a) => GetTolerance a
-getTolerance d'
-    | d < [u| 100 km |] = convert [u| 425.4 m |]
-    | d < [u| 500 km |] = convert [u| 2.495 km |]
-    | d < [u| 1000 km |] = [u| 4.06 km |]
-    | otherwise = [u| 19.5 km |]
-    where
-        d = convert d'
-
-dblChecks
-    :: [TaskDistance Double]
-    -> [((DMS, DMS), (DMS, DMS))]
-    -> [TestTree]
-dblChecks =
-    B.dblChecks (Dbl.distanceHaversine) getTolerance
-
-ratChecks
-    :: [TaskDistance Double]
-    -> [((DMS, DMS), (DMS, DMS))]
-    -> [TestTree]
-ratChecks =
-    B.ratChecks span getTolerance
-    where
-        span = Rat.distanceHaversine e
-        e = Epsilon $ 1 % 1000000000000000000
+import Bedford (points, solutions)
+import Sphere.Compare.SphereVersusEllipsoid (dblChecks, ratChecks)
 
 bedfordUnits :: TestTree
 bedfordUnits =
