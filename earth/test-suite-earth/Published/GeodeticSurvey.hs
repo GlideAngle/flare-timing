5504c3ed897b119c23620a4dc6bf08df8960f817
Phil de Joux <philderbeast@gmail.com>
2018-03-05 14:41:50 -0500

    Move code to Lat45Lng180S40 module, #8.

--- earth/test-suite-earth/Published/GeodeticSurvey.hs
+++ earth/test-suite-earth/Published/GeodeticSurvey.hs
@@ -1,19 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE QuasiQuotes #-}
-
-{-# LANGUAGE ParallelListComp #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -fplugin Data.UnitsOfMeasure.Plugin #-}
-{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
-
 -- | Test data generated using forward and inverse programs.
 --
 -- National Geodetic Survey
@@ -111,17 +95,14 @@ module Published.GeodeticSurvey
     ) where
 
 import Data.Maybe (catMaybes)
-import Data.UnitsOfMeasure (u)
 
 import Flight.Units ()
-import Flight.Units.DegMinSec (DMS(..))
-import Flight.Distance (TaskDistance(..))
 import Flight.Earth.Geodesy
     ( GeodesyProblems(..)
-    , DirectProblem(..), DirectSolution(..)
     , DProb, DSoln
     , IProb, ISoln
     )
+import qualified Published.GeodeticSurvey.Lat45Lng180S40 as Lat45Lng180S40
 
 inverseProblems :: [IProb]
 inverseProblems = fst <$> inversePairs
@@ -144,74 +125,4 @@ inversePairs =
 
 directPairs :: [(DProb, DSoln)]
 directPairs =
-    directLat45Lng180S40
-
--- | With a common first station and ellipsoidal distance of;
---
---    First  Station :
---    ----------------
---     LAT =  45  0  0.00000 North
---     LON = 180  0  0.00000 West
---
---   Ellipsoidal distance     S =        40.0000 m
-directLat45Lng180S40 :: [(DProb, DSoln)]
-directLat45Lng180S40 =
-    [
---    Second Station :
---    ----------------
---     LAT =  45  0  1.29576 North
---     LON = 180  0  0.00000 West
--- 
---   Forward azimuth        FAZ =   0  0  0.0000 From North
---   Back azimuth           BAZ = 180  0  0.0000 From North
-        ( DirectProblem x (DMS (0, 0, 0)) d
-        , DirectSolution
-            (DMS (45, 0, 1.29576), DMS (180, 0, 0)) 
-            (Just (DMS (180, 0, 0)))
-        )
-    ,
-
---    Second Station :
---    ----------------
---     LAT =  45  0  0.00000 North
---     LON = 179 59 58.17367 West
--- 
---   Forward azimuth        FAZ =  90  0  0.0000 From North
---   Back azimuth           BAZ = 270  0  1.2914 From North
-        ( DirectProblem x (DMS (90, 0, 0)) d
-        , DirectSolution
-            (DMS (45, 0, 0), DMS (-179, 59, 58.17367)) 
-            (Just (DMS (270, 0, 1.2914)))
-        )
-    ,
-
---    Second Station :
---    ----------------
---     LAT =  44 59 58.70424 North
---     LON = 180  0  0.00000 West
--- 
---   Forward azimuth        FAZ = 180  0  0.0000 From North
---   Back azimuth           BAZ =   0  0  0.0000 From North
-        ( DirectProblem x (DMS (180, 0, 0)) d
-        , DirectSolution
-            (DMS (44, 59, 58.70424), DMS (180, 0, 0)) 
-            (Just (DMS (0, 0, 0)))
-        )
-    ,
-
---    Second Station :
---    ----------------
---     LAT =  45  0  0.00000 North
---     LON = 179 59 58.17367 East
--- 
---   Forward azimuth        FAZ = 270  0  0.0000 From North
---   Back azimuth           BAZ =  89 59 58.7086 From North
-        ( DirectProblem x (DMS (270, 0, 0)) d
-        , DirectSolution
-            (DMS (45, 0, 0), DMS (179, 59, 58.17367)) 
-            (Just (DMS (89, 559, 58.7086)))
-        )
-    ]
-    where
-        x = (DMS (45, 0, 0), DMS (180, 0, 0)) 
-        d = TaskDistance $ [u| 40 m |]
+    Lat45Lng180S40.fwd
