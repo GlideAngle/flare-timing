76f0de2040639c38414391db9cd923e034d0445f
Phil de Joux <philderbeast@gmail.com>
2018-05-22 11:59:26 -0400

    Add rules for generating cabal files, #13.

--- build/app-cmd/Pkg.hs
+++ build/app-cmd/Pkg.hs
@@ -3,53 +3,68 @@ module Pkg (buildRules) where
 import Development.Shake
     ( Rules
     , CmdOption(Shell)
+    , (%>)
     , phony
     , cmd
     , need
     )
 
-import Development.Shake.FilePath ((</>))
+import Development.Shake.FilePath ((<.>), (</>))
 
 -- | The names of the packages for dhall-format and hpack-dhall.
 dhallPkgs :: [String]
-dhallPkgs =
-    [ "aeson-via-sci"
-    , "aeson-via-uom"
-    , "build"
-    , "cmd"
-    , "comp"
-    , "earth"
-    , "flare-timing"
-    , "fsdb"
-    , "gap"
-    , "igc"
-    , "kml"
-    , "latlng"
-    , "lookup"
-    , "mask"
-    , "route"
-    , "scribe"
-    , "siggy-chardust"
-    , "span"
-    , "task"
-    , "track"
-    , "units"
-    , "www"
-    , "zone"
-    ] 
+dhallPkgs = fst <$> dhallCabal
 
-hpack :: String -> Rules ()
-hpack x =
-    phony ("hpack-dhall-" ++ x) $ cmd Shell ("hpack-dhall " ++ x)
+-- | Pairs of package folder name used by dhall and the produced cabal file
+-- name.
+dhallCabal :: [(String, String)]
+dhallCabal =
+    [ ("aeson-via-sci", "aeson-via-sci")
+    , ("aeson-via-uom", "aeson-via-uom")
+    , ("build", "build-flare-timing")
+    , ("cmd", "flight-cmd")
+    , ("comp", "flight-comp")
+    , ("earth", "flight-earth")
+    , ("flare-timing", "flare-timing")
+    , ("fsdb", "flight-fsdb")
+    , ("gap", "flight-gap")
+    , ("igc", "flight-igc")
+    , ("kml", "flight-kml")
+    , ("latlng", "flight-latlng")
+    , ("lookup", "flight-lookup")
+    , ("mask", "flight-mask")
+    , ("route", "flight-route")
+    , ("scribe", "flight-scribe")
+    , ("siggy-chardust", "siggy-chardust")
+    , ("span", "flight-span")
+    , ("task", "flight-task")
+    , ("track", "flight-track")
+    , ("units", "flight-units")
+    , ("www", "www-flare-timing")
+    , ("zone", "flight-zone")
+    ] 
 
 format :: String -> Rules ()
 format x =
     phony ("dhall-format-" ++ x)
     $ cmd Shell ("dhall-format --inplace " ++ (x </> "package.dhall"))
 
+hpack :: String -> Rules ()
+hpack x =
+    phony ("hpack-dhall-" ++ x) $ do
+        need ["dhall-format-" ++ x]
+        cmd Shell ("hpack-dhall " ++ x)
+
+cabal :: (String, String) -> Rules ()
+cabal (x, y) =
+    x </> y <.> "cabal" %> \_ -> do
+        need ["hpack-dhall-" ++ x]
+
 buildRules :: Rules ()
 buildRules = do
     sequence_ $ format <$> dhallPkgs
     sequence_ $ hpack <$> dhallPkgs
+    sequence_ $ cabal <$> dhallCabal
     phony "dhall-format" $ need $ (\x -> "dhall-format-" ++ x) <$> dhallPkgs
     phony "hpack-dhall" $ need $ (\x -> "hpack-dhall-" ++ x) <$> dhallPkgs
+    phony "cabal-files" $ need $ (\(x, y) -> x </> y <.> "cabal") <$> dhallCabal
