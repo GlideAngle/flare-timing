c69ab73c07a3112a42750bc29e1fa20c86892e93
Phil de Joux <philderbeast@gmail.com>
2018-06-06 12:44:46 +0200

    Update the docs for siggy-chardust.

--- siggy-chardust/library/Data/Number/RoundingFunctions.hs
+++ siggy-chardust/library/Data/Number/RoundingFunctions.hs
@@ -1,17 +1,37 @@
+{-|
+Module      : Data.Number.RoundingFunctions 
+Copyright   : (c) Block Scope Limited 2017
+License     : MPL-2.0
+Maintainer  : phil.dejoux@blockscope.com
+Stability   : experimental
+
+Functions rounding to decimal places and significant digits.
+-}
 module Data.Number.RoundingFunctions (dpRound, sdRound) where
 
 import Data.Word (Word8)
 
--- | Rounds to the given number of decimal places.
+-- | Rounds to a number of decimal places.
+
 -- SEE: https://stackoverflow.com/questions/12450501/round-number-to-specified-number-of-digits
-dpRound :: Integer -> Rational -> Rational
+dpRound
+    :: Integer
+    -- ^ A positive number of decimal places to keep
+    -> Rational
+    -- ^ The number to round
+    -> Rational
 dpRound n f
     | n < 0 = f
     | otherwise =
         fromInteger (round $ f * (10^n)) / (10.0^^n)
 
--- | Keeps the given number of significant digits with rounding.
-sdRound :: Word8 -> Rational -> Rational
+-- | Rounds to a number of significant digits.
+sdRound
+    :: Word8
+    -- ^ A positive number of significant digits to keep
+    -> Rational
+    -- ^ The number to round
+    -> Rational
 sdRound sd' f =
     if m < 0
         then
