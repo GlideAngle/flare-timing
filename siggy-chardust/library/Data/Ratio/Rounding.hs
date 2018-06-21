e57baf75cd3fa63416605bf5be97837bb32133cf
Phil de Joux <philderbeast@gmail.com>
2018-06-21 10:56:30 -0400

    Reference the round function, #40.

--- siggy-chardust/library/Data/Ratio/Rounding.hs
+++ siggy-chardust/library/Data/Ratio/Rounding.hs
@@ -9,7 +9,7 @@ Stability: experimental
 
 Rounding rationals to significant digits and decimal places.
 
-The round function from the prelude returns an integer. The standard librarys
+The 'round' function from the prelude returns an integer. The standard librarys
 of C and C++ have round functions that return floating point numbers. Rounding
 in this library takes and returns 'Rational's and can round to a number of
 significant digits or a number of decimal places.
