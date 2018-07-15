c6cb2e9f1aa1a628ecf48a29d0f2aaa958fd3ad3
Phil de Joux <philderbeast@gmail.com>
2018-07-15 16:18:10 -0400

    Split modules adding Flight.Zone.Zone, #65.

--- zone/library/Flight/Zone.hs
+++ zone/library/Flight/Zone.hs
@@ -29,221 +29,18 @@ module Flight.Zone
     , rawZonesToZones
     ) where
 
-import Data.Foldable (asum)
-import Data.Aeson
-    (ToJSON(..), FromJSON(..), (.:), (.=), object, withObject)
-import Data.UnitsOfMeasure (u, toRational', fromRational', zero)
+import Data.UnitsOfMeasure (toRational', fromRational')
 import Data.UnitsOfMeasure.Internal (Quantity(..))
 
 import Flight.Units (realToFrac')
-import Flight.Units.DegMinSec (fromQ)
-import Flight.LatLng (QLat, Lat(..), QLng, Lng(..), LatLng(..), fromDMS)
+import Flight.LatLng (QLat, Lat(..), QLng, Lng(..), LatLng(..))
 import Flight.Zone.Radius (Radius(..), QRadius)
 import Flight.Zone.Bearing (Bearing(..), QBearing)
 import Flight.Zone.Incline (Incline(..), QIncline)
-import qualified Flight.Zone.Raw.Zone as Raw (RawZone(..))
-import Flight.LatLng.Raw (RawLat(..), RawLng(..))
-
--- | Does it have area?
-class HasArea a where
-    hasArea :: a -> Bool
-
--- | A control zone of the task. Taken together these make up the course to fly
--- with start enter and exit cylinders, turnpoint cylinders, goal lines and
--- cylinders.
-data Zone a where
-    -- | Used to mark the exact turnpoints in the optimized task distance.
-    Point
-        :: (Eq a, Ord a)
-        => LatLng a [u| rad |]
-        -> Zone a
-
-    -- | Used only in open distance tasks these mark the start and direction of
-    -- the open distance.
-    Vector
-        :: (Eq a, Ord a)
-        => QBearing a [u| rad |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-    -- | The turnpoint cylinder.
-    Cylinder
-        :: (Eq a, Ord a)
-        => QRadius a [u| m |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-    -- | Only used in paragliding, this is the conical end of speed section
-    -- used to discourage too low an end to final glides.
-    Conical
-        :: (Eq a, Ord a)
-        => QIncline a [u| rad |]
-        -> QRadius a [u| m |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-    -- | A goal line perpendicular to the course line.
-    Line 
-        :: (Eq a, Ord a)
-        => QRadius a [u| m |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-    -- | This like a cylinder control zone but only used for goal.
-    Circle
-        :: (Eq a, Ord a)
-        => QRadius a [u| m |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-    -- | This control zone is only ever used as a goal for paragliding. It is
-    -- a goal line perpendicular to the course line followed by half
-    -- a cylinder.
-    SemiCircle
-        :: (Eq a, Ord a)
-        => QRadius a [u| m |]
-        -> LatLng a [u| rad |]
-        -> Zone a
-
-deriving instance Eq (Zone a)
-deriving instance Ord (Zone a)
-deriving instance
-    ( Show (QIncline a [u| rad |])
-    , Show (QBearing a [u| rad |])
-    , Show (QRadius a [u| m |])
-    , Show (LatLng a [u| rad |])
+import Flight.Zone.Zone
+    ( HasArea(..), Zone(..), RawZoneToZone
+    , center, radius, showZoneDMS, rawZonesToZones
     )
-    => Show (Zone a)
-
-instance (Ord a, Num a) => HasArea (Zone a) where
-    hasArea (Point _) = False
-    hasArea (Vector _ _) = False
-    hasArea (Cylinder (Radius x) _) = x > zero
-    hasArea (Conical _ (Radius x) _) = x > zero
-    hasArea (Line (Radius x) _) = x > zero
-    hasArea (Circle (Radius x) _) = x > zero
-    hasArea (SemiCircle (Radius x) _) = x > zero
-
-instance
-    ( ToJSON (LatLng a [u| rad |])
-    , ToJSON (QBearing a [u| rad |])
-    , ToJSON (QIncline a [u| rad |])
-    , ToJSON (QRadius a [u| m |])
-    )
-    => ToJSON (Zone a) where
-    toJSON (Point x) = object
-        [ "point" .= object
-            [ "latlng" .= toJSON x
-            ]
-        ]
-    toJSON (Vector b x) = object
-        [ "vector" .= object
-            [ "bearing" .= toJSON b
-            , "latlng" .= toJSON x
-            ]
-        ]
-    toJSON (Cylinder r x) = object
-        ["cylinder" .= object
-            [ "radius" .= toJSON r
-            , "latlng" .= toJSON x
-            ]
-        ]
-    toJSON (Conical i r x) = object
-        [ "conical" .= object
-            [ "radius" .= toJSON r
-            , "incline" .= toJSON i
-            , "latlng" .= toJSON x
-            ]
-        ]
-    toJSON (Line r x) = object
-        [ "line" .= object
-            [ "radius" .= toJSON r
-            , "latlng" .= toJSON x
-            ]
-        ]
-    toJSON (Circle r x) = object
-        [ "circle" .= object
-            [ "radius" .= toJSON r
-            , "latlng" .= toJSON x
-            ]
-        ]
-
-    toJSON (SemiCircle r x) = object
-        [ "semicircle" .= object
-            [ "radius" .= toJSON r
-            , "latlng" .= toJSON x
-            ]
-        ]
-
-instance
-    ( Eq a
-    , Ord a
-    , FromJSON (LatLng a [u| rad |])
-    , FromJSON (QBearing a [u| rad |])
-    , FromJSON (QIncline a [u| rad |])
-    , FromJSON (QRadius a [u| m |])
-    )
-    => FromJSON (Zone a) where
-    parseJSON = withObject "Zone" $ \o ->
-        asum
-            [ do
-                pt <- o .: "point"
-                Point <$> pt .: "x" 
-
-            , do
-                vc <- o .: "vector"
-                Vector <$> vc .: "b" <*> vc .: "x"
-
-            , do
-                cy <- o .: "cylinder"
-                Cylinder <$> cy .: "r" <*> cy .: "x"
-
-            , do
-                co <- o .: "conical"
-                Conical <$> co .: "r" <*> co .: "i" <*> co .: "x"
-
-            , do
-                ln <- o .: "line"
-                Line <$> ln .: "r" <*> ln .: "x"
-
-            , do
-                cc <- o .: "circle"
-                Circle <$> cc .: "r" <*> cc .: "x"
-
-            , do
-                sc <- o .: "semicircle"
-                SemiCircle <$> sc .: "r" <*> sc .: "x"
-
-            , fail $ "Unknown type of zone "
-            ]
-        where
-
-showZoneDMS :: Zone Double -> String
-showZoneDMS (Point (LatLng (Lat x, Lng y))) =
-    "Point " ++ show (fromQ x, fromQ y)
-
-showZoneDMS (Vector (Bearing b) (LatLng (Lat x, Lng y))) =
-    "Vector " ++ show (fromQ b) ++ " " ++ show (fromQ x, fromQ y)
-
-showZoneDMS (Cylinder r (LatLng (Lat x, Lng y))) =
-    "Cylinder " ++ show r ++ " " ++ show (fromQ x, fromQ y)
-
-showZoneDMS (Conical (Incline i) r (LatLng (Lat x, Lng y))) =
-    "Conical "
-    ++ show (fromQ i)
-    ++ " "
-    ++ show r
-    ++ " "
-    ++ show (fromQ x, fromQ y)
-
-showZoneDMS (Line r (LatLng (Lat x, Lng y))) =
-    "Line " ++ show r ++ " " ++ show (fromQ x, fromQ y)
-
-showZoneDMS (Circle r (LatLng (Lat x, Lng y))) =
-    "Circle " ++ show r ++ " " ++ show (fromQ x, fromQ y)
-
-showZoneDMS (SemiCircle r (LatLng (Lat x, Lng y))) =
-    "SemiCircle " ++ show r ++ " " ++ show (fromQ x, fromQ y)
 
 fromRationalRadius :: Fractional a => QRadius Rational u -> QRadius a u
 fromRationalRadius (Radius r) =
@@ -372,26 +169,6 @@ realToFracZone (Circle r x) =
 realToFracZone (SemiCircle r x) =
     SemiCircle (realToFracRadius r) (realToFracLatLng x)
 
--- | The effective center point of a zone.
-center :: Zone a -> LatLng a [u| rad |]
-center (Point x) = x
-center (Vector _ x) = x
-center (Cylinder _ x) = x
-center (Conical _ _ x) = x
-center (Line _ x) = x
-center (Circle _ x) = x
-center (SemiCircle _ x) = x
-
--- | The effective radius of a zone.
-radius :: Num a => Zone a -> QRadius a [u| m |]
-radius (Point _) = Radius [u| 0m |]
-radius (Vector _ _) = Radius [u| 0m |]
-radius (Cylinder r _) = r
-radius (Conical _ r _) = r
-radius (Line r _) = r
-radius (Circle r _) = r
-radius (SemiCircle r _) = r
-
 newtype Deadline = Deadline Integer deriving (Eq, Ord, Show)
 newtype TimeOfDay = TimeOfDay Rational deriving (Eq, Ord, Show)
 newtype Interval = Interval Rational deriving (Eq, Ord, Show)
@@ -410,27 +187,3 @@ data Task a
         , startGates :: StartGates
         , deadline :: Maybe Deadline
         }
-
-type RawZoneToZone =
-    QRadius Double [u| m |] -> LatLng Double [u| rad |] -> Zone Double
-
-rawZonesToZones
-    :: RawZoneToZone
-    -> [Raw.RawZone]
-    -> [Zone Double]
-rawZonesToZones goal xs =
-    case reverse xs of
-        (x : y : ys) -> reverse $ f goal x : (f Cylinder <$> (y : ys))
-        _ -> f Cylinder <$> xs
-    where
-        f ctor Raw.RawZone{radius = r, lat, lng} =
-            ctor r $ fromDMS (fromQ qLat, fromQ qLng)
-                where
-                    RawLat lat' = lat
-                    RawLng lng' = lng
-
-                    qLat :: Quantity Double [u| deg |]
-                    qLat = fromRational' $ MkQuantity lat'
-
-                    qLng :: Quantity Double [u| deg |]
-                    qLng = fromRational' $ MkQuantity lng'
