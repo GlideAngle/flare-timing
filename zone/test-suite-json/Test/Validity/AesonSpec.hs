f579c87966fd12210e6257b19babc0c846efd9c5
Phil de Joux <philderbeast@gmail.com>
2018-07-29 17:13:18 -0400

    Add test for ESS is goal race, #79.

--- zone/test-suite-json/Test/Validity/AesonSpec.hs
+++ zone/test-suite-json/Test/Validity/AesonSpec.hs
@@ -45,6 +45,9 @@ spec = do
         it "encodes a circle zone kind"
             $ encode zkCircle `shouldBe` T.encodeUtf8 strCircle
 
+        it "encodes an ESS is goal race"
+            $ encode tzEssIsGoalRace `shouldBe` T.encodeUtf8 strEssIsGoalRace
+
     describe "FromJSON" $ do
         it ("decodes an altitude time of 3.5 s / m as " ++ show altTime)
             $ decode "\"3.5 s / m\"" `shouldBe` (Just altTime)
@@ -72,6 +75,10 @@ spec = do
         it "decodes a circle zone kind"
             $ decode "{\"circle\":{\"radius\":\"11.22 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}}"
             `shouldBe` (Just zkCircle)
+
+        it "decodes an ESS is goal race"
+            $ decode "{\"race-ess-is-goal\":{\"circle\":{\"radius\":\"11.22 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}},\"race\":[{\"cylinder\":{\"radius\":\"11.22 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}}],\"prolog\":[]}"
+            `shouldBe` (Just tzEssIsGoalRace)
 
     where
         deg90 :: Quantity Double [u| deg |]
@@ -117,6 +124,9 @@ spec = do
         zkCircle :: ZoneKind Goal Double
         zkCircle = Circle radius gs1
         strCircle = "{\"circle\":{\"radius\":\"11.2 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}}" 
+
+        tzEssIsGoalRace = TzEssIsGoal [] [zkCyl] zkCircle
+        strEssIsGoalRace = "{\"race-ess-is-goal\":{\"circle\":{\"radius\":\"11.2 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}},\"race\":[{\"cylinder\":{\"radius\":\"11.2 m\",\"center\":[\"43.82972999 deg\",\"16.64243 deg\"]}}],\"prolog\":[]}"
 
 gs1 :: LatLng Double [u| rad |]
 gs1 = LatLng 
