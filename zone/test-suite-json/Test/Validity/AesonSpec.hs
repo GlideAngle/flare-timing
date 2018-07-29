0735ffa96e5eb772712559963e926fafc2111707
Phil de Joux <philderbeast@gmail.com>
2018-07-29 14:17:37 -0400

    Add JSON encoding and decoding tests for bearing, #72.

--- zone/test-suite-json/Test/Validity/AesonSpec.hs
+++ zone/test-suite-json/Test/Validity/AesonSpec.hs
@@ -3,19 +3,36 @@ module Test.Validity.AesonSpec where
 import Test.Hspec (Spec, describe, it, shouldBe)
 import Data.Aeson (encode, decode)
 import qualified Data.Text.Lazy.Encoding as T (encodeUtf8)
-import Data.UnitsOfMeasure (u)
-import Flight.Zone.Radius (Radius(..))
+import Data.UnitsOfMeasure (u, convert)
+import Data.UnitsOfMeasure.Internal (Quantity(..))
+import Flight.Zone.Bearing (Bearing(..))
+import Flight.Zone.Radius (QRadius, Radius(..))
 
 spec :: Spec
 spec = do
     describe "ToJSON" $ do
-        it "encodes a radius" $ do
-            encode (Radius [u| 11.22 m |])
-            `shouldBe`
-            T.encodeUtf8 "\"11.2 m\""
+        it "encodes a bearing"
+            $ encode bearing `shouldBe` T.encodeUtf8 "\"1.57 rad\""
+
+        it "encodes a radius"
+            $ encode radius `shouldBe` T.encodeUtf8 "\"11.2 m\""
 
     describe "FromJSON" $ do
-        it "decodes a radius" $ do
-            decode "\"11.2 m\""
-            `shouldBe`
-            (Just $ Radius [u| 11.2 m |])
+        it "decodes a bearing"
+            $ decode "\"1.57 rad\"" `shouldBe` (Just bearing')
+
+        it "decodes a radius"
+            $ decode "\"11.2 m\"" `shouldBe` (Just radius')
+
+    where
+        deg90 :: Quantity Double [u| rad |]
+        deg90 = convert [u| 90 deg |]
+
+        bearing = Bearing deg90
+        bearing' = Bearing [u| 1.57 rad |]
+
+        radius :: QRadius Double [u| m |]
+        radius = Radius [u| 11.22 m |]
+
+        radius' :: QRadius Double [u| m |]
+        radius' = Radius [u| 11.2 m |]
