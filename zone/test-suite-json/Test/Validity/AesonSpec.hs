875b165025559856f1aa4e01bf3a59b2e6528547
Phil de Joux <philderbeast@gmail.com>
2018-07-29 14:24:47 -0400

    Add unit tests for incline, #72.

--- zone/test-suite-json/Test/Validity/AesonSpec.hs
+++ zone/test-suite-json/Test/Validity/AesonSpec.hs
@@ -6,6 +6,7 @@ import qualified Data.Text.Lazy.Encoding as T (encodeUtf8)
 import Data.UnitsOfMeasure (u, convert)
 import Data.UnitsOfMeasure.Internal (Quantity(..))
 import Flight.Zone.Bearing (Bearing(..))
+import Flight.Zone.Incline (Incline(..))
 import Flight.Zone.Radius (QRadius, Radius(..))
 
 spec :: Spec
@@ -14,12 +15,18 @@ spec = do
         it "encodes a bearing"
             $ encode bearing `shouldBe` T.encodeUtf8 "\"1.57 rad\""
 
+        it "encodes a incline"
+            $ encode incline `shouldBe` T.encodeUtf8 "\"90.0 deg\""
+
         it "encodes a radius"
             $ encode radius `shouldBe` T.encodeUtf8 "\"11.2 m\""
 
     describe "FromJSON" $ do
         it "decodes a bearing"
             $ decode "\"1.57 rad\"" `shouldBe` (Just bearing')
+
+        it "decodes an incline"
+            $ decode "\"90.0 deg\"" `shouldBe` (Just incline)
 
         it "decodes a radius"
             $ decode "\"11.2 m\"" `shouldBe` (Just radius')
@@ -30,6 +37,8 @@ spec = do
 
         bearing = Bearing deg90
         bearing' = Bearing [u| 1.57 rad |]
+
+        incline = Incline deg90
 
         radius :: QRadius Double [u| m |]
         radius = Radius [u| 11.22 m |]
