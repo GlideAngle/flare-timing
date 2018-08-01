b6259dfa2a078bd0e8c1287683d84ab982f37773
Phil de Joux <philderbeast@gmail.com>
2018-08-01 12:25:12 -0400

    Add FieldOrdering instance for lists, #72.

--- latlng/library/Flight/Field.hs
+++ latlng/library/Flight/Field.hs
@@ -4,3 +4,7 @@ import Data.String (IsString())
 
 class FieldOrdering b where
     fieldOrder :: (Ord a, IsString a) => b -> (a -> a -> Ordering)
+
+instance (FieldOrdering a) => FieldOrdering [a] where
+    fieldOrder [] = \_ _ -> EQ
+    fieldOrder (x : _) = fieldOrder x
