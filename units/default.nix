c4add97ab1cf17035ca1e710754bf2a06488a4e8
Phil de Joux <philderbeast@gmail.com>
2018-08-15 17:22:02 -0400

    Build flight-units under nix, #81.

---
+++ units/default.nix
@@ -0,0 +1,31 @@
+# SEE: https://github.com/Gabriel439/haskell-nix/blob/master/project1/README.md
+# SEE: https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/haskell.section.md
+let
+  config = {
+    packageOverrides = super: let self = super.pkgs; in
+    {
+      haskell = super.haskell // {
+        packages = super.haskell.packages // {
+          ghc822 = super.haskell.packages.ghc822.override {
+            overrides = self: super: {
+              ghc-tcplugins-extra =
+                self.callPackage ../nix/ghc-tcplugins-extra.nix { };
+              tasty =
+                self.callPackage ../nix/tasty.nix { };
+              uom-plugin =
+                self.callPackage ../nix/uom-plugin.nix { };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  pkgs = import <nixpkgs> { inherit config; };
+
+in
+  { compiler ? "ghc822"
+  }:
+  pkgs.haskell.packages.${compiler}.callPackage
+  ./flight-units.nix
+  { }
--- units/flight-units.nix
+++ units/flight-units.nix
@@ -9,6 +9,7 @@ mkDerivation {
     base bifunctors fixed formatting numbers siggy-chardust text
     uom-plugin
   ];
+  doHaddock = false;
   homepage = "https://github.com/blockscope/flare-timing#readme";
   description = "Units used in hang gliding and paragliding competitions";
   license = stdenv.lib.licenses.mpl20;
