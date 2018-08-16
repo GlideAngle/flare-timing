a3bfa6419a7b0aa38b48d59c608620e492eb3000
Phil de Joux <philderbeast@gmail.com>
2018-08-16 12:34:07 -0400

    Use general, not ghc822 specific override, #81.

--- units/default.nix
+++ units/default.nix
@@ -5,17 +5,13 @@ let
     packageOverrides = super: let self = super.pkgs; in
     {
       haskell = super.haskell // {
-        packages = super.haskell.packages // {
-          ghc822 = super.haskell.packages.ghc822.override {
-            overrides = self: super: {
-              ghc-tcplugins-extra =
-                self.callPackage ../nix/ghc-tcplugins-extra.nix { };
-              tasty =
-                self.callPackage ../nix/tasty.nix { };
-              uom-plugin =
-                self.callPackage ../nix/uom-plugin.nix { };
-            };
-          };
+        packageOverrides = self: super: {
+          ghc-tcplugins-extra =
+            self.callPackage ../nix/ghc-tcplugins-extra.nix { };
+          tasty =
+            self.callPackage ../nix/tasty.nix { };
+          uom-plugin =
+            self.callPackage ../nix/uom-plugin.nix { };
         };
       };
     };
