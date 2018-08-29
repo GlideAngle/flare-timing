ded3350ea81e09cffdcb00ca02af956e34c978fc
Phil de Joux <philderbeast@gmail.com>
2018-08-29 16:36:15 -0400

    Move default.nix to ./nix/nixpkgs.nix, #81.

---
+++ nix/nixpkgs.nix
@@ -0,0 +1,12 @@
+{ compiler ? "ghc822" }:
+
+# SEE: https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+let
+   hostPkgs = import <nixpkgs> {};
+   pinnedVersion = hostPkgs.lib.importJSON ./nixpkgs-version.json;
+   pinnedPkgs = hostPkgs.fetchgit {
+     inherit (pinnedVersion) url rev sha256;
+   };
+
+in
+  import pinnedPkgs {config = { allowUnsupportedSystem = true; allowUnfree = true; }; }
--- siggy-chardust/default.nix
+++ siggy-chardust/default.nix
@@ -1,6 +1,27 @@
-{ nixpkgs ? import <nixpkgs> {}
+{ pkgs ? import ../nix/nixpkgs.nix {}
 , compiler ? "ghc822"
 }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage
-./siggy-chardust.nix
-{ }
+
+let
+  hlib = pkgs.haskell.lib;
+
+  fgl-src = pkgs.fetchgit {
+          url = "http://github.com/haskell/fgl.git";
+          sha256 = "0biwsaj6s24l8ix95hkw4syl87ywxy363pr413kazzbhi0csf20s";
+          rev = "e29503775e474b7a7cd8951b6bebcf1529b888b5";
+        };
+
+  hcoord-src = pkgs.fetchgit {
+          url = "http://github.com/BlockScope/hcoord.git";
+          rev = "bb17ddba271829f3902d9ae3af97f2723eb0ab47";
+        };
+
+  hp = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: {
+      fgl = super.callCabal2nix fgl-src {};
+      hcoord = super.callCabal2nix (hcoord-src + "/hcoord") {};
+      hcoord-utm = super.callCabal2nix (hcoord-src + "/hcoord-utm") {};
+    };
+  };
+
+in hp.callPackage ./siggy-chardust.nix {}
