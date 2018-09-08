8f0ca53653d8d7b5c7c0cd25434cd7c371a4bec1
Phil de Joux <philderbeast@gmail.com>
2018-09-08 10:38:31 -0400

    For fgl, hcoord & hcoord-utm, get the src with fetchgit, #83.

--- nix/hcoord-utm.nix
+++ nix/hcoord-utm.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, data-default, hcoord, hlint, HUnit, ieee754
-, mtl, stdenv
+{ mkDerivation, base, data-default, fetchgit, hcoord, hlint, HUnit
+, ieee754, mtl, stdenv
 }:
 mkDerivation {
   pname = "hcoord-utm";
   version = "2.1.0";
-  src = ../.stack2cabal/hcoord;
+  src = fetchgit {
+    url = "http://github.com/BlockScope/hcoord.git";
+    sha256 = "0267n694m08bv73ld7f5flb66h3dxc7xgrbmkr757q0g87l8ndzq";
+    rev = "3c3859dac5da111e57a6de09764ffdb127197c4a";
+  };
   postUnpack = "sourceRoot+=/hcoord-utm; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [ base hcoord mtl ];
   testHaskellDepends = [
--- nix/hcoord.nix
+++ nix/hcoord.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, data-default, hlint, HUnit, ieee754, mtl
-, stdenv
+{ mkDerivation, base, data-default, fetchgit, hlint, HUnit, ieee754
+, mtl, stdenv
 }:
 mkDerivation {
   pname = "hcoord";
   version = "2.1.0";
-  src = ../.stack2cabal/hcoord;
+  src = fetchgit {
+    url = "http://github.com/BlockScope/hcoord.git";
+    sha256 = "0267n694m08bv73ld7f5flb66h3dxc7xgrbmkr757q0g87l8ndzq";
+    rev = "3c3859dac5da111e57a6de09764ffdb127197c4a";
+  };
   postUnpack = "sourceRoot+=/hcoord; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [ base mtl ];
   testHaskellDepends = [ base data-default hlint HUnit ieee754 mtl ];
