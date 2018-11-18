43f9faaa3d7f41bbdd877af3450d5e679d50bf48
Phil de Joux <philderbeast@gmail.com>
2018-11-18 11:23:24 -0500

    Add COMPLETE pragma for pattern (:%), fixes #91.

--- units/library/Flight/Ratio.hs
+++ units/library/Flight/Ratio.hs
@@ -5,5 +5,7 @@ module Flight.Ratio (pattern (:%)) where
 import Data.Ratio (Ratio, numerator, denominator)
 
 -- | SEE: http://stackoverflow.com/questions/33325370/why-cant-i-pattern-match-against-a-ratio-in-haskell
-pattern (:%) :: forall t. t -> t -> Ratio t
-pattern num :% denom <- (\x -> (numerator x, denominator x) -> (num, denom))
+pattern (:%) :: forall a. a -> a -> Ratio a
+pattern n :% d <- (\x -> (numerator x, denominator x) -> (n, d))
+
+-- {-# COMPLETE (:%) #-}
