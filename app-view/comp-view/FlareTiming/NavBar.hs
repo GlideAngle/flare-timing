eec891e6c03461356dac38625c9c45fa447f8de1
Phil de Joux <philderbeast@gmail.com>
2018-11-21 11:03:28 -0500

    Bump bulma to v0.7.2 and fixup navbar, #92.

--- app-view/comp-view/FlareTiming/NavBar.hs
+++ app-view/comp-view/FlareTiming/NavBar.hs
@@ -4,18 +4,13 @@ import Reflex.Dom (MonadWidget, elClass, el, text)
 
 navbar :: MonadWidget t m => m ()
 navbar =
-    elClass "nav" "nav has-shadow" $ do
-        elClass "div" "container" $ do
-            elClass "div" "nav-left" $ do
-                elClass "a" "nav-item disable" $ do
-                    elClass "span" "icon" $ do
-                        elClass "i" "fa fa-paper-plane-o" $ return ()
-                    el "span" $ do
-                        text "Flare Timing"
-            elClass "div" "nav-right nav-menu" $ do
-                elClass "a" "nav-item is-tab is-active" $ do
-                    text "Tasks"
-                elClass "a" "nav-item is-tab" $ do
-                    text "Scores"
-                elClass "a" "nav-item is-tab" $ do
-                    text "Breakdown"
+    elClass "nav" "navbar" $ do
+        elClass "div" "navbar-brand" $ do
+            elClass "a" "navbar-item" $ do
+                elClass "span" "icon" $ do
+                    elClass "i" "fa fa-paper-plane-o" $ return ()
+                el "span" $ text "Flare Timing"
+        elClass "div" "navbar-menu is-active" $ do
+            elClass "a" "navbar-item " $ text "Tasks"
+            elClass "a" "navbar-item" $ text "Scores"
+            elClass "a" "navbar-item" $ text "Breakdown"
