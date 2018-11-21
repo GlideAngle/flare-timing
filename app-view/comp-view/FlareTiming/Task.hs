d9ce80032af1992ba60d6606fc29837537032d20
Phil de Joux <philderbeast@gmail.com>
2018-11-21 17:39:05 -0500

    Revert to showing a simple ordered list of tasks, #92.

--- app-view/comp-view/FlareTiming/Task.hs
+++ app-view/comp-view/FlareTiming/Task.hs
@@ -49,91 +49,27 @@ getSpeedSection (Task _ Zones{raw = tps} ss) =
                 start' = fromInteger start
                 end' = fromInteger end
 
-rainbow :: [String]
-rainbow =
-    [ "is-primary"
-    , "is-warning"
-    , "is-info"
-    , "is-danger"
-    , "is-success"
-    ]
-
-color :: Int -> String
-color ii = rainbow !! (ii `mod` length rainbow)
-
-taskTpNames
-    :: forall t (m :: * -> *). MonadWidget t m
-    => Dynamic t Bool
-    -> Dynamic t T.Text
-    -> Dynamic t [T.Text]
-    -> m ()
-taskTpNames detailed ii tps = do
-    let e :: Event t Bool = updated detailed
-    let f = do elClass "p" "subtitle" $ dynText ii; listTpNames tps
-
-    widgetHold_ f
-        $ fmap (\case
-            False -> f
-            True -> labelTpNames ii tps) e
-
-listTpNames
-    :: MonadWidget t m
-    => Dynamic t [T.Text]
-    -> m ()
-listTpNames tps = do
-    el "ul" $ do
-        _ <- simpleList tps (el "li" . dynText)
-        return ()
-    return ()
-
-labelTpNames
-    :: MonadWidget t m
-    => Dynamic t T.Text
-    -> Dynamic t [T.Text]
-    -> m ()
-labelTpNames ii tps = do
-    xs <- sample . current $ tps
-    jj <- sample . current $ ii
-    text $ jj <> ": " <> T.intercalate " - " xs
-    return ()
-
 task
     :: forall t (m :: * -> *). MonadWidget t m
     => Dynamic t (Int, Task)
-    -> m (Dynamic t Bool)
+    -> m ()
 task ix = do
     ii :: Int <- sample . current $ fst <$> ix
-    let jj = show ii
+    let jj = T.pack $ show ii
     let x :: Dynamic t Task = snd <$> ix
     let xs = getSpeedSection <$> x
     let tps = (fmap . fmap) (T.pack . TP.getName) xs
+    zs <- sample . current $ tps
 
-    mdo
-        (aa, _) <- elDynClass' "div" cc $ do
-            elClass "div" "tile is-parent is-vertical" $
-                elClass "div" (T.pack $ "tile is-child notification " ++ color ii) $ do
-                    taskTpNames ee dd tps
-
-        ee
-            :: Dynamic t Bool
-            <- toggle False $ domEvent Click aa
-
-        cc
-            :: Dynamic t T.Text
-            <- return $ (\case False -> "tile is-1"; True -> "tile is-12") <$> ee
-
-        dd
-            :: Dynamic t T.Text
-            <- return $ T.pack . (\case False -> jj; True -> "Task " ++ jj) <$> ee
-
-        return ee
+    el "li" $ do
+        el "a" . text $ T.intercalate " - " zs
 
 tasks :: MonadWidget t m => m ()
 tasks = do
     pb :: Event t () <- getPostBuild
     elClass "div" "spacer" $ return ()
     elClass "div" "container" $ do
-        _ <- el "ul" $ do widgetHold loading $ fmap getTasks pb
+        widgetHold loading $ fmap getTasks pb
         elClass "div" "spacer" $ return ()
 
     return ()
@@ -149,13 +85,7 @@ getTasks () = do
     xs :: Dynamic t [Task] <- holdDyn [] es
     let ys :: Dynamic t [(Int, Task)] = fmap (zip [1 .. ]) xs
 
-    _ <-
-        elAttr
-            "div"
-            (union
-                ("class" =: "tile is-ancestor")
-                ("style" =: "flex-wrap: wrap;")
-            ) $ do
-        simpleList ys task
+    elClass "h3" "subtitle is-3" $ text "Tasks"
+    elAttr "ol" ("type" =: "1") $ simpleList ys task
 
     return ()
