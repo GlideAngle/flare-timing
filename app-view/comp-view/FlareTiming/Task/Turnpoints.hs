6476b8024dfd3478dedeca169c5eba67a9ea3497
Phil de Joux <philderbeast@gmail.com>
2018-11-23 11:42:37 -0500

    Show the turnpoint number in the "No" column, #92.

--- app-view/comp-view/FlareTiming/Task/Turnpoints.hs
+++ app-view/comp-view/FlareTiming/Task/Turnpoints.hs
@@ -1,6 +1,7 @@
 module FlareTiming.Task.Turnpoints (tableTurnpoints) where
 
 import Reflex.Dom
+import qualified Data.Text as T (pack)
 
 import Data.Flight.Types (Task(..), Zones(..), RawZone(..))
 import qualified FlareTiming.Turnpoint as TP
@@ -10,7 +11,8 @@ tableTurnpoints
     => Dynamic t Task
     -> m ()
 tableTurnpoints x = do
-    let zs = getZones <$> x
+    let zs = fmap (zip [1..]) $ getZones <$> x
+
     _ <- elClass "table" "table" $
             el "thead" $ do
                 el "tr" $ do
@@ -28,11 +30,12 @@ getZones (Task _ Zones{raw} _) = raw
 
 row
     :: MonadWidget t m
-    => Dynamic t RawZone
+    => Dynamic t (Integer, RawZone)
     -> m ()
-row z = do
+row iz = do
+    let z = snd <$> iz
     el "tr" $ do
-        el "td" $ text "-"
+        el "td" $ dynText $ T.pack . show . fst <$> iz
         el "td" . dynText $ TP.getName <$> z
         el "td" . dynText $ TP.getRadius <$> z
         el "td" . dynText $ TP.getLat <$> z
