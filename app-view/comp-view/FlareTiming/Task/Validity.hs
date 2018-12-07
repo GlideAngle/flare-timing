8f3928ad2a6b9427b800a9155ca05b188d69da47
Phil de Joux <philderbeast@gmail.com>
2018-12-07 08:23:36 -0500

    Display tags for distance validity working, #95.

--- app-view/comp-view/FlareTiming/Task/Validity.hs
+++ app-view/comp-view/FlareTiming/Task/Validity.hs
@@ -1,11 +1,16 @@
 module FlareTiming.Task.Validity (viewValidity) where
 
+import Prelude hiding (sum)
 import Reflex
 import Reflex.Dom
 import qualified Data.Text as T (Text, pack)
 
 import qualified WireTypes.Validity as Vy (Validity(..), showLaunchValidity)
-import WireTypes.ValidityWorking (ValidityWorking(..), LaunchValidityWorking(..))
+import WireTypes.ValidityWorking
+    ( ValidityWorking(..)
+    , LaunchValidityWorking(..)
+    , DistanceValidityWorking(..)
+    )
 
 launchWorking :: Vy.Validity -> T.Text
 launchWorking vy =
@@ -31,16 +36,28 @@ viewValidity vy vw = do
         case (x, y) of
             (Nothing, _) -> text "Loading validity ..."
             (_, Nothing) -> text "Loading validity workings ..."
-            (Just vy', Just vw') -> viewLaunch vy' vw')
+            (Just vy', Just vw') -> do
+                elAttr
+                    "a"
+                    (("class" =: "button") <> ("onclick" =: launchWorking vy'))
+                    (text "Show Working")
+
+                elAttr
+                    "div"
+                    ("id" =: "launch-working")
+                    (text "")
+
+                viewLaunch vw'
+                viewDistance vw'
+                return ())
 
     return ()
 
 viewLaunch
     :: DomBuilder t m
-    => Vy.Validity
-    -> ValidityWorking
+    => ValidityWorking
     -> m ()
-viewLaunch vy ValidityWorking{launch = LaunchValidityWorking{..}} = do
+viewLaunch ValidityWorking{launch = LaunchValidityWorking{..}} = do
     elClass "div" "field is-grouped is-grouped-multiline" $ do
         elClass "div" "control" $ do
             elClass "div" "tags has-addons" $ do
@@ -58,14 +75,49 @@ viewLaunch vy ValidityWorking{launch = LaunchValidityWorking{..}} = do
                 elClass "span" "tag is-primary"
                     $ text (T.pack . show $ nominalLaunch)
 
-    elAttr
-        "a"
-        (("class" =: "button") <> ("onclick" =: launchWorking vy))
-        (text "Show Working")
+    return ()
 
-    elAttr
-        "div"
-        ("id" =: "launch-working")
-        (text "")
+viewDistance
+    :: DomBuilder t m
+    => ValidityWorking
+    -> m ()
+viewDistance ValidityWorking{distance = DistanceValidityWorking{..}} = do
+    elClass "div" "field is-grouped is-grouped-multiline" $ do
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "pilots flying"
+                elClass "span" "tag is-info"
+                    $ text (T.pack . show $ flying)
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "area"
+                elClass "span" "tag is-success"
+                    $ text (T.pack . show $ area)
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "nominal goal"
+                elClass "span" "tag is-primary"
+                    $ text (T.pack . show $ nominalGoal)
+    elClass "div" "field is-grouped is-grouped-multiline" $ do
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "sum of distance"
+                elClass "span" "tag is-dark"
+                    $ text (T.pack . show $ sum)
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "nominal distance"
+                elClass "span" "tag is-dark"
+                    $ text (T.pack . show $ nominalDistance)
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "minimum distance"
+                elClass "span" "tag is-dark"
+                    $ text (T.pack . show $ minimumDistance)
+        elClass "div" "control" $ do
+            elClass "div" "tags has-addons" $ do
+                elClass "span" "tag" $ do text "best distance"
+                elClass "span" "tag is-dark"
+                    $ text (T.pack . show $ bestDistance)
 
     return ()
--- app-view/comp-view/WireTypes/ValidityWorking.hs
+++ app-view/comp-view/WireTypes/ValidityWorking.hs
@@ -2,19 +2,31 @@
 
 module WireTypes.ValidityWorking
     ( ValidityWorking(..)
+    -- * Launch Validity Working
     , LaunchValidityWorking(..)
-    , PilotsPresent()
-    , PilotsFlying()
+    , PilotsPresent(..)
+    , PilotsFlying(..)
+    -- * Distance Validity Working
+    , DistanceValidityWorking(..)
+    , SumOfDistance(..)
+    , NominalDistanceArea(..)
+    , NominalGoal(..)
+    , NominalDistance(..)
+    , MinimumDistance(..)
+    , MaximumDistance(..)
     ) where
 
+import Control.Applicative (empty)
 import GHC.Generics (Generic)
-import Data.Aeson (ToJSON(..), FromJSON(..))
+import qualified Data.Text as T
+import Data.Aeson (Value(..), FromJSON(..))
 
 data ValidityWorking =
     ValidityWorking 
         { launch :: LaunchValidityWorking
+        , distance :: DistanceValidityWorking
         }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show, Generic, FromJSON)
 
 data LaunchValidityWorking =
     LaunchValidityWorking 
@@ -23,16 +35,93 @@ data LaunchValidityWorking =
         , nominalLaunch :: NominalLaunch
         }
     deriving (Eq, Ord, Show, Generic)
-    deriving anyclass (ToJSON, FromJSON)
+    deriving anyclass (FromJSON)
 
 newtype PilotsPresent = PilotsPresent Integer
     deriving (Eq, Ord)
-    deriving newtype (Show, ToJSON, FromJSON)
+    deriving newtype (Show, FromJSON)
 
 newtype PilotsFlying = PilotsFlying Integer
     deriving (Eq, Ord)
-    deriving newtype (Show, ToJSON, FromJSON)
+    deriving newtype (Show, FromJSON)
 
 newtype NominalLaunch = NominalLaunch Double
     deriving (Eq, Ord)
-    deriving newtype (Show, ToJSON, FromJSON)
+    deriving newtype (Show, FromJSON)
+
+data DistanceValidityWorking =
+    DistanceValidityWorking 
+        { sum :: SumOfDistance
+        , flying :: PilotsFlying
+        , area :: NominalDistanceArea
+        , nominalGoal :: NominalGoal
+        , nominalDistance :: NominalDistance
+        , minimumDistance :: MinimumDistance
+        , bestDistance :: MaximumDistance
+        }
+    deriving (Eq, Ord, Show, Generic)
+    deriving anyclass (FromJSON)
+
+newtype SumOfDistance = SumOfDistance Double
+    deriving (Eq, Ord)
+
+newtype NominalDistanceArea = NominalDistanceArea Double
+    deriving (Eq, Ord)
+    deriving newtype (Show, FromJSON)
+
+newtype NominalGoal = NominalGoal Double
+    deriving (Eq, Ord)
+    deriving newtype (Show, FromJSON)
+
+newtype NominalDistance = NominalDistance Double
+    deriving (Eq, Ord)
+
+newtype MinimumDistance = MinimumDistance Double
+    deriving (Eq, Ord)
+
+newtype MaximumDistance = MaximumDistance Double
+    deriving (Eq, Ord)
+
+instance Show SumOfDistance where
+    show (SumOfDistance x) = show x ++ " km"
+
+instance Show NominalDistance where
+    show (NominalDistance x) = show x ++ " km"
+
+instance Show MinimumDistance where
+    show (MinimumDistance x) = show x ++ " km"
+
+instance Show MaximumDistance where
+    show (MaximumDistance x) = show x ++ " km"
+
+instance FromJSON SumOfDistance where
+    parseJSON x@(String _) = do
+        s <- reverse . T.unpack <$> parseJSON x
+        case s of
+            'm' : 'k' : ' ' : xs -> return . SumOfDistance . read . reverse $ xs
+            _ -> empty
+    parseJSON _ = empty
+
+instance FromJSON NominalDistance where
+    parseJSON x@(String _) = do
+        s <- reverse . T.unpack <$> parseJSON x
+        case s of
+            'm' : 'k' : ' ' : xs -> return . NominalDistance . read . reverse $ xs
+            _ -> empty
+    parseJSON _ = empty
+
+instance FromJSON MinimumDistance where
+    parseJSON x@(String _) = do
+        s <- reverse . T.unpack <$> parseJSON x
+        case s of
+            'm' : 'k' : ' ' : xs -> return . MinimumDistance . read . reverse $ xs
+            _ -> empty
+    parseJSON _ = empty
+
+instance FromJSON MaximumDistance where
+    parseJSON x@(String _) = do
+        s <- reverse . T.unpack <$> parseJSON x
+        case s of
+            'm' : 'k' : ' ' : xs -> return . MaximumDistance . read . reverse $ xs
+            _ -> empty
+    parseJSON _ = empty
