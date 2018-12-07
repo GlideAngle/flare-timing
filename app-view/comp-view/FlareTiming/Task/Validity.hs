87ca079995bd91827b3cf1809c9ba52636eefa6f
Phil de Joux <philderbeast@gmail.com>
2018-12-07 08:41:55 -0500

    Use cards to separate validities, #95.

--- app-view/comp-view/FlareTiming/Task/Validity.hs
+++ app-view/comp-view/FlareTiming/Task/Validity.hs
@@ -26,6 +26,9 @@ launchWorking vy =
     <> ", getElementById('launch-working')"
     <> ", {throwOnError: false});"
 
+spacer :: DomBuilder t m => m ()
+spacer = elClass "div" "spacer" $ return ()
+
 viewValidity
     :: MonadWidget t m
     => Dynamic t (Maybe Vy.Validity)
@@ -47,8 +50,11 @@ viewValidity vy vw = do
                     ("id" =: "launch-working")
                     (text "")
 
+                spacer
                 viewLaunch vw'
+                spacer
                 viewDistance vw'
+                spacer
                 return ())
 
     return ()
@@ -58,22 +64,25 @@ viewLaunch
     => ValidityWorking
     -> m ()
 viewLaunch ValidityWorking{launch = LaunchValidityWorking{..}} = do
-    elClass "div" "field is-grouped is-grouped-multiline" $ do
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "pilots flying"
-                elClass "span" "tag is-info"
-                    $ text (T.pack . show $ flying)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "pilots present"
-                elClass "span" "tag is-success"
-                    $ text (T.pack . show $ present)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "nominal launch"
-                elClass "span" "tag is-primary"
-                    $ text (T.pack . show $ nominalLaunch)
+    elClass "div" "card" $ do
+        elClass "div" "card-content" $ do
+            elClass "h2" "title is-4" $ text "Launch Validity"
+            elClass "div" "field is-grouped is-grouped-multiline" $ do
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "pilots flying"
+                        elClass "span" "tag is-info"
+                            $ text (T.pack . show $ flying)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "pilots present"
+                        elClass "span" "tag is-success"
+                            $ text (T.pack . show $ present)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "nominal launch"
+                        elClass "span" "tag is-primary"
+                            $ text (T.pack . show $ nominalLaunch)
 
     return ()
 
@@ -82,42 +91,45 @@ viewDistance
     => ValidityWorking
     -> m ()
 viewDistance ValidityWorking{distance = DistanceValidityWorking{..}} = do
-    elClass "div" "field is-grouped is-grouped-multiline" $ do
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "pilots flying"
-                elClass "span" "tag is-info"
-                    $ text (T.pack . show $ flying)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "area"
-                elClass "span" "tag is-success"
-                    $ text (T.pack . show $ area)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "nominal goal"
-                elClass "span" "tag is-primary"
-                    $ text (T.pack . show $ nominalGoal)
-    elClass "div" "field is-grouped is-grouped-multiline" $ do
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "sum of distance"
-                elClass "span" "tag is-dark"
-                    $ text (T.pack . show $ sum)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "nominal distance"
-                elClass "span" "tag is-dark"
-                    $ text (T.pack . show $ nominalDistance)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "minimum distance"
-                elClass "span" "tag is-dark"
-                    $ text (T.pack . show $ minimumDistance)
-        elClass "div" "control" $ do
-            elClass "div" "tags has-addons" $ do
-                elClass "span" "tag" $ do text "best distance"
-                elClass "span" "tag is-dark"
-                    $ text (T.pack . show $ bestDistance)
+    elClass "div" "card" $ do
+        elClass "div" "card-content" $ do
+            elClass "h2" "title is-4" $ text "Distance Validity"
+            elClass "div" "field is-grouped is-grouped-multiline" $ do
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "pilots flying"
+                        elClass "span" "tag is-info"
+                            $ text (T.pack . show $ flying)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "area"
+                        elClass "span" "tag is-success"
+                            $ text (T.pack . show $ area)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "nominal goal"
+                        elClass "span" "tag is-primary"
+                            $ text (T.pack . show $ nominalGoal)
+            elClass "div" "field is-grouped is-grouped-multiline" $ do
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "sum of distance"
+                        elClass "span" "tag is-dark"
+                            $ text (T.pack . show $ sum)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "nominal distance"
+                        elClass "span" "tag is-dark"
+                            $ text (T.pack . show $ nominalDistance)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "minimum distance"
+                        elClass "span" "tag is-dark"
+                            $ text (T.pack . show $ minimumDistance)
+                elClass "div" "control" $ do
+                    elClass "div" "tags has-addons" $ do
+                        elClass "span" "tag" $ do text "best distance"
+                        elClass "span" "tag is-dark"
+                            $ text (T.pack . show $ bestDistance)
 
     return ()
