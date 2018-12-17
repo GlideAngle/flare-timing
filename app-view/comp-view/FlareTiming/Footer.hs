9fbeeb03c7a0a173807a16a071c11ecd3f943a02
Phil de Joux <philderbeast@gmail.com>
2018-12-17 17:08:08 -0500

    Add a link to Block Scope in the footer.

--- app-view/comp-view/FlareTiming/Footer.hs
+++ app-view/comp-view/FlareTiming/Footer.hs
@@ -1,38 +1,52 @@
 module FlareTiming.Footer (footer) where
 
 import Reflex.Dom
-import Data.Map (union)
 
 attribution :: MonadWidget t m => m () 
-attribution = do
-    el "p" $ do
-        el "small" $ do
-            text "Map data: © "
-            elAttr "a" (union ("href" =: "http://www.openstreetmap.org/copyright")
-                              ("target" =: "_blank")) $ do
-                text "OpenStreetMap"
+attribution =
+    elClass "div" "is-size-7" $ do
+    el "strong" $ text "Flare Timing"
+    text " by "
+    elAttr
+        "a"
+        ("href" =: "http://www.blockscope.com" <> "target" =: "_blank")
+        $ text "Block Scope"
 
-            text ", "
-            elAttr "a" (union ("href" =: "http://viewfinderpanoramas.org")
-                              ("target" =: "_blank")) $ do
-                text "SRTM"
+    el "br" $ return ()
+    text "Map data © "
 
-            text " | Map style: © "
-            elAttr "a" (union ("href" =: "https://opentopomap.org")
-                              ("target" =: "_blank")) $ do
-                text "OpenTopoMap"
+    elAttr
+        "a"
+        ("href" =: "http://www.openstreetmap.org/copyright" <> "target" =: "_blank")
+        $ text "OpenStreetMap"
 
-            text " ("
-            elAttr "a" (union ("href" =: "https://creativecommons.org/licenses/by-sa/3.0/")
-                              ("target" =: "_blank")) $ do
-                text "CC-BY-SA"
+    text ", "
 
-            text ")"
+    elAttr
+        "a"
+        ("href" =: "http://viewfinderpanoramas.org" <> "target" =: "_blank")
+        $ text "SRTM"
+
+    el "br" $ return ()
+    text "Map style © "
+
+    elAttr
+        "a" ("href" =: "https://opentopomap.org" <> "target" =: "_blank")
+        $ text "OpenTopoMap"
+
+    text " ("
+
+    elAttr
+        "a"
+        ("href" =: "https://creativecommons.org/licenses/by-sa/3.0/" <> "target" =: "_blank")
+        $ text "CC-BY-SA"
+
+    text ")"
 
 
 footer :: MonadWidget t m => m ()
 footer =
     elClass "footer" "footer" $ do
         elClass "div" "container" $ do
-            elClass "div" "content has-text-centered" $ do
+            elClass "div" "content" $ do
                 attribution
