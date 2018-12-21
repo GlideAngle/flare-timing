db22a5d7ab35607a178635b68e3aa96cea4869f7
Phil de Joux <philderbeast@gmail.com>
2018-12-21 15:01:35 -0500

    Bump version to app-view-0.4 2018-12-21T13:36.

--- app-view/comp-view/FlareTiming/Footer.hs
+++ app-view/comp-view/FlareTiming/Footer.hs
@@ -5,7 +5,7 @@ import Reflex.Dom
 attribution :: MonadWidget t m => m () 
 attribution =
     elClass "div" "is-size-7" $ do
-    elAttr "strong" ("title" =: "app-view-0.3 2018-12-19T10:27") $ text "Flare Timing"
+    elAttr "strong" ("title" =: "app-view-0.4 2018-12-21T13:36") $ text "Flare Timing"
     text " by "
     elAttr
         "a"
