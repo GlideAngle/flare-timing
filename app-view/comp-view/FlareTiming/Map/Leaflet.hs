0479bf73b5f8df21a0f28d3531e6815146ebedb7
Phil de Joux <philderbeast@gmail.com>
2018-12-24 07:52:51 -0500

    Setup the layers control FFI using easier to read multiline string, #115.

--- app-view/comp-view/FlareTiming/Map/Leaflet.hs
+++ app-view/comp-view/FlareTiming/Map/Leaflet.hs
@@ -62,7 +62,13 @@ foreign import javascript unsafe
     tileLayerAddToMap_ :: JSVal -> JSVal -> IO ()
 
 foreign import javascript unsafe
-    "L.control.layers({'Course line (point to point)': $3, 'Course line (shortest route)': $4, 'Speed section line (course line subset)': $5, 'Speed section line (task waypoints subset)': $6}, {'Map': $1}).addTo($2)"
+    "L.control.layers(\
+    \{ 'Course line (point to point)': $3\
+    \, 'Course line (shortest route)': $4\
+    \, 'Speed section line (course line subset)': $5\
+    \, 'Speed section line (task waypoints subset)': $6\
+    \}\
+    \, {'Map': $1}).addTo($2)"
     layersControl_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO ()
 
 foreign import javascript unsafe
