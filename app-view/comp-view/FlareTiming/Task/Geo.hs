75d0a53ce4d0917b963eea5e222382e81d9dde75
Phil de Joux <philderbeast@gmail.com>
2019-01-04 20:00:57 -0500

    Show the distance and legs for the other methods, #141.

--- app-view/comp-view/FlareTiming/Task/Geo.hs
+++ app-view/comp-view/FlareTiming/Task/Geo.hs
@@ -5,7 +5,10 @@ import Reflex.Dom
 import qualified Data.Text as T (pack)
 
 import FlareTiming.Comms
-import WireTypes.Route (TaskLength(..), taskLength, taskLegs, showTaskDistance)
+import WireTypes.Route
+    ( TaskLength(..), TrackLine(..), PlanarTrackLine(..), TaskLegs(..)
+    , taskLength, taskLegs, showTaskDistance
+    )
 import FlareTiming.Events (IxTask(..))
 
 tableGeo
@@ -19,18 +22,93 @@ tableGeo ix = do
             elClass "div" "content" $
                 tableCmp ix
 
-tableCmp
+rowSpherical
     :: MonadWidget t m
     => IxTask
     -> m ()
-tableCmp ix = do
+rowSpherical ix = do
     let lnTask = getTaskLengthSphericalEdge ix
     ln <- (fmap . fmap) taskLength lnTask
     let d = ffor ln (maybe "" $ \TaskLength{..} ->
                 showTaskDistance taskRoute)
 
-    legs <- (fmap . fmap) (T.pack . show . length . taskLegs) lnTask
+    legs <- (fmap . fmap) ((maybe "" $ T.pack . show . length . (\TaskLegs{legs} -> legs)) . taskLegs) lnTask
 
+    el "tr" $ do
+        el "th" $ text "Spherical"
+        elClass "th" "td-geo-distance" $ dynText d
+        elClass "th" "td-geo-legs" $ dynText legs
+
+rowEllipsoid
+    :: MonadWidget t m
+    => IxTask
+    -> m ()
+rowEllipsoid ix = do
+    let lnTask = getTaskLengthEllipsoidEdge ix
+    ln <- (fmap . fmap) taskLength lnTask
+    let d = ffor ln (maybe "" $ \TaskLength{..} ->
+                showTaskDistance taskRoute)
+
+    legs <- (fmap . fmap) ((maybe "" $ T.pack . show . length . (\TaskLegs{legs} -> legs)) . taskLegs) lnTask
+
+    el "tr" $ do
+        el "th" $ text "Ellipsoid"
+        elClass "th" "td-geo-distance" $ dynText d
+        elClass "th" "td-geo-legs" $ dynText legs
+
+rowProjectedSphere
+    :: MonadWidget t m
+    => IxTask
+    -> m ()
+rowProjectedSphere ix = do
+    ln <- getTaskLengthProjectedEdgeSpherical ix
+    let d = ffor ln (maybe "" $ \TrackLine{distance = x} -> showTaskDistance x)
+
+    let legs =
+            ffor ln (maybe "" $ T.pack . show . length . (\TrackLine{legs = xs} -> xs))
+
+    el "tr" $ do
+        el "th" $ text "Projected sphere"
+        elClass "th" "td-geo-distance" $ dynText d
+        elClass "th" "td-geo-legs" $ dynText legs
+
+rowProjectedEllipsoid
+    :: MonadWidget t m
+    => IxTask
+    -> m ()
+rowProjectedEllipsoid ix = do
+    ln <- getTaskLengthProjectedEdgeEllipsoid ix
+    let d = ffor ln (maybe "" $ \TrackLine{distance = x} -> showTaskDistance x)
+
+    let legs =
+            ffor ln (maybe "" $ T.pack . show . length . (\TrackLine{legs = xs} -> xs))
+
+    el "tr" $ do
+        el "th" $ text "Projected ellipsoid"
+        elClass "th" "td-geo-distance" $ dynText d
+        elClass "th" "td-geo-legs" $ dynText legs
+
+rowProjectedPlanar
+    :: MonadWidget t m
+    => IxTask
+    -> m ()
+rowProjectedPlanar ix = do
+    ln <- getTaskLengthProjectedEdgePlanar ix
+    let d = ffor ln (maybe "" $ \PlanarTrackLine{distance = x} -> showTaskDistance x)
+
+    let legs =
+            ffor ln (maybe "" $ T.pack . show . length . (\PlanarTrackLine{legs = xs} -> xs))
+
+    el "tr" $ do
+        el "th" $ text "Projected planar"
+        elClass "th" "td-geo-distance" $ dynText d
+        elClass "th" "td-geo-legs" $ dynText legs
+
+tableCmp
+    :: MonadWidget t m
+    => IxTask
+    -> m ()
+tableCmp ix = do
     _ <- elClass "table" "table is-striped" $ do
             el "thead" $ do
                 el "tr" $ do
@@ -39,30 +117,12 @@ tableCmp ix = do
                     elClass "th" "th-geo-legs" $ text "Legs"
 
             _ <- el "tbody" $ do
-                el "tr" $ do
-                    el "th" $ text "Spherical"
-                    elClass "th" "td-geo-distance" $ dynText d
-                    elClass "th" "td-geo-legs" $ dynText legs
+                rowSpherical ix
+                rowEllipsoid ix
 
-                el "tr" $ do
-                    el "th" $ text "Ellipsoid"
-                    elClass "th" "td-geo-distance" $ text "102.9 km"
-                    elClass "th" "td-geo-legs" $ text "3"
-
-                el "tr" $ do
-                    el "th" $ text "Projection spherical"
-                    elClass "th" "td-geo-distance" $ text "102.9 km"
-                    elClass "th" "td-geo-legs" $ text "3"
-
-                el "tr" $ do
-                    el "th" $ text "Projection ellipsoid"
-                    elClass "th" "td-geo-distance" $ text "102.9 km"
-                    elClass "th" "td-geo-legs" $ text "3"
-
-                el "tr" $ do
-                    el "th" $ text "Projection planar"
-                    elClass "th" "td-geo-distance" $ text "102.9 km"
-                    elClass "th" "td-geo-legs" $ text "3"
+                rowProjectedSphere ix
+                rowProjectedEllipsoid ix
+                rowProjectedPlanar ix
 
             return ()
 
