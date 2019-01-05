ef9985e7c1fcb0173e1dc6989f5048a21f3971f4
Phil de Joux <philderbeast@gmail.com>
2019-01-05 07:14:15 -0500

    Add another row of headers, footers and more style, #141.

--- app-view/comp-view/FlareTiming/Task/Geo.hs
+++ app-view/comp-view/FlareTiming/Task/Geo.hs
@@ -17,7 +17,7 @@ tableGeo
     => IxTask
     -> m ()
 tableGeo ix = do
-    elClass "div" "tile is-parent is-6" $ do
+    elClass "div" "tile is-parent" $ do
         elClass "article" "tile is-child box" $ do
             elClass "p" "title" $ text "Geo distance comparison"
             elClass "div" "content" $
@@ -26,9 +26,10 @@ tableGeo ix = do
 rowOptimal
     :: MonadWidget t m
     => T.Text
+    -> T.Text
     -> m (Dynamic t (OptimalRoute (Maybe TrackLine)))
     -> m ()
-rowOptimal rowHeader lnTask = do
+rowOptimal earth algo  lnTask = do
     ln <- (fmap . fmap) taskLength lnTask
     let d = ffor ln (maybe "" $ \TaskLength{..} ->
                 showTaskDistance taskRoute)
@@ -36,29 +37,38 @@ rowOptimal rowHeader lnTask = do
     legs <- (fmap . fmap) ((maybe "" $ T.pack . show . length . (\TaskLegs{legs} -> legs)) . taskLegs) lnTask
 
     el "tr" $ do
-        el "th" $ text rowHeader
+        el "td" $ text earth
+        el "td" $ text algo
+        el "td" $ text earth
+        el "td" $ text algo
         elClass "td" "td-geo-distance" $ dynText d
         elClass "td" "td-geo-legs" $ dynText legs
 
 rowSpherical :: MonadWidget t m => IxTask -> m ()
-rowSpherical = rowOptimal "Spherical" . getTaskLengthSphericalEdge
+rowSpherical = rowOptimal "Sphere" "Haversines" . getTaskLengthSphericalEdge
 
 rowEllipsoid :: MonadWidget t m => IxTask -> m ()
-rowEllipsoid = rowOptimal "Ellipsoid" . getTaskLengthEllipsoidEdge
+rowEllipsoid = rowOptimal "Ellipsoid" "Vincenty" . getTaskLengthEllipsoidEdge
 
 rowTrackLine
     :: MonadWidget t m
     => T.Text
+    -> T.Text
+    -> T.Text
+    -> T.Text
     -> Dynamic t (Maybe TrackLine)
     -> m ()
-rowTrackLine rowHeader ln = do
+rowTrackLine earthIn algoIn earthOut algoOut ln = do
     let d = ffor ln (maybe "" $ \TrackLine{distance = x} -> showTaskDistance x)
 
     let legs =
             ffor ln (maybe "" $ T.pack . show . length . (\TrackLine{legs = xs} -> xs))
 
     el "tr" $ do
-        el "th" $ text rowHeader
+        el "td" $ text earthIn
+        el "td" $ text algoIn
+        el "td" $ text earthOut
+        el "td" $ text algoOut
         elClass "td" "td-geo-distance" $ dynText d
         elClass "td" "td-geo-legs" $ dynText legs
 
@@ -68,7 +78,7 @@ rowProjectedSphere
     -> m ()
 rowProjectedSphere ix = do
     ln  <- getTaskLengthProjectedEdgeSpherical ix
-    rowTrackLine "Projected sphere" ln
+    rowTrackLine "Plane" "Pythagorus" "Sphere" "Haversines" ln
 
 rowProjectedEllipsoid
     :: MonadWidget t m
@@ -76,7 +86,7 @@ rowProjectedEllipsoid
     -> m ()
 rowProjectedEllipsoid ix = do
     ln <- getTaskLengthProjectedEdgeEllipsoid ix
-    rowTrackLine "Projected ellipsoid" ln
+    rowTrackLine "Plane" "Pythagorus" "Ellipsoid" "Vincenty" ln
 
 rowProjectedPlanar
     :: MonadWidget t m
@@ -90,7 +100,10 @@ rowProjectedPlanar ix = do
             ffor ln (maybe "" $ T.pack . show . length . (\PlanarTrackLine{legs = xs} -> xs))
 
     el "tr" $ do
-        el "th" $ text "Projected planar"
+        el "td" $ text "Plane"
+        el "td" $ text "Pythagorus"
+        el "td" $ text "Plane"
+        el "td" $ text "Pythagorus"
         elClass "td" "td-geo-distance" $ dynText d
         elClass "td" "td-geo-legs" $ dynText legs
 
@@ -102,7 +115,15 @@ tableCmp ix = do
     _ <- elClass "table" "table is-striped" $ do
             el "thead" $ do
                 el "tr" $ do
-                    el "th" $ text "Method"
+                    elAttr "th" ("colspan" =: "2" <> "class" =: "th-geo-network") $ text "Network Paths Cost *"
+                    elAttr "th" ("colspan" =: "2" <> "class" =: "th-geo-path") $ text "Shortest Path Cost †"
+                    elAttr "th" ("colspan" =: "2" <> "class" =: "th-geo-how-far") $ text "Shortest Path Distance"
+
+                el "tr" $ do
+                    elClass "th" "th-geo-network-earth" $ text "Earth"
+                    elClass "th" "th-geo-network-algo" $ text "Algorithm"
+                    elClass "th" "th-geo-path-earth" $ text "Earth"
+                    elClass "th" "th-geo-path-algo" $ text "Algorithm"
                     elClass "th" "th-geo-distance" $ text "Distance"
                     elClass "th" "th-geo-legs" $ text "Legs"
 
@@ -113,6 +134,11 @@ tableCmp ix = do
                 rowProjectedSphere ix
                 rowProjectedEllipsoid ix
                 rowProjectedPlanar ix
+
+            let tr = el "tr" . elAttr "td" ("colspan" =: "6")
+            _ <- el "tfoot" $ do
+                tr $ text "* The Earth model and algorithm used when constructing a network of points with distance between pairs as the cost to be minimized when finding the shortest path through the network"
+                tr $ text "† The Earth model and algorithm used when reporting the sum of distances between pairs of points along the path as the path distance"
 
             return ()
 
