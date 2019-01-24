8c6be1c3f0b1bf309faa8e67ae1c3b60e7dd7072
Phil de Joux <philderbeast@gmail.com>
2019-01-24 06:52:02 -0500

    Show the penalties, #131.

--- app-view/comp-view/FlareTiming/Pilot.hs
+++ app-view/comp-view/FlareTiming/Pilot.hs
@@ -7,6 +7,8 @@ module FlareTiming.Pilot
 
 import Reflex.Dom
 import qualified Data.Text as T (Text, pack)
+import Data.List (find)
+import Text.Printf (printf)
 
 import WireTypes.Pilot (Pilot(..), PilotId(..), PilotName(..))
 import WireTypes.Point (PointPenalty(..))
@@ -25,14 +27,34 @@ rowPenal
     :: MonadWidget t m
     => Dynamic t (Pilot, [PointPenalty])
     -> m ()
-rowPenal x = do
-    let pilot = fst <$> x
-    let td = el "td" . dynText
-    el "tr" $ do
-        td $ showPilotId <$> pilot
-        td $ showPilotName <$> pilot
-        el "td" $ text ""
-        el "td" $ text ""
+rowPenal ppp = do
+    let td = el "td" . text . T.pack . printf "%.3f"
+    dyn_ $ ffor ppp (\(pilot, ps) -> el "tr" $ do
+        let p =
+                find
+                    (\case PenaltyFraction _ -> True; PenaltyPoints _ -> False)
+                    ps
+
+        let pp =
+                find
+                    (\case PenaltyFraction _ -> False; PenaltyPoints _ -> True)
+                    ps
+
+        el "td" . text . showPilotId $ pilot
+        el "td" . text . showPilotName $ pilot
+        case (p, pp) of
+            (Just (PenaltyFraction x), Nothing) -> do
+                td x
+                el "td" $ text ""
+            (Nothing, Just (PenaltyPoints y)) -> do
+                el "td" $ text ""
+                td y
+            (Just (PenaltyFraction x), Just (PenaltyPoints y)) -> do
+                td x
+                td y
+            _ -> do
+                el "td" $ text ""
+                el "td" $ text "")
 
 showPilotId :: Pilot -> T.Text
 showPilotId (Pilot (PilotId x, _)) = T.pack x
