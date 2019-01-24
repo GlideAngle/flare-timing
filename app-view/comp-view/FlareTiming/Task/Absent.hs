2d09dd444b4cdacb299e3fe862b4c4afd5528aaa
Phil de Joux <philderbeast@gmail.com>
2019-01-24 07:10:56 -0500

    Stack ABS, DNF & NYP vertically, #131.

--- app-view/comp-view/FlareTiming/Task/Absent.hs
+++ app-view/comp-view/FlareTiming/Task/Absent.hs
@@ -25,41 +25,59 @@ tableAbsent ix nyp' dnf' dfNt' penal' = do
     abs <- holdDyn [] =<< getTaskPilotAbs ix pb
 
     elClass "div" "tile is-ancestor" $ do
-        elClass "div" "tile is-parent" $ do
-            elClass "article" "tile is-child box" $ do
-                elClass "p" "title" $ text "ABS"
-                elClass "p" "subtitle" $ text "absent"
-                elClass "div" "content" $ do
-                    _ <- elClass "table" "table" $ do
-                            el "thead" $ do
-                                el "tr" $ do
-                                    el "th" $ text "Id"
-                                    el "th" $ text "Name"
+        elClass "div" "tile is-vertical is-4" $ do
+            elClass "div" "tile" $ do
+                elClass "div" "tile is-parent is-vertical" $ do
+                    elClass "article" "tile is-child box" $ do
+                        elClass "p" "title" $ text "ABS"
+                        elClass "p" "subtitle" $ text "absent"
+                        elClass "div" "content" $ do
+                            _ <- elClass "table" "table" $ do
+                                    el "thead" $ do
+                                        el "tr" $ do
+                                            el "th" $ text "Id"
+                                            el "th" $ text "Name"
 
-                                simpleList abs rowPilot
+                                        simpleList abs rowPilot
 
-                    el "p" . text
-                        $ "A pilot's absence does not reduce the task validity."
+                            el "p" . text
+                                $ "A pilot's absence does not reduce the task validity."
 
-                    return ()
+                            return ()
 
-        elClass "div" "tile is-parent" $ do
-            elClass "article" "tile is-child box" $ do
-                elClass "p" "title" $ text "DNF"
-                elClass "p" "subtitle" $ text "did not fly"
-                elClass "div" "content" $ do
-                    _ <- elClass "table" "table" $ do
-                            el "thead" $ do
-                                el "tr" $ do
-                                    el "th" $ text "Id"
-                                    el "th" $ text "Name"
+                    elClass "article" "tile is-child box" $ do
+                        elClass "p" "title" $ text "DNF"
+                        elClass "p" "subtitle" $ text "did not fly"
+                        elClass "div" "content" $ do
+                            _ <- elClass "table" "table" $ do
+                                    el "thead" $ do
+                                        el "tr" $ do
+                                            el "th" $ text "Id"
+                                            el "th" $ text "Name"
 
-                                simpleList dnf rowPilot
+                                        simpleList dnf rowPilot
 
-                    el "p" . text
-                        $ "Both launch validity and task validity are reduced when pilots elect not to fly. The reduction is made by taking the fraction of those not flying over those present at launch."
+                            el "p" . text
+                                $ "Both launch validity and task validity are reduced when pilots elect not to fly. The reduction is made by taking the fraction of those not flying over those present at launch."
 
-                    return ()
+                            return ()
+
+                    elClass "article" "tile is-child box" $ do
+                        elClass "p" "title" $ text "NYP"
+                        elClass "p" "subtitle" $ text "not yet processed"
+                        elClass "div" "content" $ do
+                            _ <- elClass "table" "table" $ do
+                                    el "thead" $ do
+                                        el "tr" $ do
+                                            el "th" $ text "Id"
+                                            el "th" $ text "Name"
+
+                                        simpleList nyp rowPilot
+
+                            el "p" . text
+                                $ "Unlike DNF pilots, these pilots do not decrease launch validity. When a task is not at full distance validity, if any one of the NYP pilots flew further then the task validity will increase when they are processed. Likewise for time validity and the fastest pilots being NYP."
+
+                            return ()
 
         elClass "div" "tile is-parent" $ do
             elClass "article" "tile is-child box" $ do
@@ -76,24 +94,6 @@ tableAbsent ix nyp' dnf' dfNt' penal' = do
 
                     el "p" . text
                         $ "These pilots get awarded minimum distance."
-
-                    return ()
-
-        elClass "div" "tile is-parent" $ do
-            elClass "article" "tile is-child box" $ do
-                elClass "p" "title" $ text "NYP"
-                elClass "p" "subtitle" $ text "not yet processed"
-                elClass "div" "content" $ do
-                    _ <- elClass "table" "table" $ do
-                            el "thead" $ do
-                                el "tr" $ do
-                                    el "th" $ text "Id"
-                                    el "th" $ text "Name"
-
-                                simpleList nyp rowPilot
-
-                    el "p" . text
-                        $ "Unlike DNF pilots, these pilots do not decrease launch validity. When a task is not at full distance validity, if any one of the NYP pilots flew further then the task validity will increase when they are processed. Likewise for time validity and the fastest pilots being NYP."
 
                     return ()
 
