66ed5b97c6d5d6255a8a12a5d9e60614f9d77657
Phil de Joux <philderbeast@gmail.com>
2019-02-12 16:26:25 -0500

    Changes sizes and show plots for HG and PG, #189.

--- app-view/comp-view/FlareTiming/Plot/FunctionPlot.hs
+++ app-view/comp-view/FlareTiming/Plot/FunctionPlot.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module FlareTiming.Plot.FunctionPlot
-    ( plot
+    ( hgPlot
+    , pgPlot
     ) where
 
 import Prelude hiding (map, log)
@@ -17,8 +18,8 @@ newtype Plot = Plot { unPlot :: JSVal }
 
 foreign import javascript unsafe
     "functionPlot(\
-    \{ target: '#plot'\
-    \, width: 800\
+    \{ target: '#hg-plot'\
+    \, width: 360\
     \, height: 400\
     \, disableZoom: true\
     \, xAxis: {label: 'Fraction of Pilots in Goal', domain: [0, 1]}\
@@ -45,8 +46,39 @@ foreign import javascript unsafe
     \  , graphType: 'polyline'\
     \  }\
     \]})"
-    plot_ :: JSVal -> IO JSVal
+    hgPlot_ :: JSVal -> IO JSVal
 
-plot :: IsElement e => e -> IO Plot
-plot e =
-    Plot <$> (plot_ . unElement . toElement $ e)
+hgPlot :: IsElement e => e -> IO Plot
+hgPlot e =
+    Plot <$> (hgPlot_ . unElement . toElement $ e)
+
+foreign import javascript unsafe
+    "functionPlot(\
+    \{ target: '#pg-plot'\
+    \, width: 360\
+    \, height: 400\
+    \, disableZoom: true\
+    \, xAxis: {label: 'Fraction of Pilots in Goal', domain: [0, 1]}\
+    \, yAxis: {label: 'Fraction of Available Points', domain: [0, 1]}\
+    \, data: [{\
+    \    fn: '0.9 - 1.665*x + 1.713*x^2 - 0.587*x^3'\
+    \  , nSamples: 101\
+    \  , color: 'blue'\
+    \  , graphType: 'polyline'\
+    \  },{\
+    \    fn: '(1 - (0.9 - 1.665*x + 1.713*x^2 - 0.587*x^3))/8 * 1.4 * 2'\
+    \  , nSamples: 101\
+    \  , color: 'red'\
+    \  , graphType: 'polyline'\
+    \  },{\
+    \    fn: '1 - ((0.9 - 1.665*x + 1.713*x^2 - 0.587*x^3) + (1 - (0.9 - 1.665*x + 1.713*x^2 - 0.587*x^3))/8 * 1.4 * 2)'\
+    \  , nSamples: 101\
+    \  , color: 'green'\
+    \  , graphType: 'polyline'\
+    \  }\
+    \]})"
+    pgPlot_ :: JSVal -> IO JSVal
+
+pgPlot :: IsElement e => e -> IO Plot
+pgPlot e =
+    Plot <$> (pgPlot_ . unElement . toElement $ e)
--- app-view/comp-view/FlareTiming/Plot/View.hs
+++ app-view/comp-view/FlareTiming/Plot/View.hs
@@ -5,7 +5,7 @@ import Reflex.Time (delay)
 
 import Control.Monad.IO.Class (liftIO)
 
-import qualified FlareTiming.Plot.FunctionPlot as P (plot)
+import qualified FlareTiming.Plot.FunctionPlot as P (hgPlot, pgPlot)
 
 viewPlot :: MonadWidget t m => m ()
 viewPlot = plot
@@ -13,10 +13,12 @@ viewPlot = plot
 plot :: MonadWidget t m => m ()
 plot = do
     pb <- delay 1 =<< getPostBuild
-    (ePlot, _) <- elAttr' "div" (("id" =: "plot") <> ("style" =: "height: 680px;width: 100%")) $ return ()
+    (hgPlot, _) <- elAttr' "div" (("id" =: "hg-plot") <> ("style" =: "height: 400px;width: 50%")) $ return ()
+    (pgPlot, _) <- elAttr' "div" (("id" =: "pg-plot") <> ("style" =: "height: 400px;width: 50%")) $ return ()
     rec performEvent_ $ leftmost
             [ ffor pb (\_ -> liftIO $ do
-                _ <- P.plot (_element_raw ePlot)
+                _ <- P.hgPlot (_element_raw hgPlot)
+                _ <- P.pgPlot (_element_raw pgPlot)
                 return ())
             ]
 
