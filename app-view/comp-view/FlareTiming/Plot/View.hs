519bff2363bb51fdf762d8ea262376672cb7168d
Phil de Joux <philderbeast@gmail.com>
2019-02-23 14:13:50 -0500

    Split module Plot.Weight from Plot.View, #190.

--- app-view/comp-view/FlareTiming/Plot/View.hs
+++ app-view/comp-view/FlareTiming/Plot/View.hs
@@ -1,24 +1,10 @@
 module FlareTiming.Plot.View (viewPlot) where
 
-import Text.Printf (printf)
 import Reflex.Dom
-import Reflex.Time (delay)
-import qualified Data.Text as T (Text, pack)
-
-import Control.Monad.IO.Class (liftIO)
-import qualified FlareTiming.Plot.FunctionPlot as P (hgPlot, pgPlot)
 
 import WireTypes.Comp (Discipline(..), Tweak(..))
-import WireTypes.Point
-    ( GoalRatio(..)
-    , Allocation(..)
-    , Weights(..)
-    , DistanceWeight(..)
-    , ArrivalWeight(..)
-    , LeadingWeight(..)
-    , TimeWeight(..)
-    , zeroWeights
-    )
+import WireTypes.Point (Allocation(..))
+import FlareTiming.Plot.Weight (hgPlot, pgPlot)
 
 viewPlot
     :: MonadWidget t m
@@ -36,85 +22,3 @@ viewPlot hgOrPg tweak alloc = do
                                 HangGliding -> hgPlot tweak alloc
                                 Paragliding -> pgPlot tweak alloc)
                         return ()
-
-textf :: String -> Double -> T.Text
-textf fmt d = T.pack $ printf fmt d
-
-hgPlot
-    :: MonadWidget t m
-    => Dynamic t (Maybe Tweak)
-    -> Dynamic t (Maybe Allocation)
-    -> m ()
-hgPlot tweak' alloc' = do
-    tweak <- sample . current $ tweak'
-    alloc <- sample . current $ alloc'
-    let gr@(GoalRatio gr') = maybe (GoalRatio 0) goalRatio alloc
-    let w = maybe zeroWeights weight alloc
-    pb <- delay 1 =<< getPostBuild
-    (elPlot, _) <- elAttr' "div" (("id" =: "hg-plot") <> ("style" =: "height: 360px;width: 360px")) $ return ()
-    rec performEvent_ $ leftmost
-            [ ffor pb (\_ -> liftIO $ do
-                _ <- P.hgPlot (_element_raw elPlot) tweak gr w
-                return ())
-            ]
-
-    let Weights
-            { distance = DistanceWeight d
-            , arrival = ArrivalWeight a
-            , leading = LeadingWeight l
-            , time = TimeWeight t
-            } = w
-
-    elClass "div" "level" $ do
-        elClass "div" "level-left" $
-            elClass "div" "level-item" $
-                el "ul" $ do
-                    elClass "li" "legend-goal-ratio" . text $
-                        textf "%.3f arrival ratio ⇒" gr'
-        elClass "div" "level-right" $
-            elClass "div" "level-item" $
-                el "ul" $ do
-                    elClass "li" "legend-distance" . text $
-                        textf "○ %.3f weight on distance" d
-                    elClass "li" "legend-time" . text $
-                        textf "○ %.3f weight on time" t
-                    elClass "li" "legend-leading" . text $
-                        textf "○ %.3f weight on leading" l
-                    elClass "li" "legend-arrival" . text $
-                        textf "○ %.3f weight on arrival" a
-
-    return ()
-
-pgPlot
-    :: MonadWidget t m
-    => Dynamic t (Maybe Tweak)
-    -> Dynamic t (Maybe Allocation)
-    -> m ()
-pgPlot tweak' alloc' = do
-    tweak <- sample . current $ tweak'
-    alloc <- sample . current $ alloc'
-    let w = maybe zeroWeights weight alloc
-    pb <- delay 1 =<< getPostBuild
-    (elPlot, _) <- elAttr' "div" (("id" =: "pg-plot") <> ("style" =: "height: 360px;width: 360px")) $ return ()
-    rec performEvent_ $ leftmost
-            [ ffor pb (\_ -> liftIO $ do
-                let gr = maybe (GoalRatio 0) goalRatio alloc
-                _ <- P.pgPlot (_element_raw elPlot) tweak gr w
-                return ())
-            ]
-
-    let Weights
-            { distance = DistanceWeight d
-            , leading = LeadingWeight l
-            , time = TimeWeight t
-            } = w
-
-    el "ul" $ do
-        elClass "li" "legend-distance" . text $
-            textf "○ %.3f weight on distance" d
-        elClass "li" "legend-time" . text $
-            textf "○ %.3f weight on time" t
-        elClass "li" "legend-leading" . text $
-            textf "○ %.3f weight on leading" l
-
-    return ()
