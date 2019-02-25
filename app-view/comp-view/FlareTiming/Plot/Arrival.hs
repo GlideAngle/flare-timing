839b68013afdf7490fd089475feaebd9f372d538
Phil de Joux <philderbeast@gmail.com>
2019-02-25 15:55:57 -0500

    Notify that arrival place is not scored in paragliding, #212.

--- app-view/comp-view/FlareTiming/Plot/Arrival.hs
+++ app-view/comp-view/FlareTiming/Plot/Arrival.hs
@@ -16,25 +16,30 @@ arrivalPlot
 arrivalPlot hgOrPg av = do
     elClass "div" "tile is-ancestor" $ do
         _ <- dyn $ ffor hgOrPg (\hgOrPg' ->
-            if hgOrPg' /= HangGliding then return () else
-                elClass "div" "tile is-8" $
-                    elClass "div" "tile" $
-                        elClass "div" "tile is-parent" $ do
-                            _ <- dyn $ ffor av (\case
-                                    Nothing ->
-                                        elClass "article" "tile is-child box" $ do
-                                            elClass "p" "title" $ text "Arrivals"
-                                            el "p" $ text "Loading arrivals ..."
+            if hgOrPg' /= HangGliding
+               then
+                    elClass "article" "tile is-child notification is-warning" $ do
+                        elClass "p" "title" $ text "Arrivals"
+                        el "p" $ text "Arrival place is not scored in paragliding."
+               else
+                    elClass "div" "tile is-8" $
+                        elClass "div" "tile" $
+                            elClass "div" "tile is-parent" $ do
+                                _ <- dyn $ ffor av (\case
+                                        Nothing ->
+                                            elClass "article" "tile is-child box" $ do
+                                                elClass "p" "title" $ text "Arrivals"
+                                                el "p" $ text "Loading arrivals ..."
 
-                                    Just [] ->
-                                        elClass "article" "tile is-child notification is-warning" $ do
-                                            elClass "p" "title" $ text "Arrivals"
-                                            el "p" $ text "No pilots made it to the end of the speed section. There are no arrivals"
+                                        Just [] ->
+                                            elClass "article" "tile is-child notification is-warning" $ do
+                                                elClass "p" "title" $ text "Arrivals"
+                                                el "p" $ text "No pilots made it to the end of the speed section. There are no arrivals"
 
-                                    _ ->
-                                        elClass "article" "tile is-child box" $
-                                            hgPlot (fromMaybe [] <$> av))
+                                        _ ->
+                                            elClass "article" "tile is-child box" $
+                                                hgPlot (fromMaybe [] <$> av))
 
-                            return ())
+                                return ())
 
         return ()
