804a40e186b8af8631752e033acfc69f5e9922cd
Phil de Joux <philderbeast@gmail.com>
2019-03-04 13:54:39 -0500

    Merge notifications when PG and no arrivals anyway, #234.

--- app-view/comp-view/FlareTiming/Plot/Arrival.hs
+++ app-view/comp-view/FlareTiming/Plot/Arrival.hs
@@ -2,11 +2,18 @@ module FlareTiming.Plot.Arrival (arrivalPlot) where
 
 import Data.Maybe (fromMaybe)
 import Reflex.Dom
+import qualified Data.Text as T (Text)
 
 import WireTypes.Arrival (TrackArrival(..))
 import WireTypes.Comp (Discipline(..))
 import FlareTiming.Plot.Arrival.View (hgPlot)
 import WireTypes.Pilot (Pilot(..))
+
+noPg :: T.Text
+noPg = "Arrival place is not scored in paragliding."
+
+noArrivals :: T.Text
+noArrivals = "No pilots made it to the end of the speed section. There are no arrivals"
 
 arrivalPlot
     :: MonadWidget t m
@@ -18,15 +25,6 @@ arrivalPlot hgOrPg av = do
         elClass "div" "tile is-12" $
             elClass "div" "tile" $
                 elClass "div" "tile is-parent is-vertical" $ do
-                    _ <- dyn $ ffor hgOrPg (\hgOrPg' ->
-                        if hgOrPg' /= HangGliding
-                           then
-                                elClass "article" "tile is-child notification is-warning" $ do
-                                    elClass "p" "title" $ text "Arrive"
-                                    el "p" $ text "Arrival place is not scored in paragliding."
-                           else
-                                return ())
-
                     _ <- dyn $ ffor av (\case
                             Nothing ->
                                 elClass "article" "tile is-child box" $ do
@@ -36,10 +34,29 @@ arrivalPlot hgOrPg av = do
                             Just [] ->
                                 elClass "article" "tile is-child notification is-warning" $ do
                                     elClass "p" "title" $ text "Arrive"
-                                    el "p" $ text "No pilots made it to the end of the speed section. There are no arrivals"
 
-                            _ ->
+                                    _ <- dyn $ ffor hgOrPg (\hgOrPg' ->
+                                            if hgOrPg' /= HangGliding
+                                               then
+                                                    el "p" . text $ noPg <> " " <> noArrivals
+                                               else
+                                                    el "p" $ text noArrivals)
+
+                                    return ()
+
+                            _ -> do
+                                _ <- dyn $ ffor hgOrPg (\hgOrPg' ->
+                                    if hgOrPg' /= HangGliding
+                                       then
+                                            elClass "article" "tile is-child notification is-warning" $ do
+                                                elClass "p" "title" $ text "Arrive"
+                                                el "p" . text $ noPg
+                                       else
+                                            return ())
+
                                 elClass "article" "tile is-child box" $
-                                    hgPlot (fromMaybe [] <$> av))
+                                    hgPlot (fromMaybe [] <$> av)
+
+                                return ())
 
                     return ()
