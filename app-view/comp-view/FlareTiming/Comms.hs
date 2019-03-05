b4cbe517f1ae68138fd13d2f2616305f9062b5fc
Phil de Joux <philderbeast@gmail.com>
2019-03-05 07:20:53 -0500

    Switch back to local development.

--- app-view/comp-view/FlareTiming/Comms.hs
+++ app-view/comp-view/FlareTiming/Comms.hs
@@ -62,7 +62,7 @@ import FlareTiming.Events (IxTask(..))
 -- mapUri s = "http://2018-dalmatian.flaretiming.com/json" <> s <> ".json"
 -- mapUri s = "/json" <> s <> ".json"
 mapUri :: T.Text -> T.Text
-mapUri s = "/json" <> s <> ".json"
+mapUri s = "http://localhost:3000" <> s
 
 emptyRoute :: OptimalRoute (Maybe a)
 emptyRoute =
