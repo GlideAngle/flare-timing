8979af4ef09cf66df446692aaa011efb6caa51c5
Phil de Joux <philderbeast@gmail.com>
2019-03-20 09:46:39 -0400

    Property doctests as one-liners, True line did nothing anyway, #244.

--- gap/library/Flight/Gap/Equation.hs
+++ gap/library/Flight/Gap/Equation.hs
@@ -4,16 +4,10 @@ module Flight.Gap.Equation
 
 -- |
 -- prop> powerFraction 0 x == 0
--- True
---
 -- prop> powerFraction x 0 == 0
--- True
---
 -- prop> powerFraction x y >= 0
--- True
---
 -- prop> powerFraction x y <= 1
--- True
+-- False
 powerFraction :: Double -> Double -> Double
 powerFraction 0 _ = 0
 powerFraction _ 0 = 0
