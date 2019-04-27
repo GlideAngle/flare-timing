673f6257ffe1ac2229240a50e607e31fc6b40780
Phil de Joux <philderbeast@gmail.com>
2019-04-27 13:37:05 -0400

    When task lengths exist, serve them, #256.

--- app-serve/comp-serve/ServeMain.hs
+++ app-serve/comp-serve/ServeMain.hs
@@ -330,6 +330,8 @@ go CmdServeOptions{..} compFile@(CompInputFile compPath) = do
         (Nothing, _, _, _, _, _, _, _) -> putStrLn "Couldn't read the comp settings"
         (Just cs, rt@(Just _), cg@(Just _), tg@(Just _), mg@(Just _), lo@(Just _), gp@(Just _), ns@(Just _)) ->
             f =<< mkGapPointApp (Config compFile cs rt cg tg mg lo gp ns)
+        (Just cs, rt@(Just _), _, _, _, _, _, _) ->
+            f =<< mkTaskLengthApp (Config compFile cs rt Nothing Nothing Nothing Nothing Nothing Nothing)
         (Just cs, _, _, _, _, _, _, _) ->
             f =<< mkCompInputApp (Config compFile cs Nothing Nothing Nothing Nothing Nothing Nothing Nothing)
     where
