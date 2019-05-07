b6d9e33469628a6c3d2761e4cd4be7587dd13aaf
Phil de Joux <philderbeast@gmail.com>
2019-05-07 16:27:57 -0400

    Fill in value for pilot flying tag, #158.

--- app-view/comp-view/FlareTiming/Plot/Weight.hs
+++ app-view/comp-view/FlareTiming/Plot/Weight.hs
@@ -3,6 +3,7 @@ module FlareTiming.Plot.Weight (weightPlot) where
 import Reflex.Dom
 
 import qualified WireTypes.Validity as Vy (Validity(..))
+import WireTypes.ValidityWorking (ValidityWorking(..))
 import WireTypes.Comp (Discipline(..), Tweak(..))
 import WireTypes.Point (Allocation(..))
 import FlareTiming.Plot.Weight.View as W (hgPlot, pgPlot)
@@ -12,10 +13,11 @@ weightPlot
     :: MonadWidget t m
     => Dynamic t Discipline
     -> Dynamic t (Maybe Vy.Validity)
+    -> Dynamic t (Maybe ValidityWorking)
     -> Dynamic t (Maybe Tweak)
     -> Dynamic t (Maybe Allocation)
     -> m ()
-weightPlot hgOrPg vy tweak alloc = do
+weightPlot hgOrPg vy vw tweak alloc = do
     elClass "div" "tile is-ancestor" $ do
         elClass "div" "tile" $
             elClass "div" "tile is-4 is-parent" $
@@ -27,7 +29,7 @@ weightPlot hgOrPg vy tweak alloc = do
                     return ()
 
         elClass "div" "tile is-8 is-child" $ do
-            _ <- dyn $ ffor hgOrPg (\x -> viewWeightWorking x vy tweak alloc)
+            _ <- dyn $ ffor hgOrPg (\x -> viewWeightWorking x vy vw tweak alloc)
             return ()
 
     return ()
--- app-view/comp-view/FlareTiming/Plot/Weight/Working.hs
+++ app-view/comp-view/FlareTiming/Plot/Weight/Working.hs
@@ -8,6 +8,10 @@ import Text.Printf (printf)
 import qualified Data.Text as T (Text, pack)
 
 import qualified WireTypes.Validity as Vy (Validity(..), showTaskValidity)
+import WireTypes.ValidityWorking
+    ( ValidityWorking(..)
+    , LaunchValidityWorking(..)
+    )
 import WireTypes.Comp (Discipline(..), Tweak(..))
 import WireTypes.Point
     ( GoalRatio(..)
@@ -64,115 +68,118 @@ viewWeightWorking
     :: MonadWidget t m
     => Discipline
     -> Dynamic t (Maybe Vy.Validity)
+    -> Dynamic t (Maybe ValidityWorking)
     -> Dynamic t (Maybe Tweak)
     -> Dynamic t (Maybe Allocation)
     -> m ()
-viewWeightWorking hgOrPg vy' tw' al' = do
-    _ <- dyn $ ffor3 vy' tw' al' (\vy tw al ->
-        case (vy, tw, al) of
-            (Nothing, _, _) -> text "Loading validity ..."
-            (_, Nothing, _) -> text "Loading competition tweaks ..."
-            (_, _, Nothing) -> text "Loading allocations ..."
-            (Just Vy.Validity{task}, Just tweak, Just alloc) -> do
-                let (GoalRatio gr) = goalRatio alloc
+viewWeightWorking hgOrPg vy' vw' tw' al' = do
+    _ <- dyn $ ffor3 vy' tw' al' (\vy tw al -> do
+        _ <- dyn $ ffor vw' (\vw ->
+            case (vy, vw, tw, al) of
+                (Nothing, _, _, _) -> text "Loading validity ..."
+                (_, Nothing, _, _) -> text "Loading validity working ..."
+                (_, _, Nothing, _) -> text "Loading competition tweaks ..."
+                (_, _, _, Nothing) -> text "Loading allocations ..."
+                (Just Vy.Validity{task}, Just ValidityWorking{launch = LaunchValidityWorking{flying}}, Just tweak, Just alloc) -> do
+                    let (GoalRatio gr) = goalRatio alloc
 
-                let Weights
-                        { distance = DistanceWeight dw
-                        , arrival = ArrivalWeight aw
-                        , leading = LeadingWeight lw
-                        , time = TimeWeight tw
-                        } = weight alloc
+                    let Weights
+                            { distance = DistanceWeight dw
+                            , arrival = ArrivalWeight aw
+                            , leading = LeadingWeight lw
+                            , time = TimeWeight tw
+                            } = weight alloc
 
-                elAttr
-                    "a"
-                    (("class" =: "button") <> ("onclick" =: hookWorking tweak alloc))
-                    (text "Show Working")
+                    elAttr
+                        "a"
+                        (("class" =: "button") <> ("onclick" =: hookWorking tweak alloc))
+                        (text "Show Working")
 
-                spacer
+                    spacer
 
-                elClass "div" "card" $ do
-                    elClass "div" "card-content" $ do
-                        elClass "p" "subtitle is-6" $ do
-                            el "span" $
-                                elClass "h2" "title is-4" $ text "Weights"
+                    elClass "div" "card" $ do
+                        elClass "div" "card-content" $ do
+                            elClass "p" "subtitle is-6" $ do
+                                el "span" $
+                                    elClass "h2" "title is-4" $ text "Weights"
 
-                        elClass "div" "field is-grouped is-grouped-multiline" $ do
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "tv = task validity"
-                                    elClass "span" "tag is-info" . text
-                                        $ Vy.showTaskValidity task
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "gr = goal ratio"
-                                    elClass "span" "tag is-success" . text
-                                        $ textf "%.3f" gr
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "pg = pilots in goal"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "pf = pilots flying"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "dw = distance weight"
-                                    elClass "span" "tag is-info" . text
-                                        $ textf "%.3f" dw
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "tw = time weight"
-                                    elClass "span" "tag is-success" . text
-                                        $ textf "%.3f" tw
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "lw = leading weight"
-                                    elClass "span" "tag is-danger" . text
-                                        $ textf "%.3f" lw
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "aw = arrival weight"
-                                    elClass "span" "tag is-black" . text
-                                        $ textf "%.3f" aw
+                            elClass "div" "field is-grouped is-grouped-multiline" $ do
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "tv = task validity"
+                                        elClass "span" "tag is-info" . text
+                                            $ Vy.showTaskValidity task
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "gr = goal ratio"
+                                        elClass "span" "tag is-success" . text
+                                            $ textf "%.3f" gr
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "pg = pilots in goal"
+                                        elClass "span" "tag is-primary" . text
+                                            $ ""
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "pf = pilots flying"
+                                        elClass "span" "tag is-primary" . text
+                                            $ (T.pack . show $ flying)
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "dw = distance weight"
+                                        elClass "span" "tag is-info" . text
+                                            $ textf "%.3f" dw
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "tw = time weight"
+                                        elClass "span" "tag is-success" . text
+                                            $ textf "%.3f" tw
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "lw = leading weight"
+                                        elClass "span" "tag is-danger" . text
+                                            $ textf "%.3f" lw
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "aw = arrival weight"
+                                        elClass "span" "tag is-black" . text
+                                            $ textf "%.3f" aw
 
-                spacer
+                    spacer
 
-                elClass "div" "card" $ do
-                    elClass "div" "card-content" $ do
-                        elClass "p" "subtitle is-6" $ do
-                            el "span" $
-                                elClass "h2" "title is-4" $ text "Points"
+                    elClass "div" "card" $ do
+                        elClass "div" "card-content" $ do
+                            elClass "p" "subtitle is-6" $ do
+                                el "span" $
+                                    elClass "h2" "title is-4" $ text "Points"
 
-                        elClass "div" "field is-grouped is-grouped-multiline" $ do
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "dp = distance points"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "lp = leading points"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "ap = arrival points"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
-                            elClass "div" "control" $ do
-                                elClass "div" "tags has-addons" $ do
-                                    elClass "span" "tag" $ do text "tp = time points"
-                                    elClass "span" "tag is-primary" . text
-                                        $ ""
+                            elClass "div" "field is-grouped is-grouped-multiline" $ do
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "dp = distance points"
+                                        elClass "span" "tag is-primary" . text
+                                            $ ""
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "lp = leading points"
+                                        elClass "span" "tag is-primary" . text
+                                            $ ""
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "ap = arrival points"
+                                        elClass "span" "tag is-primary" . text
+                                            $ ""
+                                elClass "div" "control" $ do
+                                    elClass "div" "tags has-addons" $ do
+                                        elClass "span" "tag" $ do text "tp = time points"
+                                        elClass "span" "tag is-primary" . text
+                                            $ ""
 
-                            elAttr
-                                "div"
-                                ("id" =: "alloc-working")
-                                (text "")
+                                elAttr
+                                    "div"
+                                    ("id" =: "alloc-working")
+                                    (text "")
 
-                return ())
-
+                    return ())
+        return ())
     return ()
