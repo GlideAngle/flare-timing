b67c499863e01172f7b396c32d4b0187b2966b0a
Phil de Joux <philderbeast@gmail.com>
2019-06-26 14:31:04 -0400

    Work out fractions from maximum points in any category, #277.

--- flare-timing/prod-apps/fs-score/FsScoreMain.hs
+++ flare-timing/prod-apps/fs-score/FsScoreMain.hs
@@ -36,6 +36,9 @@ import Flight.Score
     ( BestTime(..), PilotTime(..)
     , LeadingFraction(..), LeadingPoints(..)
     , ArrivalFraction(..), ArrivalPoints(..)
+    , DistanceFraction(..), DistancePoints(..)
+    , LinearFraction(..), LinearPoints(..)
+    , DifficultyFraction(..), DifficultyPoints(..)
     , Points(..)
     , speedFraction
     )
@@ -120,34 +123,79 @@ normScores fsdbXml = do
     let lss =
             [
                 maybe
-                    ts
-                    (\ls ->
+                    xs
+                    (\ys ->
                         [ (p, t{fractions = fracs{Frac.leading = c}})
-                        | (_, c) <- ls
-                        | (p, t@NormBreakdown{fractions = fracs}) <- ts
+                        | (_, c) <- ys
+                        | (p, t@NormBreakdown{fractions = fracs}) <- xs
                         ]
                     )
-                    (leads ts)
+                    (leads xs)
 
-            | ts <- tss
+            | xs <- tss
             ]
 
     let ass =
             [
                 maybe
-                    ls
-                    (\as ->
+                    xs
+                    (\ys ->
                         [ (p, t{fractions = fracs{Frac.arrival = c}})
-                        | (_, c) <- as
-                        | (p, t@NormBreakdown{fractions = fracs}) <- ls
+                        | (_, c) <- ys
+                        | (p, t@NormBreakdown{fractions = fracs}) <- xs
                         ]
                     )
-                    (arrivals ls)
+                    (arrivals xs)
 
-            | ls <- lss
+            | xs <- lss
             ]
 
-    return np{bestTime = (fmap . fmap) fst vss, score = ass}
+    let rss =
+            [
+                maybe
+                    xs
+                    (\ys ->
+                        [ (p, t{fractions = fracs{Frac.reach = c}})
+                        | (_, c) <- ys
+                        | (p, t@NormBreakdown{fractions = fracs}) <- xs
+                        ]
+                    )
+                    (reaches xs)
+
+            | xs <- ass
+            ]
+
+    let ess =
+            [
+                maybe
+                    xs
+                    (\ys ->
+                        [ (p, t{fractions = fracs{Frac.effort = c}})
+                        | (_, c) <- ys
+                        | (p, t@NormBreakdown{fractions = fracs}) <- xs
+                        ]
+                    )
+                    (efforts xs)
+
+            | xs <- rss
+            ]
+
+    let dss =
+            [
+                maybe
+                    xs
+                    (\ys ->
+                        [ (p, t{fractions = fracs{Frac.distance = c}})
+                        | (_, c) <- ys
+                        | (p, t@NormBreakdown{fractions = fracs}) <- xs
+                        ]
+                    )
+                    (distances xs)
+
+            | xs <- ess
+            ]
+
+    return np{bestTime = (fmap . fmap) fst vss, score = dss}
 
 arrivals :: [(Pilot, NormBreakdown)] -> Maybe [(Pilot, ArrivalFraction)]
 arrivals xs =
@@ -162,19 +210,90 @@ arrivals xs =
 
         g arMax ar = arrivalFraction arMax ar
 
+reachFraction :: LinearPoints -> LinearPoints -> LinearFraction
+reachFraction (LinearPoints maxPts) (LinearPoints pts)
+    | maxPts == 0 = LinearFraction 0
+    | otherwise = LinearFraction . toRational $ pts / maxPts
+
+effortFraction :: DifficultyPoints -> DifficultyPoints -> DifficultyFraction
+effortFraction (DifficultyPoints maxPts) (DifficultyPoints pts)
+    | maxPts == 0 = DifficultyFraction 0
+    | otherwise = DifficultyFraction . toRational $ pts / maxPts
+
+distanceFraction :: DistancePoints -> DistancePoints -> DistanceFraction
+distanceFraction (DistancePoints maxPts) (DistancePoints pts)
+    | maxPts == 0 = DistanceFraction 0
+    | otherwise = DistanceFraction . toRational $ pts / maxPts
+
 arrivalFraction :: ArrivalPoints -> ArrivalPoints -> ArrivalFraction
 arrivalFraction (ArrivalPoints maxPts) (ArrivalPoints pts)
     | maxPts == 0 = ArrivalFraction 0
     | otherwise = ArrivalFraction . toRational $ pts / maxPts
 
+leadingFraction :: LeadingPoints -> LeadingPoints -> LeadingFraction
+leadingFraction (LeadingPoints maxPts) (LeadingPoints pts)
+    | maxPts == 0 = LeadingFraction 0
+    | otherwise = LeadingFraction . toRational $ pts / maxPts
+
+maxLinearPoints :: [LinearPoints] -> Maybe LinearPoints
+maxLinearPoints [] = Nothing
+maxLinearPoints xs = Just $ maximum xs
+
+maxDifficultyPoints :: [DifficultyPoints] -> Maybe DifficultyPoints
+maxDifficultyPoints [] = Nothing
+maxDifficultyPoints xs = Just $ maximum xs
+
+maxDistancePoints :: [DistancePoints] -> Maybe DistancePoints
+maxDistancePoints [] = Nothing
+maxDistancePoints xs = Just $ maximum xs
+
 maxArrivalPoints :: [ArrivalPoints] -> Maybe ArrivalPoints
 maxArrivalPoints [] = Nothing
 maxArrivalPoints xs = Just $ maximum xs
 
+maxLeadingPoints :: [LeadingPoints] -> Maybe LeadingPoints
+maxLeadingPoints [] = Nothing
+maxLeadingPoints xs = Just $ maximum xs
+
+reaches :: [(Pilot, NormBreakdown)] -> Maybe [(Pilot, LinearFraction)]
+reaches xs =
+    (\lf -> second (g lf) <$> ys) <$> maxLinearPoints cs
+    where
+        ys :: [(Pilot, LinearPoints)]
+        ys = (\(p, NormBreakdown{breakdown = Points{reach = c}}) -> (p,c)) <$> xs
+
+        cs :: [LinearPoints]
+        cs = snd <$> ys
+
+        g rMin r = reachFraction rMin r
+
+efforts :: [(Pilot, NormBreakdown)] -> Maybe [(Pilot, DifficultyFraction)]
+efforts xs =
+    (\lf -> second (g lf) <$> ys) <$> maxDifficultyPoints cs
+    where
+        ys :: [(Pilot, DifficultyPoints)]
+        ys = (\(p, NormBreakdown{breakdown = Points{effort = c}}) -> (p,c)) <$> xs
+
+        cs :: [DifficultyPoints]
+        cs = snd <$> ys
+
+        g eMin e = effortFraction eMin e
+
+distances :: [(Pilot, NormBreakdown)] -> Maybe [(Pilot, DistanceFraction)]
+distances xs =
+    (\lf -> second (g lf) <$> ys) <$> maxDistancePoints cs
+    where
+        ys :: [(Pilot, DistancePoints)]
+        ys = (\(p, NormBreakdown{breakdown = Points{distance = c}}) -> (p,c)) <$> xs
+
+        cs :: [DistancePoints]
+        cs = snd <$> ys
+
+        g dMin d = distanceFraction dMin d
+
 leads :: [(Pilot, NormBreakdown)] -> Maybe [(Pilot, LeadingFraction)]
 leads xs =
-    (\ lf -> second (g lf) <$> ys)
-    <$> maxLeadingPoints cs
+    (\lf -> second (g lf) <$> ys) <$> maxLeadingPoints cs
     where
         ys :: [(Pilot, LeadingPoints)]
         ys = (\(p, NormBreakdown{breakdown = Points{leading = c}}) -> (p,c)) <$> xs
@@ -184,21 +303,11 @@ leads xs =
 
         g lcMin lc = leadingFraction lcMin lc
 
-leadingFraction :: LeadingPoints -> LeadingPoints -> LeadingFraction
-leadingFraction (LeadingPoints maxPts) (LeadingPoints pts)
-    | maxPts == 0 = LeadingFraction 0
-    | otherwise = LeadingFraction . toRational $ pts / maxPts
-
-maxLeadingPoints :: [LeadingPoints] -> Maybe LeadingPoints
-maxLeadingPoints [] = Nothing
-maxLeadingPoints xs = Just $ maximum xs
-
 times
     :: [(Pilot, NormBreakdown)]
     -> Maybe (BestTime (Quantity Double [u| h |]), [(Pilot, TrackSpeed)])
 times xs =
-    (\ bt -> (bt, second (g bt) <$> ys))
-    <$> Gap.bestTime' ts
+    (\bt -> (bt, second (g bt) <$> ys)) <$> Gap.bestTime' ts
     where
         ys :: [(Pilot, PilotTime (Quantity Double [u| h |]))]
         ys =
