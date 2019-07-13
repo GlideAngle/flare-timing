144752da7aff34dee9f2dec44a950a52398d5afc
Phil de Joux <philderbeast@gmail.com>
2019-07-13 08:32:07 -0400

    Filter attributes of FsScoreFormula, #293.

--- fsdb/library/Flight/Fsdb/Filter.hs
+++ fsdb/library/Flight/Fsdb/Filter.hs
@@ -46,6 +46,7 @@ filterComp (FsdbXml contents) = do
                 [ fs
                 , fsCompetition
                 , fsCompetitionNotes
+                , fsScoreFormula
                 , fsParticipant
                 ])
         >>> writeDocumentToString [withIndent yes]
@@ -88,6 +89,50 @@ fsCompetition =
                     , "from"
                     , "to"
                     , "utc_offset"
+                    ])
+                . localPart)
+
+-- <FsScoreFormula
+--     id="GAP2015"
+--     min_dist="5"
+--     nom_dist="60"
+--     nom_time="2"
+--     nom_launch="1"
+--     nom_goal="0.2"
+--     day_quality_override="0"
+--     bonus_gr="5"
+--     jump_the_gun_factor="3"
+--     jump_the_gun_max="300"
+--     normalize_1000_before_day_quality="0"
+--     time_points_if_not_in_goal="0.8"
+--     use_1000_points_for_max_day_quality="0"
+--     use_arrival_position_points="1"
+--     use_arrival_time_points="0"
+--     use_departure_points="0"
+--     use_difficulty_for_distance_points="1"
+--     use_distance_points="1"
+--     use_distance_squared_for_LC="1"
+--     use_leading_points="1"
+--     use_semi_circle_control_zone_for_goal_line="0"
+--     use_time_points="1"
+--     final_glide_decelerator="none"
+--     no_final_glide_decelerator_reason=""
+--     min_time_span_for_valid_task="60"
+--     score_back_time="15" />
+fsScoreFormula :: ArrowXml a => a XmlTree XmlTree
+fsScoreFormula =
+    processTopDown
+        $ (flip when)
+            (isElem >>> hasName "FsScoreFormula")
+            (processAttrl . filterA . hasNameWith $
+                ( `elem`
+                    [ "id"
+                    , "min_dist"
+                    , "nom_dist"
+                    , "nom_time"
+                    , "nom_launch"
+                    , "nom_goal"
+                    , "score_back_time"
                     ])
                 . localPart)
 
