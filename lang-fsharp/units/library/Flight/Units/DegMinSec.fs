964657db43270878ee42fcfdb75e25e64521ddad
Phil de Joux <philderbeast@gmail.com>
2019-10-21 16:21:14 -0400

    Add tests for fromDeg and toDeg, #38.

--- lang-fsharp/units/library/Flight/Units/DegMinSec.fs
+++ lang-fsharp/units/library/Flight/Units/DegMinSec.fs
@@ -3,18 +3,36 @@
 open System
 open Flight.Units.Angle
 
+let sign' d = sign d |> function | 0 -> 1 | s -> s
+
+let toDeg (d, m, s) =
+    let n = sign' (float d) * sign' (float m) * sign' s
+    float n * (abs (float d) + abs (float m) / 60.0 + (abs s) / 3600.0)
+
+let fromDeg (d : float) =
+    let dAbs = abs d
+    let dd = int dAbs
+    let dFrac = dAbs - float dd
+    let minsFrac = dFrac * 60.0
+    let mins = int (floor minsFrac)
+    let secs = (minsFrac - float mins) * 60.0
+    (sign' d * dd, int mins, secs)
+
 type DMS =
     | DMS of deg : int * min : int * sec : float
 
     /// 0 <= a <= 2π
-    static member Normalize (x : DMS) = x
+    static member Normalize (x : DMS) = x |> DMS.ToDeg |> (%) 360.0<deg> |> DMS.FromDeg
+
     /// -π <= a <= +π
     static member PlusMinusPi (x : DMS) = x
     /// -π/2 <= a <= +π/2
     static member PlusMinusHalfPi (x : DMS) = Some x
     static member Rotate (rotation : DMS) (x : DMS) = x
-    static member ToQuantity (x : DMS) = 1.0<rad>
-    static member FromQuantity (_ : float<rad>) = DMS (0, 0, 0.0)
+    static member ToRad (x : DMS) = 1.0<rad>
+    static member FromRad (_ : float<rad>) = DMS (0, 0, 0.0)
+    static member ToDeg (x : DMS) = 1.0<deg>
+    static member FromDeg (d : float<deg>) = fromDeg (float d) |> DMS
 
     member x.Sign : int =
         match x with
@@ -75,6 +93,18 @@ module DegMinSecTests =
 
     [<Theory>]
     [<InlineData(0, 0, 0.0, 0.0)>]
+    [<InlineData(1.0, 0, 0.0, 1.0)>]
     [<InlineData(289, 30, 0.0, 289.5)>]
     let ``convert dms to deg`` deg min sec deg' =
         test <@ DMS (deg, min, sec) |> ToQuantity |> radToDeg = deg' @>
+
+    [<Theory>]
+    [<InlineData(0, 0, 0.0, 0.0)>]
+    [<InlineData(289, 30, 0.0, 289.5)>]
+    let ``(d, m, s) to deg`` d m s deg = test <@ toDeg (d, m, s) = deg @>
+
+    [<Theory>]
+    [<InlineData(0.0, 0, 0, 0.0)>]
+    [<InlineData(1.0, 1, 0, 0.0)>]
+    [<InlineData(289.5, 289, 30, 0.0)>]
+    let ``from deg to (d, m, s)`` deg d m s = test <@ fromDeg deg = (d, m, s) @>
