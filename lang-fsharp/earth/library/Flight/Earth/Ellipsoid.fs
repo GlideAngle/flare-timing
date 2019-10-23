ea6a004ab5954da2bf5c8c58c9a3c73990f3ca44
Phil de Joux <philderbeast@gmail.com>
2019-10-23 15:40:02 -0400

    Add well known ellipsoids, #38.

--- lang-fsharp/earth/library/Flight/Earth/Ellipsoid.fs
+++ lang-fsharp/earth/library/Flight/Earth/Ellipsoid.fs
@@ -52,3 +52,43 @@ type GeodeticInverse<'a>
     = GeodeticInverseAbnormal of AbnormalLatLng
     | GeodeticInverseAntipodal
     | GeodeticInverse of 'a
+
+// SEE: https://en.wikipedia.org/wiki/World_Geodetic_System
+// https://en.wikipedia.org/wiki/World_Geodetic_System#A_new_World_Geodetic_System:_WGS_84
+let wgs84 =
+    { equatorialR = Radius 6378137.0<m>
+    ; recipF = 298.257223563
+    }
+
+// As used by the National Geodetic Survey tool inverse when selecting the
+// ellipsoid 1) GRS80 / WGS84 (NAD83) SEE:
+// https://www.ngs.noaa.gov/PC_PROD/Inv_Fwd/
+let nad83 = {wgs84 with recipF = 298.25722210088}
+
+// The Bessel ellipsoid from Vincenty 1975. Note that the flattening from
+// Wikipedia for the Bessel ellipsoid is 299.1528153513233 not 299.1528128. SEE:
+// https://en.wikipedia.org/wiki/Bessel_ellipsoid
+let bessel =
+    { equatorialR = Radius 6377397.155<m>
+    ; recipF = 299.1528128
+    }
+
+// The International ellipsoid 1924 also known as the Hayford ellipsoid from
+// Vincenty 1975. SEE: https://en.wikipedia.org/wiki/Hayford_ellipsoid
+let hayford =
+    { equatorialR = Radius 6378388.0<m>
+    ; recipF = 297.0
+    }
+
+// Clarke's 1866 ellipsoid approximated in metres. "Clarke actually defined his
+// 1866 spheroid as a = 20,926,062 British feet, b = 20,855,121 British feet"
+// SEE: https://en.wikipedia.org/wiki/North_American_Datum
+let clarke =
+    { equatorialR = Radius 6378206.4<m>
+    ; recipF = 294.978698214
+    }
+
+// The ellipsoid used in Evaluation Direct and Inverse Geodetic Algorithms, by
+// Paul Delorme, Bedford Institute of Oceanography, Dartmouth, Nova Scotia,
+// Canada, 11978.
+let bedfordClarke = {clarke with recipF = 294.9786986}
