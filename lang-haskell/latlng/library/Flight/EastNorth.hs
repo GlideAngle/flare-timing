5095c683b5c8855b15b818928320821ca9c0ef27
Phil de Joux <philderbeast@gmail.com>
2019-10-23 10:26:10 -0400

    Styling only, record indentation, #38.

--- lang-haskell/latlng/library/Flight/EastNorth.hs
+++ lang-haskell/latlng/library/Flight/EastNorth.hs
@@ -4,19 +4,21 @@ import GHC.Generics (Generic)
 import Data.Aeson (ToJSON(..), FromJSON(..))
 
 data EastingNorthing =
-    EastingNorthing { easting :: Double
-                    , northing :: Double
-                    }
-                    deriving (Eq, Ord, Show, Generic)
+    EastingNorthing
+        { easting :: Double
+        , northing :: Double
+        }
+    deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON EastingNorthing
 instance FromJSON EastingNorthing
 
 data UtmZone =
-    UtmZone { latZone :: Char
-            , lngZone :: Int
-            }
-            deriving (Eq, Ord, Show, Generic)
+    UtmZone
+        { latZone :: Char
+        , lngZone :: Int
+        }
+    deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON UtmZone
 instance FromJSON UtmZone
