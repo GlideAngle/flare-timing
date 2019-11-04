83e4ed89206e6a7f6c0ac2d97effa4b08faa3984
Phil de Joux <philderbeast@gmail.com>
2019-11-04 12:22:10 -0500

    Align decimal points in columns of test data, #38.

--- lang-fsharp/earth/library/Flight/TestVincenty.fs
+++ lang-fsharp/earth/library/Flight/TestVincenty.fs
@@ -32,17 +32,17 @@ let ``distance is not negative given latlng pairs in radians`` xLat xLng yLat yL
         test <@ distance wgs84 xLL' yLL' >= TaskDistance (0.0<m>) @>
 
 [<Theory>]
-[<InlineData(0.0, 0.0, 0.0, 0.0)>]
-[<InlineData(90.0, 0.0, -90.0, 0.0)>]
-[<InlineData(-90.0, 0.0, 90.0, 0.0)>]
-[<InlineData(0.0, 180.0, 0.0, 0.0)>]
-[<InlineData(0.0, -180.0, 0.0, 0.0)>]
-[<InlineData(0.0, 0.0, 0.0, 180.0)>]
-[<InlineData(0.0, 0.0, 0.0, -180.0)>]
-[<InlineData(0.0, 180.0, 0.0, 180.0)>]
-[<InlineData(0.0, 180.0, 0.0, -180.0)>]
-[<InlineData(0.0, -180.0, 0.0, 180.0)>]
-[<InlineData(0.0, -180.0, 0.0, -180.0)>]
+[<InlineData(  0.0,    0.0,   0.0,    0.0)>]
+[<InlineData( 90.0,    0.0, -90.0,    0.0)>]
+[<InlineData(-90.0,    0.0,  90.0,    0.0)>]
+[<InlineData(  0.0,  180.0,   0.0,    0.0)>]
+[<InlineData(  0.0, -180.0,   0.0,    0.0)>]
+[<InlineData(  0.0,    0.0,   0.0,  180.0)>]
+[<InlineData(  0.0,    0.0,   0.0, -180.0)>]
+[<InlineData(  0.0,  180.0,   0.0,  180.0)>]
+[<InlineData(  0.0,  180.0,   0.0, -180.0)>]
+[<InlineData(  0.0, -180.0,   0.0,  180.0)>]
+[<InlineData(  0.0, -180.0,   0.0, -180.0)>]
 let ``distance is not negative given latlng pairs in degrees`` xLat xLng yLat yLng =
     let xLL = mkLatLng (convertDegToRad <| xLat * 1.0<deg>) (convertDegToRad <| xLng * 1.0<deg>)
     let yLL = mkLatLng (convertDegToRad <| yLat * 1.0<deg>) (convertDegToRad <| yLng * 1.0<deg>)
@@ -72,11 +72,11 @@ let ``distance is not negative forall latlng pairs`` () = Property.check <| prop
 // SEE: https://stackoverflow.com/a/50905110/1503186
 type Vincenty1975 () =
     static let xys =
-            [ ((( 55, 45,  0.0), (  0,  0,  0.0)), ((-33, 26,  0.0), (108, 13,  0.0)))
+            [ ((( 55, 45,  0.00000), (  0,  0,  0.0)), ((-33, 26,  0.00000), (108, 13,  0.00000)))
             ; ((( 37, 19, 54.95367), (  0,  0,  0.0)), (( 26,  7, 42.83946), ( 41, 28, 35.50729)))
             ; ((( 35, 16, 11.24862), (  0,  0,  0.0)), (( 67, 22, 14.77638), (137, 47, 28.31435)))
-            ; (((  1,  0,  0.0), (  0,  0,  0.0)), (( 0, -59, 53.83076), (179, 17, 48.02997)))
-            ; (((  1,  0,  0.0), (  0,  0,  0.0)), ((  1,  1, 15.18952), (179, 46, 17.84244)))
+            ; (((  1,  0,  0.00000), (  0,  0,  0.0)), (( 0, -59, 53.83076), (179, 17, 48.02997)))
+            ; (((  1,  0,  0.00000), (  0,  0,  0.0)), ((  1,  1, 15.18952), (179, 46, 17.84244)))
             ]
             |> List.map (fun ((xLat, xLng), (yLat, yLng)) ->
                 (
@@ -92,8 +92,8 @@ type Vincenty1975 () =
     static let es = bessel :: List.replicate 4 hayford
     static let ds : float<m> list =
             [ 14110526.170<m>
-            ; 4085966.703<m>
-            ; 8084823.839<m>
+            ;  4085966.703<m>
+            ;  8084823.839<m>
             ; 19960000.000<m>
             ; 19780006.5584<m>
             ]
@@ -102,7 +102,7 @@ type Vincenty1975 () =
             [ ( 96, 36,  8.79960)
             ; ( 95, 27, 59.63089)
             ; ( 15, 44, 23.74850)
-            ; ( 89,  0,  0.0)
+            ; ( 89,  0,  0.00000)
             ; (  4, 59, 59.99995)
             ]
             |> List.map (DMS.FromTuple >> DMS.ToRad)
