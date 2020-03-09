2ee6cb7d34896cffb1ba2ad8dce1d872f7d0751f
Phil de Joux <philderbeast@gmail.com>
2020-03-09 14:30:15 -0400

    Add step up to max time then max distance, #372.

--- lang-haskell/app-serve/comp-serve/ServeArea.hs
+++ lang-haskell/app-serve/comp-serve/ServeArea.hs
@@ -24,10 +24,25 @@ data RawLeadingArea =
 instance ToJSON RawLeadingArea where
     toJSON RawLeadingArea{raceTime = t, raceDistance = d, ticks = xs} = object
         [ "race-distance" .= toJSON d
-        , "lead-all-down" .= toJSON (join $ leadAllDown <$> t)
+        , "lead-all-down" .= toJSON down
         -- When the last pilot lands, seconds from the time of first lead.
-        , "distance-time" .= toJSON (catMaybes $ mkDistanceTime d <$> xs)
+        , "distance-time" .= toJSON (stepUp down d . catMaybes $ mkDistanceTime d <$> xs)
         ]
+        where
+            down = join $ leadAllDown <$> t
+
+stepUp :: Maybe EssTime -> Maybe (QTaskDistance Double [u| m |]) -> [[Double]] -> [[Double]]
+stepUp Nothing _ xs = xs
+stepUp _ Nothing xs = xs
+stepUp (Just (EssTime tMax)) (Just (TaskDistance td)) xs =
+    case reverse xs of
+        [d, _] : _ ->
+            let (MkQuantity dMax) :: Quantity Double [u| km |] = convert td
+                tMax' = fromRational tMax
+            in
+                if d >= dMax then xs else xs ++ [[d, tMax'], [dMax, tMax']]
+
+        _ -> xs
 
 mkDistanceTime :: Maybe (QTaskDistance Double [u| m |]) -> TickRow -> Maybe [Double]
 mkDistanceTime Nothing _ = Nothing
