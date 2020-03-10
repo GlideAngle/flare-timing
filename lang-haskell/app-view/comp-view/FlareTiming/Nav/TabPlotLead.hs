d82389b7b1eb2afd63d65d3423579ae9be84b5dd
Phil de Joux <philderbeast@gmail.com>
2020-03-09 21:55:37 -0400

    Re-label the tabs Area:Coef and Coef:Frac, #372.

--- lang-haskell/app-view/comp-view/FlareTiming/Nav/TabPlotLead.hs
+++ lang-haskell/app-view/comp-view/FlareTiming/Nav/TabPlotLead.hs
@@ -14,10 +14,10 @@ tabsPlotLead =
     elClass "div" "tabs" $
         el "ul" $ mdo
             (point, _) <- elDynClass' "li" splitClass . el "a" $ do
-                            text "Coefficient"
+                            text "Coef:Frac"
 
             (area, _) <- elDynClass' "li" reachClass . el "a" $ do
-                            text "Area"
+                            text "Area:Coef"
 
             let ePoint = (const PlotLeadTabPoint) <$> domEvent Click point
             let eArea = (const PlotLeadTabArea) <$> domEvent Click area
