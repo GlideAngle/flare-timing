264e40f493559a9759f4efe3c50d3e1740b92011
Phil de Joux <philderbeast@gmail.com>
2020-04-01 09:22:13 -0400

    Serve the zone crossings, #382.

--- lang-haskell/app-serve/comp-serve/ServeTrack.hs
+++ lang-haskell/app-serve/comp-serve/ServeTrack.hs
@@ -1,9 +1,15 @@
-module ServeTrack (RawLatLngTrack(..), BolsterStats(..), tagToTrack) where
+module ServeTrack
+    ( RawLatLngTrack(..)
+    , BolsterStats(..)
+    , crossToTrack
+    , tagToTrack
+    ) where
 
 import GHC.Generics (Generic)
 import Data.Aeson (ToJSON(..))
 
 import Flight.Kml (MarkedFixes(..), Fix(..), LLA(..), Latitude(..), Longitude(..))
+import Flight.Track.Cross (PilotTrackCross(..), TrackCross(..))
 import Flight.Track.Tag (PilotTrackTag(..), TrackTag(..))
 import Flight.Track.Cross (ZoneTag)
 import Flight.Score (ReachStats)
@@ -18,6 +24,9 @@ instance ToJSON RawLatLngTrack where
 mkLatLng :: Fix -> [Double]
 mkLatLng Fix{fix = LLA{llaLat = Latitude lat', llaLng = Longitude lng'}} =
     fromRational <$> [lat', lng']
+
+crossToTrack :: PilotTrackCross -> Maybe TrackCross
+crossToTrack (PilotTrackCross _ x) = x
 
 tagToTrack :: PilotTrackTag -> [Maybe ZoneTag]
 tagToTrack (PilotTrackTag _ Nothing) = []
