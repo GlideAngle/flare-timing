265cae1676b16f838c803133b14c675a4e3d6976
Phil de Joux <philderbeast@gmail.com>
2020-05-02 22:18:02 -0400

    Get flight-gap-math:doctest passing, #393.

--- lang-haskell/gap-math/test-suite-doctest/DocTest.hs
+++ lang-haskell/gap-math/test-suite-doctest/DocTest.hs
@@ -45,21 +45,8 @@ arguments =
     , "-fno-warn-partial-type-signatures"
 
     , "-package=facts"
+    , "-package=quickcheck-classes"
 
-    , "library/Flight/Gap/Distance/Chunk.hs"
-    , "library/Flight/Gap/Distance/Pilot.hs"
-    , "library/Flight/Gap/Distance/Relative.hs"
-    , "library/Flight/Gap/Distance/Stop.hs"
-    , "library/Flight/Gap/Fraction/Arrival.hs"
-    , "library/Flight/Gap/Fraction/Speed.hs"
-    , "library/Flight/Gap/Fraction/Difficulty.hs"
-    , "library/Flight/Gap/Place/Arrival.hs"
-    , "library/Flight/Gap/Time/Arrival.hs"
-    , "library/Flight/Gap/Time/Best.hs"
-    , "library/Flight/Gap/Time/Pilot.hs"
-    , "library/Flight/Gap/Allot.hs"
-    , "library/Flight/Gap/Pilots.hs"
-    , "library/Flight/Gap/Equation.hs"
     , "library/Flight/Gap/Points/Task.hs"
     , "library/Flight/Gap/Penalty.hs"
     ]
