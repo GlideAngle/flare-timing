6e69f9381dd5c12a456673c92024f8359de964fe
Phil de Joux <philderbeast@gmail.com>
2020-05-21 15:39:01 -0400

    Remove unused imports from ServeApi, #402.

--- lang-haskell/app-serve/src/ServeApi.hs
+++ lang-haskell/app-serve/src/ServeApi.hs
@@ -1,165 +1,36 @@
 module ServeApi where
 
-import Prelude hiding (abs)
-import GHC.Records
-import qualified Data.Text as T (Text)
 import Data.Time.Clock (UTCTime)
-import System.Environment (getProgName)
-import Text.Printf (printf)
-import System.Console.CmdArgs.Implicit (cmdArgs)
-import Data.Maybe (isNothing, catMaybes)
-import Data.List ((\\), nub, sort, sortOn, find, zip4)
-import qualified Data.Map.Strict as Map
 import Data.UnitsOfMeasure (u)
-import Data.UnitsOfMeasure.Internal (Quantity(..))
-import Network.Wai (Application)
-import Network.Wai.Middleware.Cors (simpleCors)
-import Network.Wai.Handler.Warp
-    (runSettings, defaultSettings, setPort, setBeforeMainLoop)
-import Servant
-    ( (:<|>)(..)
-    , Capture, Get, JSON, Server, Handler(..), Proxy(..), ServantErr
-    , (:>)
-    , errBody, err400, hoistServer, serve, throwError
-    )
-import Data.Swagger (Swagger(..), URL(..), url, info, title, version, license, description)
-import Servant.Swagger
-import Servant.Swagger.UI
-import Network.Wai.Middleware.Gzip (gzip, def)
-import System.IO (hPutStrLn, stderr)
-import Control.Exception.Safe (MonadThrow, catchIO)
-import Control.Lens hiding (ix)
-import Control.Monad (join)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader (ReaderT, MonadReader, asks, runReaderT)
-import Control.Monad.Except (ExceptT(..), MonadError)
-import qualified Data.ByteString.Lazy.Char8 as LBS (pack)
-import qualified Statistics.Sample as Stats (meanVariance)
-import qualified Data.Vector as V (fromList)
-import Numeric.Sampling
-import System.FilePath (takeFileName)
+import Servant ((:<|>)(..), Capture, Get, JSON, Proxy(..), (:>))
 
 import Flight.Units ()
 import Flight.Clip (FlyingSection)
-import qualified Flight.Track.Cross as Cg (Crossing(..), PilotTrackCross(..))
-import qualified Flight.Track.Tag as Tg (Tagging(..), PilotTrackTag(..))
-import qualified Flight.Track.Stop as Sp (Framing(..))
 import Flight.Track.Cross (TrackFlyingSection(..), ZoneTag(..), TrackCross(..))
 import Flight.Track.Stop (TrackScoredSection(..))
 import Flight.Track.Distance (TrackReach(..))
-import Flight.Track.Land
-    (Landing(..), TaskLanding(..), TrackEffort(..), effortRank, taskLanding)
+import Flight.Track.Land (TaskLanding(..), TrackEffort(..))
 import Flight.Track.Arrival (TrackArrival(..))
-import Flight.Track.Lead (DiscardingLead(..), TrackLead(..))
+import Flight.Track.Lead (TrackLead(..))
 import Flight.Track.Speed (TrackSpeed(..))
-import Flight.Track.Time (TickRow(..))
-import qualified Flight.Track.Mask as Mask (MaskingReach(..), MaskingArrival(..))
-import Flight.Track.Mask
-    ( MaskingEffort(..)
-    , MaskingLead(..)
-    , MaskingReach(..)
-    , MaskingSpeed(..)
-    )
-import qualified Flight.Track.Point as Norm (NormPointing(..), NormBreakdown(..))
-import Flight.Track.Point
-    (Pointing(..), Velocity(..), Allocation(..), Breakdown(..))
-import qualified "flight-gap-weight" Flight.Score as Wg (Weights(..))
+import qualified Flight.Track.Point as Norm (NormBreakdown(..))
+import Flight.Track.Point (Allocation(..), Breakdown(..))
 import qualified "flight-gap-valid" Flight.Score as Vy (Validity(..))
 import qualified "flight-gap-valid" Flight.Score as Vw (ValidityWorking(..))
-import "flight-gap-allot" Flight.Score (PilotVelocity(..))
 import "flight-gap-lead" Flight.Score (LeadingArea2Units)
-import "flight-gap-math" Flight.Score (TaskPoints(..))
-import "flight-gap-valid" Flight.Score
-    ( DistanceValidity(..), LaunchValidity(..), TimeValidity(..)
-    , TaskValidity(..), StopValidity(..)
-    )
-import "flight-gap-weight" Flight.Score
-    ( DistanceWeight(..), ReachWeight(..), EffortWeight(..)
-    , LeadingWeight(..), ArrivalWeight(..), TimeWeight(..)
-    )
-import Flight.Scribe
-    ( readComp, readNormArrival, readNormLandout, readNormRoute, readNormScore
-    , readRoute, readCrossing, readTagging, readFraming
-    , readMaskingArrival
-    , readMaskingEffort
-    , readDiscardingLead
-    , readMaskingLead
-    , readMaskingReach
-    , readMaskingSpeed
-    , readBonusReach
-    , readLanding, readPointing
-    , readPilotDiscardFurther
-    )
-import Flight.Cmd.Paths (LenientFile(..), checkPaths)
-import Flight.Cmd.Options (ProgramName(..))
-import Flight.Cmd.ServeOptions (CmdServeOptions(..), mkOptions)
-import Flight.Geodesy (EarthModel(..), EarthMath(..))
 import Flight.Comp
-    ( FileType(CompInput)
-    , CompSettings(..)
-    , Comp(..)
+    ( Comp(..)
     , Task(..)
     , Nominal(..)
-    , PilotTrackLogFile(..)
-    , IxTask(..)
-    , PilotId(..)
     , Pilot(..)
     , PilotTaskStatus(..)
-    , PilotGroup(..)
-    , DfNoTrack(..)
     , DfNoTrackPilot(..)
-    , Nyp(..)
-    , CompInputFile(..)
-    , TaskLengthFile(..)
-    , CrossZoneFile(..)
-    , TagZoneFile(..)
-    , PegFrameFile(..)
-    , MaskArrivalFile(..)
-    , MaskEffortFile(..)
-    , MaskLeadFile(..)
-    , LeadAreaFile(..)
-    , MaskReachFile(..)
-    , MaskSpeedFile(..)
-    , BonusReachFile(..)
-    , LandOutFile(..)
-    , GapPointFile(..)
-    , NormArrivalFile(..)
-    , NormLandoutFile(..)
-    , NormRouteFile(..)
-    , NormScoreFile(..)
     , Pilot(..)
-    , findCompInput
-    , compToNormArrival
-    , compToNormLandout
-    , compToNormRoute
-    , compToNormScore
-    , compToTaskLength
-    , compToCross
-    , compToMaskArrival
-    , compToMaskEffort
-    , compToLeadArea
-    , compToMaskLead
-    , compToMaskReach
-    , compToMaskSpeed
-    , compToBonusReach
-    , compToLand
-    , compToPoint
-    , crossToTag
-    , tagToPeg
-    , ensureExt
     )
-import Flight.Route
-    ( OptimalRoute(..), TaskTrack(..), TrackLine(..), GeoLines(..)
-    , ProjectedTrackLine(..), PlanarTrackLine(..)
-    )
-import Flight.Mask (checkTracks)
-import Data.Ratio.Rounding (dpRound)
+import Flight.Route (OptimalRoute(..), TrackLine(..) , PlanarTrackLine(..))
 import Flight.Distance (QTaskDistance)
-import qualified ServeOptions as Opt (description)
-import ServeTrack (RawLatLngTrack(..), BolsterStats(..), crossToTrack, tagToTrack)
+import ServeTrack (RawLatLngTrack(..), BolsterStats(..))
 import ServeArea (RawLeadingArea(..))
-import ServeValidity (nullValidityWorking)
-import ServeSwagger (SwagUiApi)
 
 type CompInputApi k =
     "comp-input" :> "comps"
