90d20f3ccd30b40641088c71eddb4bac8a0ebe15
Phil de Joux <philderbeast@gmail.com>
2020-10-24 08:48:02 -0400

    Parse utc_offset as a float rather than an int, #449.

--- lang-haskell/fsdb/library/Flight/Fsdb/Comp.hs
+++ lang-haskell/fsdb/library/Flight/Fsdb/Comp.hs
@@ -3,7 +3,7 @@ module Flight.Fsdb.Comp (parseComp) where
 import Text.XML.HXT.Arrow.Pickle
     ( PU(..)
     , unpickleDoc', xpWrap, xp9Tuple, xpFilterAttr, xpDefault
-    , xpElem, xpTrees, xpAttr, xpPrim, xpInt, xpOption, xpTextAttr
+    , xpElem, xpTrees, xpAttr, xpPrim, xpOption, xpTextAttr
     )
 import Text.XML.HXT.DOM.TypeDefs (XmlTree)
 import Text.XML.HXT.Core
@@ -39,7 +39,7 @@ xpComp =
         <+> hasName "score_back"
         )
     $ xpWrap
-        ( \(i, n, d, l, f, t, utc, s, _) ->
+        ( \(i, n, d, l, f, t, utc :: Double, s, _) ->
             let e = EarthAsFlat UTM; em = Pythagorus in
             Comp
                 { civilId = i
@@ -48,7 +48,7 @@ xpComp =
                 , location = l
                 , from = f
                 , to = t
-                , utcOffset = UtcOffset $ 60 * utc
+                , utcOffset = UtcOffset . round $ 60.0 * utc
                 , scoreBack = s
                 , give = Nothing
                 , earth = e
@@ -61,7 +61,7 @@ xpComp =
             , location
             , from
             , to
-            , let (UtcOffset utc) = utcOffset in utc `div` 60
+            , fromIntegral $ let (UtcOffset utc) = utcOffset in utc `div` 60
             , scoreBack
             , []
             )
@@ -73,7 +73,7 @@ xpComp =
         (xpTextAttr "location")
         (xpTextAttr "from")
         (xpTextAttr "to")
-        (xpAttr "utc_offset" xpInt)
+        (xpAttr "utc_offset" xpPrim)
         (xpOption $ xpAttr "score_back" xpPrim)
         xpTrees
 
