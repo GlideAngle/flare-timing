a1611cd925df97bb18e5fce5c8b4402d2e0c9571
Phil de Joux <philderbeast@gmail.com>
2020-10-30 09:26:14 -0400

    Revert to using lazy bytestrings reading and writing, #448.

--- lang-haskell/scribe/library/Flight/UnpackTrack.hs
+++ lang-haskell/scribe/library/Flight/UnpackTrack.hs
@@ -8,7 +8,6 @@ import Control.DeepSeq
 import Control.Exception.Safe (MonadThrow, throwString, catchIO)
 import Control.Monad.Except (MonadIO, liftIO)
 import Control.Monad (zipWithM)
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
     ( Header
@@ -17,7 +16,7 @@ import Data.Csv
     , encodeByNameWith
     , defaultEncodeOptions
     )
-import qualified Data.ByteString.Char8 as S (writeFile, readFile)
+import qualified Data.ByteString.Lazy.Char8 as L (writeFile)
 import Data.Vector (Vector)
 import qualified Data.Vector as V (toList)
 import System.FilePath ((</>))
@@ -34,21 +33,17 @@ import Flight.Comp
     , compFileToCompDir
     )
 
--- SEE: https://gist.github.com/dino-/28b09c465c756c44b2c91d777408e166
-mkLazy :: B.ByteString -> BL.ByteString
-mkLazy = BL.fromChunks . return
-
 readUnpackTrack
     :: (MonadThrow m, MonadIO m)
     => UnpackTrackFile
     -> m (Header, Vector TrackRow)
 readUnpackTrack (UnpackTrackFile csvPath) = do
-    contents <- liftIO $ S.readFile csvPath
-    either throwString return $!! decodeByName (mkLazy contents)
+    contents <- liftIO $ BL.readFile csvPath
+    either throwString return $!! decodeByName contents
 
 writeUnpackTrack :: UnpackTrackFile -> [TrackRow] -> IO ()
 writeUnpackTrack (UnpackTrackFile path) xs =
-    S.writeFile path $ BL.toStrict rows
+    L.writeFile path rows
     where
         (TimeHeader hs) = timeHeader
         opts = defaultEncodeOptions {encUseCrLf = False}
