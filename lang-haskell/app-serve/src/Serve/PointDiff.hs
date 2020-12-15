d6b59d601f260b722a6170d99798b5c9a2fea212
Phil de Joux <philderbeast@gmail.com>
2020-12-15 16:31:38 -0500

    Substitute a score of zero when there is no score getting stats, #502.

--- lang-haskell/app-serve/src/Serve/PointDiff.hs
+++ lang-haskell/app-serve/src/Serve/PointDiff.hs
@@ -1,6 +1,6 @@
 module Serve.PointDiff (DiffWay(..), getTaskPointsDiffStats) where
 
-import Control.Monad.Reader (asks)
+import Control.Monad.Reader (MonadReader(), asks)
 import qualified Data.Map.Strict as Map
 import qualified Statistics.Sample as Stats (meanVariance)
 import qualified Data.Vector as V (fromList)
@@ -16,21 +16,33 @@ import Serve.Error (errTaskPoints, errAltPoints)
 
 data DiffWay = DiffWayFtFs | DiffWayFtAs | DiffWayAsFs
 
+getFt :: MonadReader (Config k) m => () -> m (Maybe [[(Pilot, TaskPoints)]])
+getFt () = do
+    fmap (\Pointing{score} -> (fmap . fmap . fmap) (\Breakdown{total} -> total) score) <$> asks pointing
+
+getFs :: MonadReader (Config k) m => () -> m (Maybe [[(Pilot, TaskPoints)]])
+getFs () = do
+    fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altFsScore
+
+getAs :: MonadReader (Config k) m => () -> m (Maybe [[(Pilot, TaskPoints)]])
+getAs () = do
+    fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altAsScore
+
 getTaskPointsDiffStats :: DiffWay -> AppT k IO [Maybe (Double, Double)]
 
 getTaskPointsDiffStats DiffWayFtFs = do
-    ys <- fmap (\Pointing{score} -> (fmap . fmap . fmap) (\Breakdown{total} -> total) score) <$> asks pointing
-    xs <- fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altFsScore
+    ys <- getFt ()
+    xs <- getFs ()
     getDiffStats ys xs
 
 getTaskPointsDiffStats DiffWayFtAs = do
-    ys <- fmap (\Pointing{score} -> (fmap . fmap . fmap) (\Breakdown{total} -> total) score) <$> asks pointing
-    xs <- fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altAsScore
+    ys <- getFt ()
+    xs <- getAs ()
     getDiffStats ys xs
 
 getTaskPointsDiffStats DiffWayAsFs = do
-    ys <- fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altAsScore
-    xs <- fmap (\Alt.AltPointing{score} -> (fmap . fmap . fmap) (\Alt.AltBreakdown{total} -> total) score) <$> asks altFsScore
+    ys <- getAs ()
+    xs <- getFs ()
     getDiffStats ys xs
 
 getDiffStats
@@ -56,8 +68,10 @@ getDiffStats ys xs = do
 
 diffStats :: [Maybe TaskPoints] -> [TaskPoints] -> Maybe (Double, Double)
 diffStats xs ys =
+    -- NOTE: For some pilots, airScore is giving a null score. To do the stats,
+    -- substitute zero for those scores.
     let xs' :: Maybe [TaskPoints]
-        xs' = sequence xs
+        xs' = Just $ maybe (TaskPoints 0) id <$> xs
     in do
             xs'' <- xs'
             let diffs = zipWith (\(TaskPoints y) (TaskPoints x) -> y - x) ys xs''
