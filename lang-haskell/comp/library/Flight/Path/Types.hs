a5706a4312f07ea07358c408ae6323bdfa2af9ff
Phil de Joux <philderbeast@gmail.com>
2020-12-18 14:00:21 -0500

    Use deriving newtype to get the file path shown, #502.

--- lang-haskell/comp/library/Flight/Path/Types.hs
+++ lang-haskell/comp/library/Flight/Path/Types.hs
@@ -95,111 +95,183 @@ data FileShape
     | DotDirName FilePath DotFolder
 
 -- | The path to a competition arrival file, scored elsewhere.
-newtype AltArrivalFile = AltArrivalFile FilePath deriving (Eq, Show)
+newtype AltArrivalFile = AltArrivalFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition effort file, scored elsewhere.
-newtype AltLandoutFile = AltLandoutFile FilePath deriving (Eq, Show)
+newtype AltLandoutFile = AltLandoutFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition optimal route file, scored elsewhere.
-newtype AltRouteFile = AltRouteFile FilePath deriving (Eq, Show)
+newtype AltRouteFile = AltRouteFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition score file, scored elsewhere.
-newtype AltScoreFile = AltScoreFile FilePath deriving (Eq, Show)
+newtype AltScoreFile = AltScoreFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a *.igc file.
-newtype IgcFile = IgcFile FilePath deriving (Eq, Show)
+newtype IgcFile = IgcFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a *.kml file.
-newtype KmlFile = KmlFile FilePath deriving (Eq, Show)
+newtype KmlFile = KmlFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition *.fsdb file.
-newtype FsdbFile = FsdbFile FilePath deriving (Eq, Show)
+newtype FsdbFile = FsdbFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition *.clean-fsdb.xml file, an *.fsdb clean of
 -- potentially sensitive private information.
-newtype CleanFsdbFile = CleanFsdbFile FilePath deriving (Eq, Show)
+newtype CleanFsdbFile = CleanFsdbFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition *.trim-fsdb.xml file, an *.fsdb filtered for use
 -- by flare-timing..
-newtype TrimFsdbFile = TrimFsdbFile FilePath deriving (Eq, Show)
+newtype TrimFsdbFile = TrimFsdbFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The XML string contents of a *.fsdb file.
-newtype FsdbXml = FsdbXml String deriving (Eq, Show)
+newtype FsdbXml = FsdbXml String
+    deriving Eq
+    deriving newtype Show
 
 -- | The directory path of a competition file.
-newtype CompDir = CompDir FilePath deriving (Eq, Show)
+newtype CompDir = CompDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a competition file.
-newtype CompInputFile = CompInputFile FilePath deriving (Eq, Show)
+newtype CompInputFile = CompInputFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a task length file.
-newtype TaskLengthFile = TaskLengthFile FilePath deriving (Eq, Show)
+newtype TaskLengthFile = TaskLengthFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a cross zone file.
-newtype CrossZoneFile = CrossZoneFile FilePath deriving (Eq, Show)
+newtype CrossZoneFile = CrossZoneFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a tag zone file.
-newtype TagZoneFile = TagZoneFile FilePath deriving (Eq, Show)
+newtype TagZoneFile = TagZoneFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a stop task file.
-newtype PegFrameFile = PegFrameFile FilePath deriving (Eq, Show)
+newtype PegFrameFile = PegFrameFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to an unpack track directory for a single task.
-newtype UnpackTrackDir = UnpackTrackDir FilePath deriving (Eq, Show)
+newtype UnpackTrackDir = UnpackTrackDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a discard further directory for a single task.
-newtype DiscardFurtherDir = DiscardFurtherDir FilePath deriving (Eq, Show)
+newtype DiscardFurtherDir = DiscardFurtherDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to an align time directory for a single task.
-newtype AlignTimeDir = AlignTimeDir FilePath deriving (Eq, Show)
+newtype AlignTimeDir = AlignTimeDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a area step directory for a single task.
-newtype AreaStepDir = AreaStepDir FilePath deriving (Eq, Show)
+newtype AreaStepDir = AreaStepDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a peg then discard directory for a single task.
-newtype PegThenDiscardDir = PegThenDiscardDir FilePath deriving (Eq, Show)
+newtype PegThenDiscardDir = PegThenDiscardDir FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to as unpack track file.
-newtype UnpackTrackFile = UnpackTrackFile FilePath deriving (Eq, Show)
+newtype UnpackTrackFile = UnpackTrackFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to an align time file.
-newtype AlignTimeFile = AlignTimeFile FilePath deriving (Eq, Show)
+newtype AlignTimeFile = AlignTimeFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a discard further file.
-newtype DiscardFurtherFile = DiscardFurtherFile FilePath deriving (Eq, Show)
+newtype DiscardFurtherFile = DiscardFurtherFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a area step file.
-newtype AreaStepFile = AreaStepFile FilePath deriving (Eq, Show)
+newtype AreaStepFile = AreaStepFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a peg then discard file.
-newtype PegThenDiscardFile = PegThenDiscardFile FilePath deriving (Eq, Show)
+newtype PegThenDiscardFile = PegThenDiscardFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a leading area file.
-newtype LeadAreaFile = LeadAreaFile FilePath deriving (Eq, Show)
+newtype LeadAreaFile = LeadAreaFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask arrival file.
-newtype MaskArrivalFile = MaskArrivalFile FilePath deriving (Eq, Show)
+newtype MaskArrivalFile = MaskArrivalFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask effort file.
-newtype MaskEffortFile = MaskEffortFile FilePath deriving (Eq, Show)
+newtype MaskEffortFile = MaskEffortFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask lead file.
-newtype MaskLeadFile = MaskLeadFile FilePath deriving (Eq, Show)
+newtype MaskLeadFile = MaskLeadFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask reach file.
-newtype MaskReachFile = MaskReachFile FilePath deriving (Eq, Show)
+newtype MaskReachFile = MaskReachFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask speed file.
-newtype MaskSpeedFile = MaskSpeedFile FilePath deriving (Eq, Show)
+newtype MaskSpeedFile = MaskSpeedFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a mask reach with altitude bonus distance file.
-newtype BonusReachFile = BonusReachFile FilePath deriving (Eq, Show)
+newtype BonusReachFile = BonusReachFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a land out file.
-newtype LandOutFile = LandOutFile FilePath deriving (Eq, Show)
+newtype LandOutFile = LandOutFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a far out file.
-newtype FarOutFile = FarOutFile FilePath deriving (Eq, Show)
+newtype FarOutFile = FarOutFile FilePath
+    deriving Eq
+    deriving newtype Show
 
 -- | The path to a gap point file.
-newtype GapPointFile = GapPointFile FilePath deriving (Eq, Show)
+newtype GapPointFile = GapPointFile FilePath
+    deriving Eq
+    deriving newtype Show
