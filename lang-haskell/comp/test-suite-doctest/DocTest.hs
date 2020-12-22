b73c5be441581eab098197ac412b040da295d1e7
Phil de Joux <philderbeast@gmail.com>
2020-12-22 16:07:47 -0500

    Undo comment-out of Track/Place module doctest, #507.

--- lang-haskell/comp/test-suite-doctest/DocTest.hs
+++ lang-haskell/comp/test-suite-doctest/DocTest.hs
@@ -7,7 +7,7 @@ arguments =
     [ "-isrc"
     , "library/Flight/Path/Types.hs"
     , "library/Flight/Path/Tx.hs"
-    --, "library/Flight/Track/Place.hs"
+    , "library/Flight/Track/Place.hs"
     , "-XDataKinds"
     , "-XDerivingStrategies"
     , "-XFlexibleContexts"
