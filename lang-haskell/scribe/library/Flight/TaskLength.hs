ff54a2ab1a2f997778af48b112c035ae47d3045d
Phil de Joux <philderbeast@gmail.com>
2021-01-02 09:13:04 -0500

    Log task length files with intro and nested file names, #513.

--- lang-haskell/scribe/library/Flight/TaskLength.hs
+++ lang-haskell/scribe/library/Flight/TaskLength.hs
@@ -26,22 +26,24 @@ writeRoute (TaskLengthFile lenPath) route = do
 
 readRoutes :: CompInputFile -> IO [Maybe TaskTrack]
 readRoutes compFile = do
+    putStrLn "Reading task lengths from:"
     taskFiles <- compFileToTaskFiles compFile
     parallel
         [ do
-            putStrLn $ "Reading task length from " ++ show routeFile
+            putStrLn $ "\t" ++ show routeFile
             readRoute routeFile
         | routeFile <- taskToTaskLength <$> taskFiles
         ]
 
 writeRoutes :: CompInputFile -> [Maybe TaskTrack] -> IO ()
 writeRoutes compFile routes = do
+    putStrLn "Writing task lengths to:"
     let compDir = compFileToCompDir compFile
     parallel_
         [ do
             let (TaskDir dir, TaskLengthFile file) = taskLengthPath compDir ixTask
             let routeFile = TaskLengthFile $ dir </> file
-            putStrLn $ "Writing task length to " ++ show routeFile
+            putStrLn $ "\t" ++ show routeFile
             writeRoute routeFile route
 
         | route <- routes
