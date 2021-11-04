7a1165b9010853b9f861530cce7ba1f5ac209f16
Phil de Joux <philderbeast@gmail.com>
2021-11-04 12:02:46 -0400

    Follow the hlint suggestions in flight-gap-valid.

--- lang-haskell/gap-valid/test-suite-valid/StopValidity.hs
+++ lang-haskell/gap-valid/test-suite-valid/StopValidity.hs
@@ -20,83 +20,83 @@ import TestNewtypes
 stopValidityUnits :: TestTree
 stopValidityUnits = testGroup "Is a stopped task valid?"
     [ HU.testCase "Not when no one launches = 0 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 0)
             (PilotsAtEss 0)
             (PilotsLanded 0)
             (PilotsFlying 1)
             (mkReachStats [])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 0
 
     , HU.testCase "When everyone makes ESS, one pilot launched and is still flying = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 1)
             (PilotsAtEss 1)
             (PilotsLanded 0)
             (PilotsFlying 1)
             (mkReachStats [1])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When everyone makes ESS, one pilot launched and has landed = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 1)
             (PilotsAtEss 2)
             (PilotsLanded 1)
             (PilotsFlying 0)
             (mkReachStats [1])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When everyone makes ESS, two pilots launched, both still flying = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 2)
             (PilotsAtEss 2)
             (PilotsLanded 0)
             (PilotsFlying 2)
             (mkReachStats [1, 1])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When everyone makes ESS, two pilots launched, noone still flying = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 2)
             (PilotsAtEss 2)
             (PilotsLanded 2)
             (PilotsFlying 0)
             (mkReachStats [1, 1])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When everyone makes ESS, two pilots launched, one still flying = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 2)
             (PilotsAtEss 2)
             (PilotsLanded 1)
             (PilotsFlying 1)
             (mkReachStats [1, 1])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When one makes ESS, one still flying at launch point = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 2)
             (PilotsAtEss 1)
             (PilotsLanded 0)
             (PilotsFlying 1)
             (mkReachStats [1, 0])
-            (LaunchToEss [u| 1 km |])) & fst)
+            (LaunchToEss [u| 1 km |]) & fst)
             @?= StopValidity 1
 
     , HU.testCase "When one makes ESS, one still flying on course halfway to ESS = 1 validity" $
-        ((FS.stopValidity
+        (FS.stopValidity
             (PilotsFlying 2)
             (PilotsAtEss 1)
             (PilotsLanded 0)
             (PilotsFlying 1)
             (mkReachStats [2, 1])
-            (LaunchToEss [u| 2 km |])) & fst)
+            (LaunchToEss [u| 2 km |]) & fst)
             @?= StopValidity 1
     ]
 
