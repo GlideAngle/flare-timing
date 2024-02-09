02ce13ff1c86e7bd05c06fa76f0ed9f5c542d853
Phil de Joux <philderbeast@gmail.com>
2024-02-09 14:08:06 -0500

    Regenerate projects

---
+++ project-nix/ghc-8.2.2/sha256map.nix
@@ -0,0 +1,5 @@
+{
+  "https://github.com/blockscope/uom-plugin"."7419e73e7fa534cced7ecd3232103f6304bed728" = "1wkiynb2h8pdshz23a7378aps216hkjda42qvlg4zscid16fff4s";
+  "https://github.com/typechecker/exact-real"."c4308efc37c53e266fee348a80fdfbabb18822be" = "10ix9wj8gdlm3im59zcfibk0q3nq6lwkim3mvg9xzqss6w326ixg";
+  "https://github.com/typechecker/hcoord"."e6f72eeb16c10f29b6d2438cca6b7d3978ea91f9" = "00mdqwhsfd2v8f0nsi7px039fbzqi3ar6ipx3zx39pgbqrd8zb51";
+}
