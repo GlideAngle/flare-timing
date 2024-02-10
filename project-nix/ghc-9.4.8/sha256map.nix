e3ac8377cc5149d1359de808996bce423e065d83
Phil de Joux <philderbeast@gmail.com>
2024-02-10 08:51:06 -0500

    Regenerate projects

---
+++ project-nix/ghc-9.4.8/sha256map.nix
@@ -0,0 +1,6 @@
+{
+  "https://github.com/typechecker/exact-real"."c4308efc37c53e266fee348a80fdfbabb18822be" = "10ix9wj8gdlm3im59zcfibk0q3nq6lwkim3mvg9xzqss6w326ixg";
+  "https://github.com/typechecker/facts"."fda33feb0c4ddd95f77a453dd4199ef7be4a3b82" = "0893yc5ai8648sma4v9dzpwvq4vkcn7pk6d3vzvln6gdnnzhwwz0";
+  "https://github.com/typechecker/hcoord"."e6f72eeb16c10f29b6d2438cca6b7d3978ea91f9" = "00mdqwhsfd2v8f0nsi7px039fbzqi3ar6ipx3zx39pgbqrd8zb51";
+  "https://github.com/typechecker/hxt-pickle-utils"."1c9349233701f896313a9a0cb163d65f8b15bb9d" = "1y1kl8gpgqhxibj8c8lvn5v6m88aqlambnnh0s6cj2lkfabzxw1m";
+}
